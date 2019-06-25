dbbff619cc54e4f0cb1ab7db287fb870b807d3dd
Glazelf <gidoen.steenbergen@gmail.com>
2019-06-25 02:39:47 +0200

    what

--- node_modules/enmap/docs/build.js
+++
@@ -1,4 +0,0 @@
-const jsdoc2md = require('jsdoc-to-markdown');
-const fs = require('fs');
-
-jsdoc2md.render({ files: './src/index.js' }).then(data => fs.writeFile('./docs/api-docs.md', data, () => false));
