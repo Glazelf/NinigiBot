bccaab5b978071b06cf3dc2fe89ded1fd45e51da
Glazelf <gideon.steenbergen@gmail.com>
2019-06-25 18:37:24 +0200

    message.channel fix

--- commands/restart.js
+++ commands/restart.js
@@ -1,6 +1,6 @@
 exports.run = (client, message, args) => {
     // send channel a message that you're resetting bot [optional]
-    channel.send('Resetting...')
+    message.channel.send('Resetting...')
     .then(msg => client.destroy())
     .then(() => client.login(client.config.token));
 };
