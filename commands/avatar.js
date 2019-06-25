5cb4b1cd66966982760bcd3a864a2a06aea2ad1b
Glazelf <gideon.steenbergen@gmail.com>
2019-06-25 18:29:15 +0200

    Avatar fix

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,8 +1,8 @@
-module.exports.run = async (bot, message, args) => {
+module.exports.run = async (bot, client, message, args) => {
     let name = "avatar";
     let description = "Shows a user's avatar.";
     let usage = `${client.config.prefix}avatar [@user]`
-    let msg = await message.channel.send("doing some magic ...");
+    let msg = await message.channel.send("Using Psychic to find the desired image...");
     let target = message.mentions.users.first() || message.author;
 
     await message.channel.send({
@@ -13,12 +13,11 @@ module.exports.run = async (bot, message, args) => {
             }
         ]
     })
-
     msg.delete();
 };
 
 module.exports.help = {
-    name: "avatar",
+    name: "Avatar",
     description: "Shows a user's avatar.",
     usage: `avatar [@user]`
 };
