0605d687b476dff40757605d226e13a7e5214bfe
Glazelf <gideon.steenbergen@gmail.com>
2019-06-25 15:22:43 +0200

    uptime command

---
+++ commands/uptime.js
@@ -0,0 +1,21 @@
+exports.run = (client, message, args) => {
+    // Calculate the uptime in days, hours, minutes, seconds
+    let totalSeconds = (client.uptime / 1000);
+    let days = Math.floor(totalSeconds / 86400);
+    let hours = Math.floor(totalSeconds / 3600);
+    totalSeconds %= 3600;
+    let minutes = Math.floor(totalSeconds / 60);
+    let seconds = totalSeconds % 60;
+    if (days > 1) {
+        let uptime = `${days} day(s), ${hours} hour(s), ${minutes} minute(s) and ${seconds} second(s)`;
+    } else {
+        let uptime = `${hours} hour(s), ${minutes} minute(s) and ${seconds} second(s)`;
+    }
+    message.channel.send(`Pong! ${new Date().getTime() - message.createdTimestamp}ms`).catch(console.error);
+};
+
+module.exports.help = {
+    name: "uptime",
+    description: "Displays the current uptime of this bot.",
+    usage: `uptime`
+};
