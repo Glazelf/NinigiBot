ee4645335880aa0bb021ce6e3a674728bc68c917
Glazelf <gideon.steenbergen@gmail.com>
2019-06-25 15:16:30 +0200

    botName variable

--- commands/info.js
+++ commands/info.js
@@ -1,5 +1,5 @@
 exports.run = (client, message, args) => {
-    message.channel.send(`Hello. ${message.guild.me.name} was made by ${client.config.ownerName} as a passion project in Javascript! You can DM him at ${client.config.ownerAccount} for more information!`);
+    message.channel.send(`Hello. ${client.config.botName} was made by ${client.config.ownerName} as a passion project in Javascript! You can DM him at ${client.config.ownerAccount} for more information!`);
 };
 
 module.exports.help = {
