7bac97d5abdc64e4424393829008a6925d671e4b
Glazelf <gideon.steenbergen@gmail.com>
2019-06-25 13:28:39 +0200

    ping timestamp

--- commands/ping.js
+++ commands/ping.js
@@ -1,3 +1,3 @@
 exports.run = (client, message, args) => {
-    message.channel.send("pong!").catch(console.error);
+    message.channel.send(`pong! ${new Date().getTime() - message.createdTimestamp}ms`).catch(console.error);
 }
