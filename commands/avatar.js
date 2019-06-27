4f47866b3a558de304939949028f6420b7c9dbcb
Glazelf <gideon.steenbergen@gmail.com>
2019-06-27 13:14:34 +0200

    remove ID requirement

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,7 +1,4 @@
 module.exports.run = async (bot, client, message, args) => {
-    if (!message.author.id === client.config.ownerID) {
-        return message.channel.send("This command is currently a work in progress and unavailable to you because using it might crash the bot, sorry!")
-    }
     const user = message.mentions.users.first() || message.author;
     const avatarEmbed = new Discord.RichEmbed()
         .setColor('#7E21EF')
