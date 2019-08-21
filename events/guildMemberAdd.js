5de25a2ef3aaff4080823013305dc8aa2136743d
Glazelf <gideon.steenbergen@gmail.com>
2019-08-21 23:58:15 +0200

    dont guildmemberadd

--- events/guildMemberAdd.js
+++
@@ -1,4 +0,0 @@
-module.exports = (client, member) => {
-  const defaultChannel = member.guild.channels.find(channel => channel.permissionsFor(guild.me).has("SEND_MESSAGES"));
-  defaultChannel.send(`Welcome ${member.user} to this server.`).catch(console.error);
-}
