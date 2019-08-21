db7573cf27ae89a3bde76f38bde2b0a8eebc39f8
Glazelf <gideon.steenbergen@gmail.com>
2019-08-22 00:20:14 +0200

    kill init

---
+++ commands/kill.js
@@ -0,0 +1,14 @@
+exports.run = (client, message, args) => {
+    if (message.author.id !== client.config.ownerID) {
+        return message.channel.send(client.config.lackPerms)
+    }
+    // send channel a message that you're resetting bot [optional]
+    message.channel.send('Shutting down...')
+        .then(msg => client.destroy())
+};
+
+module.exports.help = {
+    name: "Kill",
+    description: "Shuts down the entire bot. Requires ownership of this bot.",
+    usage: `kill`
+}; 
