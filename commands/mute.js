3b321111195a8c7b980440cc16159f8eee54ba41
Glazelf <gideon.steenbergen@gmail.com>
2019-08-25 13:36:50 +0200

    getUserFromMention

--- commands/mute.js
+++ commands/mute.js
@@ -2,6 +2,19 @@ const Discord = require("discord.js");
 const ms = require("ms");
 
 module.exports.run = async (client, bot, message, args) => {
+    function getUserFromMention(mention) {
+        // The id is the first and only match found by the RegEx.
+        const matches = mention.match(/^<@!?(\d+)>$/);
+    
+        // If supplied variable was not a mention, matches will be null instead of an array.
+        if (!matches) return;
+    
+        // However the first element in the matches array will be the entire mention, not just the ID,
+        // so use index 1.
+        const id = matches[1];
+    
+        return client.users.get(id);
+    }
 
   //usage: ?tempmute @user 1s/m/h/d
   const user = getUserFromMention(args[0])
