f57f78ad256789835b91c3124d4c3be28121de25
Glazelf <gideon.steenbergen@gmail.com>
2019-08-25 12:39:48 +0200

    purge success message

--- commands/purge.js
+++ commands/purge.js
@@ -1,11 +1,12 @@
 exports.run = (client, message, args) => {
     var numberOfMessages = message.content.slice(7);
     if(isNaN(numberOfMessages)){
-        message.channel.send(`"${numberOfMessages}" is not a number, please specify an amount of messages that should be deleted.`);
+        message.channel.send(`Sorry, but "${numberOfMessages}" is not a number, please specify an amount of messages that should be deleted.`);
     } else {
     let messageCount = parseInt(numberOfMessages);
     message.channel.fetchMessages({ limit: messageCount })
-        .then(messages => message.channel.bulkDelete(messages));
+        .then(messages => message.channel.bulkDelete(messages))
+        .then(message.channel.send(`${numberOfMessages} messages have been deleted.`))
     }
 };
 
