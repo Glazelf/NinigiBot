4be351aeb394e578d3c5d691a01f272e86720bad
Glazelf <gideon.steenbergen@gmail.com>
2019-08-25 13:02:49 +0200

    msg => message

--- commands/mute.js
+++ commands/mute.js
@@ -2,16 +2,16 @@ exports.run = (client, message, args) => {
     if (message.author.id !== client.config.ownerID) {
         return message.channel.send(client.config.lackPerms)
     }
-    let member= msg.mentions.members.first();
-    if(!member) return msg.reply("You have not mentioned a member to mute, please specify a target.");
-    let muteRole = msg.guild.roles.find("name", "Muted");
-    if(!muteRole) return msg.reply(`There is no "Muted" role, please create one.`);
-    let params = msg.content.split(" ").slice(1);
+    let member= message.mentions.members.first();
+    if(!member) return message.reply("You have not mentioned a member to mute, please specify a target.");
+    let muteRole = message.guild.roles.find("name", "Muted");
+    if(!muteRole) return message.reply(`There is no "Muted" role, please create one.`);
+    let params = message.content.split(" ").slice(1);
     let time = params[1];
-    if(!time) return msg.reply("There was no time specified, please specify an amount of time to mute for.");
+    if(!time) return message.reply("There was no time specified, please specify an amount of time to mute for.");
 
     member.addRole(muteRole.id);
-    msg.channel.send(`You've been muted for ${ms(ms(time), {long: true})} ${member.user.tag}`);
+    message.channel.send(`You've been muted for ${ms(ms(time), {long: true})} ${member.user.tag}`);
 
     setTimeout(function(){
         member.removeRole(mute.id);
