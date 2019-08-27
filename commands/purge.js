a888cf2e32e62ce962bc3b2cdd9de8922cc33ca4
Glazelf <gideon.steenbergen@gmail.com>
2019-08-27 13:50:48 +0200

    Catch messages > 100

--- commands/purge.js
+++ commands/purge.js
@@ -5,6 +5,8 @@ exports.run = (client, message, args) => {
     var numberOfMessages = message.content.slice(7);
     if (isNaN(numberOfMessages)) {
         message.channel.send(`Sorry, but "${numberOfMessages}" is not a number, please specify an amount of messages that should be deleted.`);
+    } else if (numberOfMessages > 100) {
+        message.channel.send(`Sorry, but Discord does not allow more than 100 messages to be deleted at once.`);
     } else {
         let messageCount = parseInt(numberOfMessages);
         message.channel.fetchMessages({ limit: messageCount })
