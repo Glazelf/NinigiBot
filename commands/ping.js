a204c517a9b19609689931880fe36892ffef8240
Glazelf <gideon.steenbergen@gmail.com>
2019-08-30 20:48:26 +0200

    message.member

--- commands/ping.js
+++ commands/ping.js
@@ -1,5 +1,5 @@
 exports.run = (client, message, args, member) => {
-    if (!message.guild.member.hasPermission("MANAGE_MESSAGES")) return message.reply(client.config.lackPerms);
+    if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply(client.config.lackPerms);
     return message.channel.send(`Pong!'ed back at <@${message.member.user.id}> in ${new Date().getTime() - message.createdTimestamp}ms.`).catch(console.error);
 };
 
