8718623c9a8d445d8400147fa89403923ccaaffd
Glazelf <gideon.steenbergen@gmail.com>
2019-08-30 20:37:01 +0200

    permission fix

--- commands/ping.js
+++ commands/ping.js
@@ -1,5 +1,5 @@
 exports.run = (client, message, args) => {
-    if (message.author.hasPermission("MANAGE_MESSAGES")) return message.reply(client.config.lackPerms);
+    if (member.hasPermission("MANAGE_MESSAGES") = false) return message.reply(client.config.lackPerms);
     return message.channel.send(`Pong!'ed back at <@${message.member.user.id}> in ${new Date().getTime() - message.createdTimestamp}ms.`).catch(console.error);
 };
 
