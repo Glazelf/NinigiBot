3de38968d2e8ce9cf4c500fc1d365a314c4e8f4a
Glazelf <gideon.steenbergen@gmail.com>
2019-08-30 09:10:05 +0200

    dangogifs include array

--- commands/dango.js
+++ commands/dango.js
@@ -2,7 +2,7 @@ exports.run = (client, message, args) => {
     if (message.author.id !== client.config.ownerID && message.author.id !== client.config.subOwnerID) {
         return message.channel.send(client.config.lackPerms)
     }
-    var randomDango = client.config.dangoGifs[Math.floor(Math.random() * myArray.length)];
+    var randomDango = client.config.dangoGifs[Math.floor(Math.random() * client.config.dangoGifs.length)];
     message.channel.send(`Here's your gif: <@${message.member.user.id}>: ${randomDango}`);
 };
 
