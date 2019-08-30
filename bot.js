940ede39baedb465125e358e7112967609e9b94f
Glazelf <gideon.steenbergen@gmail.com>
2019-08-30 13:28:57 +0200

    formatting

--- bot.js
+++ bot.js
@@ -13,12 +13,16 @@ client.config = config;
 fs.readdir("./events/", (err, files) => {
   if (err) return console.error(err);
   files.forEach(file => {
+
     // If the file is not a JS file, ignore it.
     if (!file.endsWith(".js")) return;
+
     // Load the event file itself
     const event = require(`./events/${file}`);
+
     // Get just the event name from the file name
     let eventName = file.split(".")[0];
+
     // Each event will be called with the client argument,
     // followed by its "normal" arguments, like message, member, etc.
     client.on(eventName, event.bind(null, client));
@@ -32,11 +36,14 @@ fs.readdir("./commands/", (err, files) => {
   if (err) return console.error(err);
   files.forEach(file => {
     if (!file.endsWith(".js")) return;
+
     // Load the command file itself
     let props = require(`./commands/${file}`);
+
     // Get just the command name from the file name
     let commandName = file.split(".")[0];
     console.log(`Attempting to load command ${commandName}`);
+    
     // Store the whole thing in Enmap
     client.commands.set(commandName, props);
   });
--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,7 @@
 module.exports = (client) => {
   console.log(`Ready to serve in ${client.channels.size} channels on ${client.guilds.size} servers, for a total of ${client.users.size} users.`);
   console.log(`Connected as ${client.user.tag}.`)
+  
   // Set bot status
   client.user.setStatus('idle')
   client.user.setActivity('over Sinnoh', { type: 'WATCHING' })
