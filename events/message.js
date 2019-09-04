3dbeee2d422580baf3dbfd7747fd1350d6087842
Glazelf <gideon.steenbergen@gmail.com>
2019-09-04 17:08:45 +0200

    reorder command checks

--- events/message.js
+++ events/message.js
@@ -9,16 +9,6 @@ module.exports = (client, message, channel) => {
   // Ignore all bots
   if (message.author.bot) return;
 
-  // Ignore messages that aren't in bot channel or by a mod (except help)
-  if (message.channel.id != `${client.config.botChannelID}` && !message.member.hasPermission("MANAGE_MESSAGES") && message != `${client.config.prefix}help`) {
-    return message.channel.send(`Sorry <@${message.member.user.id}>, you're not allowed to use commands here, try using commands in: <#${client.config.botChannelID}>.`);
-  };
-
-  // Ignore commands in DMs
-  if (message.channel.type == "dm" && message.author.id !== client.config.ownerID && message != `${client.config.prefix}help` && message != `${client.config.prefix}info`) {
-    return message.author.send(`Sorry <@${message.member.user.id}>, you're not allowed to use commands other than "${client.config.prefix}help" and "${client.config.prefix}info"  in private messages!`).catch(console.error);
-  };
-
   // Ignore messages not starting with the prefix
   if (message.content.indexOf(client.config.prefix) !== 0) return;
 
@@ -28,6 +18,16 @@ module.exports = (client, message, channel) => {
   // Ignore messages that start with prefix double or prefix space
   var secondCharacter = message.content.charAt(1);
   if (secondCharacter == `${client.config.prefix}` || secondCharacter == ` `) return;
+
+  // Ignore messages that aren't in bot channel or by a mod (except help)
+  if (message.channel.id != `${client.config.botChannelID}` && !message.member.hasPermission("MANAGE_MESSAGES") && message != `${client.config.prefix}help`) {
+    return message.channel.send(`Sorry <@${message.member.user.id}>, you're not allowed to use commands here, try using commands in: <#${client.config.botChannelID}>.`);
+  };
+
+  // Ignore commands in DMs
+  if (message.channel.type == "dm" && message.author.id !== client.config.ownerID && message != `${client.config.prefix}help` && message != `${client.config.prefix}info`) {
+    return message.author.send(`Sorry <@${message.member.user.id}>, you're not allowed to use commands other than "${client.config.prefix}help" and "${client.config.prefix}info"  in private messages!`).catch(console.error);
+  };
 
   // Standard definition
   const args = message.content.slice(client.config.prefix.length).trim().split(/ +/g);
