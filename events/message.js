1a129ec057b485e971dd87784eaf685e7e55978f
Glazelf <gideon.steenbergen@gmail.com>
2019-09-04 16:48:02 +0200

    export channel

--- events/message.js
+++ events/message.js
@@ -1,4 +1,4 @@
-module.exports = (client, message) => {
+module.exports = (client, message, channel) => {
   // Import totals
   var { totalMessages } = require('./ready');
   var { totalCommands } = require('./ready');
