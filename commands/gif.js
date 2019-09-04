f7215a597537c94310106e894677ccd903334d0e
Glazelf <gideon.steenbergen@gmail.com>
2019-09-04 16:28:15 +0200

    revert gif update

--- commands/gif.js
+++ commands/gif.js
@@ -281,12 +281,15 @@ exports.run = (client, message, args) => {
 -Stitch
 -Hug`);
   } else if (gifArgument == "squirtle") {
-
-    var gifsArray = gifArgument.charAt(0).toUpperCase() + string.slice(1);
+    var gifsArray = gifsSquirtle;
     var randomGif = gifsArray[Math.floor(Math.random() * gifsArray.length)];
     var totalMessage = `${gifString} ${randomGif}`;
-    return message.channel.send(`${totalMessage}`);
-    
+
+    // Test for changing layout of this giant if/else statement:
+    // var gifsArray = gifArgument.charAt(0).toUpperCase() + string.slice(1);
+    // var randomGif = gifsArray[Math.floor(Math.random() * gifsArray.length)];
+    // var totalMessage = `${gifString} ${randomGif}`;
+    // return message.channel.send(`${totalMessage}`);
   } else if (gifArgument == "wooper") {
     var gifsArray = gifsWooper;
     var randomGif = gifsArray[Math.floor(Math.random() * gifsArray.length)];
