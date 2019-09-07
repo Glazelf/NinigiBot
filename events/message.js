3081c7f67c15eca5364c6633b776209ceed031cf
Glazelf <gideon.steenbergen@gmail.com>
2019-09-07 21:55:39 +0200

    remove channel requirement bc of multiserver

--- events/message.js
+++ events/message.js
@@ -24,11 +24,6 @@ module.exports = (client, message, channel) => {
     return message.author.send(`Sorry <@${message.member.user.id}>, you're not allowed to use commands other than "${client.config.prefix}help" and "${client.config.prefix}info"  in private messages!`).catch(console.error);
   };
 
-  // Ignore messages that aren't in bot channel or by a mod (except help)
-  if (message.channel.id != `${client.config.botChannelID}` && !message.member.hasPermission("MANAGE_MESSAGES") && message != `${client.config.prefix}help`) {
-    return message.channel.send(`Sorry <@${message.member.user.id}>, you're not allowed to use commands here, try using commands in: <#${client.config.botChannelID}>.`);
-  };
-
   // Standard definition
   const args = message.content.slice(client.config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
