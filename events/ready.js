66246aa6d9d0d04f5d09e634622a9f4814038b45
Glazelf <gideon.steenbergen@gmail.com>
2019-09-16 15:03:17 +0200

    formatting

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,7 @@
 module.exports = (client) => {
   console.log(`Ready to serve in ${client.channels.size} channels on ${client.guilds.size} servers, for a total of ${client.users.size} users.`);
   console.log(`Connected as ${client.user.tag}.`)
-  
+
   // Set bot status
   client.user.setStatus('idle')
   client.user.setActivity('over Sinnoh', { type: 'WATCHING' })
@@ -12,11 +12,11 @@ module.exports = (client) => {
     console.log(' - ' + guild.name)
   });
 
-// Create global variables
-var totalMessages = 0;
-var totalCommands = 0;
+  // Create global variables
+  var totalMessages = 0;
+  var totalCommands = 0;
 
-// Export global variables
-module.exports = { totalMessages };
-module.exports = { totalCommands };
+  // Export global variables
+  module.exports = { totalMessages };
+  module.exports = { totalCommands };
 };
