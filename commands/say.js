ad615d3d58018f40d8a3b9c8a0f51b8b52746132
Glazelf <gideon.steenbergen@gmail.com>
2019-09-21 21:17:42 +0200

    remote say usage

--- commands/say.js
+++ commands/say.js
@@ -2,18 +2,36 @@ exports.run = (client, message, args) => {
     // Split off command
     let textMessage = message.content.slice(5);
 
-    //Catch empty argument
+    // Catch empty argument
     if (textMessage.length < 1) {
         return message.channel.send("> You need to specify text for me to say.")
     };
 
-    //Add credits to avoid anonymous abuse
+    // Owner only function to send messages in different channels
+    if (message.author.id == client.config.ownerID && message.member.hasPermission("ADMINISTRATOR")) {
+        let split = textMessage.split(` `, 1);
+        let remoteMessage = textMessage.slice(channelID.length + 1);
+        const channelID = split[0];
+
+        // Catch nonexisting channel IDs
+        if (!client.channels.has(channelID)) {
+            return message.channel.send("> The bot can't access the channel ID you provided.");
+        };
+
+        return message.guild.channels.find("id", channelID).sendMessage(remoteMessage);
+    };
+
+    // Add credits to avoid anonymous abuse by people who are admin nor owner
     if (!message.member.hasPermission("ADMINISTRATOR") && message.author.id !== client.config.ownerID) {
         textMessage = `> "${textMessage}"
 > -<@${message.member.user.id}>`;
+        return message.channel.send(textMessage);
     };
 
-    return message.channel.send(textMessage)
+    // Return plain message if member is only either admin or owner
+    if(message.member.hasPermission("ADMINISTRATOR") || message.author.id == client.config.ownerID){
+        return message.channel.send(textMessage);
+    };
 };
 
 module.exports.help = {
