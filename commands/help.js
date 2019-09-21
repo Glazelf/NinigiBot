5b86f59e0573abd171d1575133c02de871359596
Glazelf <gideon.steenbergen@gmail.com>
2019-09-21 23:36:23 +0200

    typo errorhandling, jesus someone should streamline this

--- commands/help.js
+++ commands/help.js
@@ -40,7 +40,7 @@ module.exports.run = async (bot, message, args, con) => {
         // send msg to owner
         let members = message.channel.members;
         let owner = members.find('id', client.config.ownerID);
-        owner.send(`> An error occurred using a command in <#${message.channel.id}> by <@${message.member.user.id}> using a command, check console for more information.`);
+        owner.send(`> An error occurred while <@${message.member.user.id}> tried to use a command in <#${message.channel.id}>, check console for more information.`);
         // log error
         console.log(e);
         return message.channel.send(`> An error has occurred trying to run the command, please contact <@${client.config.ownerID}>.`)
