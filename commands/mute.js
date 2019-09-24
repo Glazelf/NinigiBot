df13eab236ca310730c0b793db3d1b8c8a11413d
Glazelf <gideon.steenbergen@gmail.com>
2019-09-24 18:26:31 +0200

    mute test

--- commands/mute.js
+++ commands/mute.js
@@ -1,12 +1,8 @@
-module.exports.run = async (client, bot, message, args) => {
+module.exports.run = async (client, message, args) => {
   try {
     if (message.author.id !== client.config.ownerID) {
       return message.channel.send(client.config.lackPerms)
     };
-
-    const Discord = require("discord.js");
-    const ms = require("ms");
-
     function getUserFromMention(mention) {
       if (!mention) return;
 
@@ -16,55 +12,36 @@ module.exports.run = async (client, bot, message, args) => {
         if (mention.startsWith('!')) {
           mention = mention.slice(1);
         };
-
         return client.users.get(mention);
       };
     };
 
-    //usage: ?tempmute @user 1s/m/h/d
-    let tomute = message.member(message.mentions.users.first() || message.members.get(args[0]));
-    if (!tomute) return message.reply("Couldn't find user.");
-    if (tomute.hasPermission("MANAGE_MESSAGES")) return message.reply(client.config.lackPerms);
-    let muterole = message.guild.roles.find(`name`, "Muted");
+    if (args[0]) {
+      const user = getUserFromMention(args[0]);
+      let roleMute = msg.guild.roles.find("name", "Muted");
 
-    //if no role, create
-    if (!muterole) {
-      try {
-        muterole = await message.guild.createRole({
-          name: "Muted",
-          color: "#000000",
-          permissions: []
-        })
-        message.guild.channels.forEach(async (channel, id) => {
-          await channel.overwritePermissions(muterole, {
-            SEND_MESSAGES: false,
-            ADD_REACTIONS: false
-          });
-        });
-      } catch (e) {
-        console.log(e.stack);
+      if (!user) {
+        return message.reply(`> Please use a proper mention if you want to mute someone, <@${message.author.id}.`);
       };
+
+      // this assign the role
+      user.addRole(roleMute);
+
+      // sets a timeout to unmute the user.
+      setTimeout(() => { user.removeRole(roleMute); }, 60 * 1000);
     };
 
-    let mutetime = args[1];
-    if (!mutetime) return message.reply("You didn't specify a time the target should be muted for.");
-
-    await (tomute.addRole(muterole.id));
-    message.reply(`<@${tomute.id}> has been muted for ${ms(ms(mutetime))}.`);
-
-    setTimeout(function () {
-      tomute.removeRole(muterole.id);
-      message.channel.send(`<@${tomute.id}> has been unmuted.`);
-    }, ms(mutetime));
-    
   } catch (e) {
     // send msg to owner
     let members = message.channel.members;
     let owner = members.find('id', client.config.ownerID);
     owner.send(`> An error occurred while <@${message.member.user.id}> tried to use a command in <#${message.channel.id}>, check console for more information.`);
+
     // log error
     console.log(e);
-    return message.channel.send(`> An error has occurred trying to run the command, please contact <@${client.config.ownerID}>.`)
+
+    // return confirmation
+    return message.channel.send(`> An error has occurred trying to run the command, <@${message.author.id}>, please use "${client.config.prefix}report" to report the issue.`);
   };
 };
 
