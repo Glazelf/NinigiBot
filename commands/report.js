290cc56d5dc4f3110a11994882412e3f311b97fa
Glazelf <gideon.steenbergen@gmail.com>
2019-09-24 12:06:44 +0200

    init report command

---
+++ commands/report.js
@@ -0,0 +1,34 @@
+exports.run = (client, message, args) => {
+    try {
+        // Split off command
+        let reportMessage = message.content.slice(5);
+
+        // send msg to owner
+        let members = message.channel.members;
+        let owner = members.find('id', client.config.ownerID);
+        owner.send(`> Report done by: <@${message.member.user.id}> in <#${message.channel.id}>.
+> Contents: 
+${reportMessage}`);
+
+        // return confirmation
+        return message.channel.send(`> Report successfully sent.`);
+
+    } catch (e) {
+        // send msg to owner
+        let members = message.channel.members;
+        let owner = members.find('id', client.config.ownerID);
+        owner.send(`> An error occurred while <@${message.member.user.id}> tried to use a command in <#${message.channel.id}>, check console for more information.`);
+
+        // log error
+        console.log(e);
+
+        // return confirmation
+        return message.channel.send(`> An error has occurred trying to run the command, please contact <@${client.config.ownerID}>.`);
+    };
+};
+
+module.exports.help = {
+    name: "Report",
+    description: "Reports a complaint or bug you have, please only use in case it's usefull and be specific about your issue.",
+    usage: `Report [text]`
+};
