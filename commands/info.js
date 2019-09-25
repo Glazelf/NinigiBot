b9f710d90a0bb07ed31085bf052c45f06eeb0a23
Glazelf <gideon.steenbergen@gmail.com>
2019-09-25 14:05:50 +0200

    add all of ?stats to ?info

--- commands/info.js
+++ commands/info.js
@@ -1,7 +1,7 @@
 exports.run = (client, message, args) => {
-    try {    
+    try {
         const Discord = require("discord.js");
-        
+
         let bot = client.users.find("id", client.config.botID);
 
         // Name presence type
@@ -36,6 +36,40 @@ exports.run = (client, message, args) => {
             presenceName = bot.presence.game;
         };
 
+        // Calculate the uptime in days, hours, minutes, seconds
+        let totalSeconds = (client.uptime / 1000);
+        let days = Math.floor(totalSeconds / 86400);
+        let hours = Math.floor(totalSeconds / 3600);
+        totalSeconds %= 3600;
+        let minutes = Math.floor(totalSeconds / 60);
+        let seconds = Math.floor(totalSeconds % 60);
+
+        // Figure out if the numbers given is different than 1
+        let multiDays = "";
+        if (days !== 1) { multiDays = "s" };
+        let multiHours = "";
+        if (hours !== 1) { multiHours = "s" };
+        let multiMinutes = "";
+        if (minutes !== 1) { multiMinutes = "s" };
+        let multiSeconds = "";
+        if (seconds !== 1) { multiSeconds = "s" };
+
+        // Import totals
+        let globalVars = require('../events/ready');
+
+        // Reset hours
+        if (hours >= 24) {
+            hours = hours - 24;
+        };
+
+        // Bind variables together into a string
+        let uptime = `${hours} hour${multiHours}, ${minutes} minute${multiMinutes} and ${seconds} second${multiSeconds}`;
+
+        // Add day count if there are days
+        if (days != 0) {
+            uptime = `${days} day${multiDays}, ${uptime}`;
+        };
+
         const profileEmbed = new Discord.RichEmbed()
             .setColor(0x219dcd)
             .setAuthor(`Name: ${client.config.botName}`)
@@ -44,9 +78,15 @@ exports.run = (client, message, args) => {
             .addField("Owner:", `<@${client.config.ownerID}>`, true)
             .addField("Full account:", client.config.botAccount, true)
             .addField("Bot ID:", client.config.botID, true)
-            .addField("Code:", "[Github](https://github.com/Glazelf/NinigiBot 'NinigiBot')", true)
             .addField("Prefix:", client.config.prefix, true)
+            .addField("Users:", client.users.size, true)
+            .addField("Servers:", client.guilds.size, true)
+            .addField("Channels:", client.channels.size, true)
+            .addField("Messages read:", globalVars.totalMessages, true)
+            .addField("Commands used:", globalVars.totalCommands, true)
+            .addField("Code:", "[Github](https://github.com/Glazelf/NinigiBot 'NinigiBot')", true)
             .addField("Language:", `Javascript`, true)
+            .addField("Uptime:", uptime)
             .addField("Contributors:", `<@${client.config.contributorZoraID}>, <@${client.config.contributorSkinnixID}>`)
             .addField("Created at:", bot.createdAt)
             .setFooter(`Requested by ${message.author.tag} at:`)
