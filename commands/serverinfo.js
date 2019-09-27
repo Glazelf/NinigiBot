3e8b0e61062780fbb7af76afc8769ea4df8730e0
Glazelf <gideon.steenbergen@gmail.com>
2019-09-27 21:59:43 +0200

    serverinfo init

---
+++ commands/serverinfo.js
@@ -0,0 +1,68 @@
+exports.run = (client, message, args) => {
+    try {
+        const Discord = require("discord.js");
+
+        function checkDays(date) {
+            let now = new Date();
+            let diff = now.getTime() - date.getTime();
+            let days = Math.floor(diff / 86400000);
+            return days + (days == 1 ? " day" : " days") + " ago";
+        };
+
+        let verifLevels = ["None", "Low", "Medium", "(╯°□°）╯︵  ┻━┻", "┻━┻ミヽ(ಠ益ಠ)ノ彡┻━┻"];
+
+        let region = {
+            "brazil": ":flag_br: Brazil",
+            "eu-central": ":flag_eu: Central Europe",
+            "singapore": ":flag_sg: Singapore",
+            "us-central": ":flag_us: U.S. Central",
+            "sydney": ":flag_au: Sydney",
+            "us-east": ":flag_us: U.S. East",
+            "us-south": ":flag_us: U.S. South",
+            "us-west": ":flag_us: U.S. West",
+            "eu-west": ":flag_eu: Western Europe",
+            "vip-us-east": ":flag_us: VIP U.S. East",
+            "london": ":flag_gb: London",
+            "amsterdam": ":flag_nl: Amsterdam",
+            "hongkong": ":flag_hk: Hong Kong",
+            "russia": ":flag_ru: Russia",
+            "southafrica": ":flag_za:  South Africa"
+        };
+
+        const profileEmbed = new Discord.RichEmbed()
+            .setColor(0x219dcd)
+            .setAuthor(message.guild.name, message.guild.iconURL)
+            .setThumbnail(message.guild.iconURL)
+            .addField("Owner:", `<@${message.guild.owner.user.id}>`, true)
+            .addField("Region:", region[message.guild.region], true)
+            .addField("Verification Level:", verifLevels[message.guild.verificationLevel], true)
+            .addField("ID:", message.guild.id, true)
+            .addField("Users:", `${message.guild.members.filter(member => !member.user.bot).size}`, true)
+            .addField("Bots:", `${message.guild.members.filter(member => member.user.bot).size}`, true)
+            .addField("Channels:", message.guild.channels.size, true)
+            .addField("Roles:", message.guild.roles.size, true)
+            .addField("Created at:", `${message.channel.guild.createdAt.toUTCString().substr(0, 16)}, ${checkDays(message.channel.guild.createdAt)}.`)
+            .setFooter(`Requested by ${message.author.tag} at:`)
+            .setTimestamp();
+
+        return message.channel.send(profileEmbed);
+
+    } catch (e) {
+        // send msg to owner
+        let members = message.channel.members;
+        let owner = members.find('id', client.config.ownerID);
+        owner.send(`> An error occurred while <@${message.member.user.id}> tried to use a command in <#${message.channel.id}>, check console for more information.`);
+
+        // log error
+        console.log(e);
+
+        // return confirmation
+        return message.channel.send(`> An error has occurred trying to run the command, <@${message.author.id}>, please use "${client.config.prefix}report" to report the issue.`);
+    };
+};
+
+module.exports.help = {
+    name: "Serverinfo",
+    description: "Returns info about the server.",
+    usage: `serverinfo`
+}; 
