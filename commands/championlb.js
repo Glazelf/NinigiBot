17be0326f5ea0e5fd6df964e7c7c164d11e66e20
Glazelf <gideon.steenbergen@gmail.com>
2019-11-29 20:41:14 +0100

    fix typo

--- commands/championlb.js
+++ commands/championlb.js
@@ -17,7 +17,7 @@ exports.run = (client, message) => {
             .setAuthor(`Battle Champion Leaderboard`, message.guild.iconURL)
             .setThumbnail("https://i.imgur.com/gpraYi7.png")
             //Cris
-            .addField(`Current <@&649737913537658931>`, `<@492847613754736650>, false`
+            .addField(`Current <@&649737913537658931>`, `<@492847613754736650>`, false)
             //Cris
             .addField(`1st place`, `<@492847613754736650> with 1 point`, false)
             //2ndplace
