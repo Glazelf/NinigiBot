96254bc9ec25a2abe8ba15d2b8eaa6087e1b6027
Glazelf <gideon.steenbergen@gmail.com>
2019-12-25 12:27:00 +0100

    message executor --> author

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -7,7 +7,7 @@ module.exports = async (client, message, oldMessage, newMessage) => {
 
     if(!log) return;
     
-    let user = entry.executor;
+    let user = entry.author;
 
     const updateEmbed = new Discord.RichEmbed()
         .setColor("#219DCD")
