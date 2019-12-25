a9fee1bf08d250b7a6c345490c943b627968b1e5
Glazelf <gideon.steenbergen@gmail.com>
2019-12-25 12:20:29 +0100

    add message edit logging

---
+++ events/messageUpdate.js
@@ -0,0 +1,23 @@
+module.exports = async (client, message, oldMessage, newMessage) => {
+    const Discord = require("discord.js");
+    const entry = await message.guild.fetchAuditLogs({ type: 'MESSAGE_UPDATE' }).then(audit => audit.entries.first());
+    const log = message.guild.channels.find(channel => channel.name === "log");
+
+    if(message.content == "") return;
+
+    if(!log) return;
+    
+    let user = entry.executor;
+
+    const updateEmbed = new Discord.RichEmbed()
+        .setColor("#219DCD")
+        .setAuthor(`Message edited ⚒️`, user.avatarURL)
+        .setDescription(`Message sent by ${message.author} edited in <#${message.channel.id}>.`)
+        .addField(`Before:`, `"${oldMessage.content}"`, false)
+        .addField(`After:`, `"${message.content}"`, false)
+        .addField(`Jump to message:`, `[Link](https://discordapp.com/channels/${message.guild.id}/${message.channel.id}/${message.id})`, false)
+        .setFooter(`Edited by ${user.tag}`)
+        .setTimestamp();
+
+    return log.send(updateEmbed);
+};
