857d2acaa824e5f767ba23fa66dbea1e366d8c46
Glazelf <gideon.steenbergen@gmail.com>
2020-02-04 23:12:33 +0100

    Log DMs

--- events/message.js
+++ events/message.js
@@ -1,4 +1,7 @@
 module.exports = (client, message) => {
+  const Discord = require("discord.js");
+  let NinigiDMChannelID = "674371091006881832";
+
   // Import totals
   let globalVars = require('./ready');
 
@@ -8,6 +11,29 @@ module.exports = (client, message) => {
   // +1 messages count
   globalVars.totalMessages += 1;
 
+  // Ignore commands in DMs
+  if (message.channel.type == "dm") {
+    if (message.content.indexOf(client.config.prefix) == 0) {
+      message.author.send(`> Sorry <@${message.author.id}>, you're not allowed to use commands in private messages!`).catch(console.error);
+    };
+    // Send message contents to dm channel
+    let DMChannel = client.channels.find('id', NinigiDMChannelID);
+
+    console.log("yest")
+
+    const dmEmbed = new Discord.RichEmbed()
+      .setColor("#219DCD")
+      .setAuthor(`DM`, message.author.avatarURL)
+      .setThumbnail(message.author.avatarURL)
+      .addField(`Author account:`, message.author, false)
+      .addField(`Author ID:`, message.author.id, false)
+      .addField(`Message content:`, message.content, false)
+      .setFooter(`DM passed through by ${client.config.botName}.`)
+      .setTimestamp();
+
+    return DMChannel.send(dmEmbed);
+  };
+
   // Ignore messages not starting with the prefix
   if (message.content.indexOf(client.config.prefix) !== 0) return;
 
@@ -16,12 +42,7 @@ module.exports = (client, message) => {
 
   // Ignore messages that start with prefix double or prefix space
   let secondCharacter = message.content.charAt(1);
-  if (secondCharacter == `${client.config.prefix}` || secondCharacter == ` `) return;
-
-  // Ignore commands in DMs
-  if (message.channel.type == "dm") {
-    return message.author.send(`> Sorry <@${message.author.id}>, you're not allowed to use commands in private messages!`).catch(console.error);
-  };
+  if (secondCharacter == client.config.prefix || secondCharacter == ` `) return;
 
   // Standard definition
   const args = message.content.slice(client.config.prefix.length).trim().split(/ +/g);
