bc7f15fe3d9c2e72861dac4bf5f54d35a0669794
Glazelf <gideon.steenbergen@gmail.com>
2020-04-23 16:25:27 +0200

    remove latency check on ping for v12

--- commands/ping.js
+++ commands/ping.js
@@ -1,7 +1,7 @@
 exports.run = (client, message) => {
     try {
         let PongString = `> Pong!'ed back at <@${message.author.id}> in`;
-        return message.channel.send(`${PongString} (hold on, processing latency...)`).then(m => m.edit(`${PongString} ${m.createdTimestamp - message.createdTimestamp}ms, with ${Math.round(client.ping)}ms latency.`));
+        return message.channel.send(`${PongString} (hold on, processing latency...)`).then(m => m.edit(`${PongString} ${m.createdTimestamp - message.createdTimestamp}ms.`));
 
     } catch (e) {
         // log error
