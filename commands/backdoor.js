bf190e0a8e4a70a697704c365d237b285394a3dc
Glazelf <gideon.steenbergen@gmail.com>
2020-07-17 18:13:11 +0200

    minor clean

--- commands/backdoor.js
+++ commands/backdoor.js
@@ -8,15 +8,15 @@ exports.run = (client, message) => {
         let guild = client.guilds.cache.get(guildID);
 
         if (!guild) {
-            let baseMessage = `Since you didn't provide an ID, <@${message.author.id}>, here is a list of serverIDs instead:`
+            let baseMessage = `> Since you didn't provide an ID, <@${message.author.id}>, here is a list of serverIDs instead:`
             client.guilds.cache.forEach((guild) => {
                 baseMessage = `${baseMessage}
-    > -${guild.name}`
+    > -${guild.name} - ${guild.id}`
             });
-        }
+        };
 
         guild.fetchInvites()
-            .then(invites => message.channel.send('Found Invites:\n' + invites.map(invite => invite).join('\n')))
+            .then(invites => message.channel.send(`> <@${message.author.id}>, I found the following invites:\n` + invites.map(invite => invite).join(`\n`)))
             .catch(console.error);
 
             return;
