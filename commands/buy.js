1761f048653667496c0ef65b54b56b32b84abc36
Glazelf <gideon.steenbergen@gmail.com>
2020-08-10 17:46:02 +0200

    minor clean

--- commands/buy.js
+++ commands/buy.js
@@ -6,16 +6,16 @@ exports.run = async (client, message) => {
         const input = message.content.slice(1).trim();
         const [, , commandArgs] = input.match(/(\w+)\s*([\s\S]*)/);
         const item = await CurrencyShop.findOne({ where: { name: { [Op.like]: commandArgs } } });
-        if (!item) return message.channel.send('That item doesn\'t exist.');
+        if (!item) return message.channel.send(`> That item doesn't exist, ${message.author}.`);
         if (item.cost > bank.currency.getBalance(message.author.id)) {
-            return message.channel.send(`You don't have enough currency, ${message.author}`);
+            return message.channel.send(`> You don't have enough currency, ${message.author}, you only have ${Math.floor(bank.currency.getBalance(userCache.id))}💰.`);
         }
 
         const user = await Users.findOne({ where: { user_id: message.author.id } });
         bank.currency.add(message.author.id, -item.cost);
         await user.addItem(item);
 
-        return message.channel.send(`You've bought a ${item.name}, ${message.author}.`);
+        return message.channel.send(`> You've bought a ${item.name}, ${message.author}.`);
 
     } catch (e) {
         // log error
