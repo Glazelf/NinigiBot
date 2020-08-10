06542e73c58a4f45eb6d0a49eb442267057f0265
Glazelf <gideon.steenbergen@gmail.com>
2020-08-10 18:06:46 +0200

    sort items in price

--- storeInit.js
+++ storeInit.js
@@ -17,17 +17,17 @@ const force = process.argv.includes('--force') || process.argv.includes('-f');
 
 sequelize.sync({ force }).then(async () => {
 	const shop = [
+		CurrencyShop.upsert({ name: 'Water', cost: 10 }),
 		CurrencyShop.upsert({ name: 'Juice', cost: 20}),
 		CurrencyShop.upsert({ name: 'Pokéball', cost: 50 }),
-		CurrencyShop.upsert({ name: 'Water', cost: 10 }),
-		CurrencyShop.upsert({ name: 'Keysss', cost: 200 }),
 		CurrencyShop.upsert({ name: 'Chocolate', cost: 75 }),
+		CurrencyShop.upsert({ name: 'Keysss', cost: 200 }),
 		CurrencyShop.upsert({ name: 'Shinx shirt', cost: 500 }),
 		CurrencyShop.upsert({ name: 'Gilgamesh nudes', cost: 750 }),
 		CurrencyShop.upsert({ name: 'Konohana figure', cost: 1000}),
+		CurrencyShop.upsert({ name: 'Gold dango', cost: 1200 }),
 		CurrencyShop.upsert({ name: 'Geass', cost: 1500 }),
 		CurrencyShop.upsert({ name: 'C.C. figure', cost: 2000 }),
-		CurrencyShop.upsert({ name: 'Gold dango', cost: 1200 }),
 		CurrencyShop.upsert({ name: 'Skaidus poster', cost: 2500 }),
 		CurrencyShop.upsert({ name: 'Skinnix dakimakura', cost: 5000 }),
 	];
