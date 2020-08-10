b9580841842eb2b0e8be3d7ad39e5618c57a8e2c
Glazelf <gideon.steenbergen@gmail.com>
2020-08-10 18:05:30 +0200

    add item name and cost into buy error

--- commands/buy.js
+++ commands/buy.js
@@ -8,8 +8,8 @@ exports.run = async (client, message) => {
         const item = await CurrencyShop.findOne({ where: { name: { [Op.like]: commandArgs } } });
         if (!item) return message.channel.send(`> That item doesn't exist, ${message.author}.`);
         if (item.cost > bank.currency.getBalance(message.author.id)) {
-            return message.channel.send(`> You don't have enough currency, ${message.author}, you only have ${Math.floor(bank.currency.getBalance(message.author.id))}💰.`);
-        }
+            return message.channel.send(`> You don't have enough currency, ${message.author}, you only have ${Math.floor(bank.currency.getBalance(message.author.id))}💰 whereas the ${input} costs ${item.cost}💰.`);
+        };
 
         const user = await Users.findOne({ where: { user_id: message.author.id } });
         bank.currency.add(message.author.id, -item.cost);
