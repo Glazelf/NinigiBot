446dc3462d5bff1fc03e6083f30408baf5143bd4
Glazelf <gideon.steenbergen@gmail.com>
2020-08-11 13:16:34 +0200

    slight string cleaning

--- commands/use.js
+++ commands/use.js
@@ -5,13 +5,14 @@ exports.run = async (client, message) => {
         const target = message.mentions.users.first() || message.author;
         const userDB = await Users.findOne({ where: { user_id: target.id } });
         const items = await userDB.getItems();
-        if(!items) return message.channel.send('You don\'t have any items to use');
+        if(!items) return message.channel.send(`You don't have any items to use`);
 
         const input = message.content.slice(1).trim();
         const [, , commandArgs] = input.match(/(\w+)\s*([\s\S]*)/);
         const item = items.filter(i => i.item.name.toLowerCase() === commandArgs.toLowerCase());
-        if(item.length<1) return message.channel.send('You don\'t have that item.');
+        if(item.length<1) return message.channel.send(`You don't have that item.`);
         return message.channel.send(item[0].item.use);
+        
     } catch (e) {
         // log error
         console.log(e);
