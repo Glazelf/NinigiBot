3ba5c78a4ebe621e8831ac3beb0b4f3d2ef13eec
Skaidus <aitor.izuzquiza@gmail.com>
2020-08-11 17:04:31 +0200

    Commands to change and get bio and FC

--- bank.js
+++ bank.js
@@ -6,6 +6,46 @@ module.exports = {
         get currency() {
             if (!this._currency) {
                 const money = new Discord.Collection();
+                Reflect.defineProperty(money, 'switchCode', {
+                    value: async function switchCode(id, code) {
+                        const user = money.get(id);
+                        if (user) {
+                            user.swcode = code
+                            return user.save();
+                        }
+                        const newUser = await Users.create({ user_id: id, swcode:code });
+                        money.set(id, newUser);
+                        return newUser;
+                    }
+                });
+
+                Reflect.defineProperty(money, 'getSwitchCode', {
+                    value: function getSwitchCode(id) {
+                        const user = money.get(id);
+                        return user ? user.swcode : 'None';
+                    },
+                });
+
+                Reflect.defineProperty(money, 'biography', {
+                    value: async function biography(id, text) {
+                        const user = money.get(id);
+                        if (user) {
+                            user.biography = text;
+                            return user.save();
+                        }
+                        const newUser = await Users.create({ user_id: id, biograhy: text});
+                        money.set(id, newUser);
+                        return newUser;
+                    }
+                });
+
+                Reflect.defineProperty(money, 'getBiography', {
+                    value: function getBiography(id) {
+                        const user = money.get(id);
+                        return user ? user.biography : 'None';
+                    },
+                });
+
                 Reflect.defineProperty(money, 'add', {
                     value: async function add(id, amount) {
                         const user = money.get(id);
