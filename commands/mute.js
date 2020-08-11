f8ea34429c5831f4f6b5d9a67b60d3e5957cc273
Glazelf <gideon.steenbergen@gmail.com>
2020-08-11 13:56:24 +0200

    allow an amunt of minutes to be specified

--- commands/mute.js
+++ commands/mute.js
@@ -1,16 +1,24 @@
 module.exports.run = async (client, message, args) => {
   try {
     //Second the user is muted
-    const seconds = 10;
+    let muteTime = 60;
+    let split = message.content.split(` `, 3);
+
+    if (split[2]) {
+      muteTime = split[2];
+      if (isNaN(muteTime) || 1 > muteTime) return message.channel.send(`> Please provide a valid number, ${message.author}.`)
+    };
 
     if (args[0]) {
       const member = message.mentions.members.first();
-      if (!member) return message.reply(`> Please use a proper mention if you want to mute someone, <@${message.author.id}.`);
-      const role= member.guild.roles.cache.find(role => role.name === "Muted");
+      if (!member) return message.reply(`> Please use a proper mention if you want to mute someone, ${message.author}.`);
+      const role = member.guild.roles.cache.find(role => role.name === "Muted");
       await member.roles.add(role);
 
+      message.channel.send(`> ${member} has been muted for ${muteTime} minute(s), ${message.author}.`)
+
       // sets a timeout to unmute the user.
-      setTimeout(async () => { await member.roles.remove(role) }, seconds * 1000);
+      setTimeout(async () => { await member.roles.remove(role) }, muteTime * 60 * 1000);
     };
 
   } catch (e) {
