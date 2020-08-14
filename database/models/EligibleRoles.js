8e3734f796aacda645259472fa85e1db5ec340bd
Skaidus <aitor.izuzquiza@gmail.com>
2020-08-14 17:11:23 +0200

    Changed the model a bit

---
+++ database/models/EligibleRoles.js
@@ -0,0 +1,14 @@
+module.exports = (sequelize, DataTypes) => {
+	return sequelize.define('eligible_roles', {
+		role_id : {
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		name : {
+			type: DataTypes.STRING,
+			allowNull: false
+		}
+	}, {
+		timestamps: false,
+	});
+};
