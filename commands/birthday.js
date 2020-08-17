670c80988573c6544d195075940bc777ebe9d3a6
Skaidus <aitor.izuzquiza@gmail.com>
2020-08-17 13:06:23 +0200

    Fixed regex

--- commands/birthday.js
+++ commands/birthday.js
@@ -7,7 +7,8 @@ module.exports.run = async (client, message) => {
         const input = message.content.slice(1).trim();
         const [, , arguments] = input.match(/(\w+)\s*([\s\S]*)/);
         if (arguments.length<1) return message.channel.send(`> Please specify a valid birthday on dd-mm format, ${message.author}.`);
-        let birthday = /^(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])(?:[- /.](19|20)\d\d)$/.exec(arguments);
+        let birthday = /^(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])/.exec(arguments);
+
         if (!birthday) return message.channel.send(`> Please specify a valid birthday on dd-mm format, ${message.author}.`);
         bank.currency.birthday(message.author.id, birthday[1]+birthday[2]);
         return message.channel.send(`> Successfully updated your birthday, ${message.author}.`)
