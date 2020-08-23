ec3a6944a64ccc575fbd269796b06aadc6e4d126
Skaidus <aitor.izuzquiza@gmail.com>
2020-08-23 14:30:01 +0200

    Roulette beta

--- events/ready.js
+++ events/ready.js
@@ -1,8 +1,8 @@
 module.exports = async (client) => {
   try {
-    const stan = require('../daily/stan')(client);
-    const birthday = require('../daily/birthday')(client);
-    const lotery = require('../daily/lottery')(client);
+    const stan = require('../affairs/stan')(client);
+    const birthday = require('../affairs/birthday')(client);
+    const lotery = require('../affairs/lottery')(client);
     const { bank } = require('../database/bank');
     const { Users } = require('../database/dbObjects');
     const storedBalances = await Users.findAll();
