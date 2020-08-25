fe0d70fd1afb0a9578b0cb45b1240a3198cea369
Skaidus <aitor.izuzquiza@gmail.com>
2020-08-25 17:32:02 +0200

    Added OOP methods to shinx

--- database/dbObjects.js
+++ database/dbObjects.js
@@ -8,10 +8,41 @@ const sequelize = new Sequelize('database', 'username', 'password', {
 });
 
 const Users = require('./models/Users')(sequelize, Sequelize.DataTypes);
+const Shinx = require('./models/Shinx')(sequelize, Sequelize.DataTypes)
 const CurrencyShop = require('./models/CurrencyShop')(sequelize, Sequelize.DataTypes);
 const UserItems = require('./models/UserItems')(sequelize, Sequelize.DataTypes);
 const EligibleRoles = require('./models/EligibleRoles')(sequelize, Sequelize.DataTypes);
 UserItems.belongsTo(CurrencyShop, { foreignKey: 'item_id', as: 'item' });
+
+Shinx.prototype.levelUp = function (experience) {
+	this.level += experience;
+	this.save();
+	return this.level;
+}
+
+Shinx.prototype.changeNick = function (newNick) {
+	this.nick = newNick;
+	this.save();
+	return this.nick;
+}
+
+Shinx.prototype.sleeping = function (sleep) {
+	this.sleep = Math.max(Math.min(100, this.sleep + sleep), 0);
+	this.save();
+	return this.sleep;
+}
+
+Shinx.prototype.eat = function (food) {
+	this.hunger = Math.max(Math.min(100, this.hunger + food), 0);
+	this.save();
+	return this.hunger;
+}
+
+Shinx.prototype.shine = function () {
+	this.shiny = !this.shiny
+	this.save();
+	return this.shiny;
+}
 
 Users.prototype.addItem = async function (item) {
 	const useritem = await UserItems.findOne({
@@ -50,4 +81,4 @@ Users.prototype.getItems = function () {
 	});
 };
 
-module.exports = { Users, CurrencyShop, UserItems, EligibleRoles };
+module.exports = { Users, CurrencyShop, UserItems, EligibleRoles, Shinx };
