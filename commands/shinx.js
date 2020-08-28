bb525db21f8cf5e0dddea70d826535b22078f56b
Skaidus <aitor.izuzquiza@gmail.com>
2020-08-29 01:44:46 +0200

    Experimental shinx data command

--- commands/shinx.js
+++ commands/shinx.js
@@ -1,4 +1,5 @@
 const UserItems = require('../database/models/UserItems');
+const Canvas = require('canvas');
 
 module.exports.run = async (client, message) => {
     // Import globals
@@ -10,26 +11,33 @@ module.exports.run = async (client, message) => {
         const [, , biography] = input.match(/(\w+)\s*([\s\S]*)/);
 
         const shinx = await bank.currency.getShinx(message.author.id)
-        console.log(biography)
         let userCache = client.users.cache.get(message.author.id);
         if(biography==='shiny') return shinx.shine()? message.channel.send('Now your shinx shines!'):message.channel.send('Your shinx doesn\'t shine anymmore!')
         if(biography==='nick') return shinx.changeNick('Glaze')
         if(biography==='level') return message.channel.send(`Shinx levels up to level ${shinx.levelUp(1)}`); 
         let avatar = null;
-        console.log(shinx)
+        const canvas = Canvas.createCanvas(791, 541);
+        const ctx = canvas.getContext('2d');
+        const background = await Canvas.loadImage('./assets/data.png');
+        ctx.drawImage(background, 0, 0);
+        if(shinx.shiny){
+            const cap = await Canvas.loadImage('./assets/shiny.png');
+            ctx.drawImage(cap, 98, 206);
+        }
+        ctx.font = 'normal bolder 45px Arial';
+        ctx.fillStyle = '#FFFFFF';
+        ctx.fillText(shinx.nick, 88, 133);
+        ctx.font = 'normal bolder 35px Arial';
+        ctx.fillStyle = '#0073FF'
+        ctx.fillText(message.author.username, 490, 190);
+        ctx.fillStyle = '#000000'
+        ctx.fillText(shinx.level, 93, 191);
+        ctx.fillText(Math.floor(shinx.hunger*100)+'%', 490, 251);
+        ctx.fillText(Math.floor(shinx.sleep*100)+'%', 490, 310);
+        ctx.fillText(Math.floor(shinx.friendship*100)+'%', 490, 364);
+        ctx.fillText(shinx.meetup, 490, 481);
         if (userCache.avatarURL()) avatar = userCache.avatarURL({ format: "png", dynamic: true });
-        const shinxData = new Discord.MessageEmbed()
-            .setColor(globalVars.embedColor)
-            .setTitle('Shinx data')
-            .setAuthor(userCache.username, avatar)
-            .addField("Nick:", shinx.nick, true)
-            .addField("Level:", shinx.level, true)
-            .addField("Hunger:", shinx.hunger+'%', true)
-            .addField("Sleep:", shinx.sleep+'%', true)
-            .addField("Friendship:", shinx.friendship+'%', true)
-            console.log(shinx.shiny)
-            shinx.shiny? shinxData.setImage('https://media.redadn.es/imagenes/pokemon-go-android-ios_323027.jpg'): shinxData.setImage('https://img.pngio.com/others-png-download-1115985-free-transparent-pokemon-x-and-y-shinx-png-900_800.jpg')
-            .setTimestamp();
+        const shinxData = new Discord.MessageAttachment(canvas.toBuffer(), 'welcome-image.png');
 
         return message.channel.send(shinxData);
     } catch (e) {
