f8d8f1e577da964e674e51acbbdf7821e3b39c95
Skaidus <aitor.izuzquiza@gmail.com>
2020-09-03 00:46:11 +0200

    New buy command

--- commands/buy.js
+++ commands/buy.js
@@ -3,24 +3,45 @@ exports.run = async (client, message) => {
     let globalVars = require('../events/ready');
     try {
         const { bank } = require('../database/bank');
-        const { Users, CurrencyShop } = require('../database/dbObjects');
+        const { Users, Equipments, Foods, KeyItems, Room, BattleItems } = require('../database/dbObjects');
         const { Op } = require('sequelize');
-
+        const shops = [Equipments, Foods, KeyItems, Room, BattleItems]
         const input = message.content.slice(1).trim();
         const [, , commandArgs] = input.match(/(\w+)\s*([\s\S]*)/);
+        for(let i=0; i < shops.length; i++){
+            const item = await shops[i].findOne({ where: { name: { [Op.like]: commandArgs } } });
+            if (item) {
+                if (item.cost > bank.currency.getBalance(message.author.id)) {
+                    return message.channel.send(`> You don't have enough currency, ${message.author}.
+                    > The ${item.name} costs ${item.cost}💰 but you only have ${Math.floor(bank.currency.getBalance(message.author.id))}💰.`);
+                };
+                const user = await Users.findOne({ where: { user_id: message.author.id } });
+                
+                bank.currency.add(message.author.id, -item.cost);
+                if(i===0){
+                    await user.addEquipment(item);
 
-        const item = await CurrencyShop.findOne({ where: { name: { [Op.like]: commandArgs } } });
-        if (!item) return message.channel.send(`> That item doesn't exist, ${message.author}.`);
-        if (item.cost > bank.currency.getBalance(message.author.id)) {
-            return message.channel.send(`> You don't have enough currency, ${message.author}.
-> The ${item.name} costs ${item.cost}💰 but you only have ${Math.floor(bank.currency.getBalance(message.author.id))}💰.`);
-        };
+                }else if(i===1){
+                    await user.addFood(item);
+                    
+                }else if(i===2){
+                    await user.addKey(item);
+                    
+                }else if(i===3){
+                    await user.changeRoom(item);
+                }else{
+                    await user.addItem(item);
+                }
+                
+                return message.channel.send(`> You've bought a ${item.name}, ${message.author}.`);    
 
-        const user = await Users.findOne({ where: { user_id: message.author.id } });
-        bank.currency.add(message.author.id, -item.cost);
-        await user.addItem(item);
+            }
+        }
+        return message.channel.send(`> That item doesn't exist, ${message.author}.`);
+        
 
-        return message.channel.send(`> You've bought a ${item.name}, ${message.author}.`);
+        
+        
 
     } catch (e) {
         // log error
