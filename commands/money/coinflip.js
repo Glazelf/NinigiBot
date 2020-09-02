8148d45d3af1a9434641b665f53a28c54bd987d4
Glazelf <gideon.steenbergen@gmail.com>
2020-09-02 15:57:25 +0200

    implement coinflip half

--- commands/money/coinflip.js
+++ commands/money/coinflip.js
@@ -5,6 +5,7 @@ exports.run = (client, message) => {
         const { bank } = require('../../database/bank');
         const input = message.content.split(` `, 2);
         if (input[1] == "all") input[1] = bank.currency.getBalance(message.author.id);
+        if (input[1] == "half") input[1] = bank.currency.getBalance(message.author.id)/2;
         let amount = input[1];
 
         if (!amount || isNaN(amount)) return message.channel.send(`> You need to specify a valid number to gamble, ${message.author}.`);
