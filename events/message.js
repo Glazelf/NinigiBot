5da3ec2a68e3a04b01cbf7df2c4eff13e7b11311
Glazelf <gideon.steenbergen@gmail.com>
2020-09-02 15:57:17 +0200

    fix client user call

--- events/message.js
+++ events/message.js
@@ -61,7 +61,7 @@ ${Attachment.url}`;
         .addField(`Author ID:`, message.author.id, false)
         .addField(`Message content:`, message.content, false)
         .setImage(messageImage)
-        .setFooter(`DM passed through by ${client.config.botName}.`)
+        .setFooter(`DM passed through by ${client.user.tag}.`)
         .setTimestamp();
 
       return DMChannel.send(dmEmbed);
