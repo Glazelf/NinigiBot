0d9e66f1fd4d1dd0d6026fd697654879e6bd423f
Skaidus <aitor.izuzquiza@gmail.com>
2020-09-04 01:39:20 +0200

    Shop displays info if needed

--- commands/shop.js
+++ commands/shop.js
@@ -1,3 +1,5 @@
+const Sequelize = require('sequelize');
+const {ne} = Sequelize.Op;
 exports.run = async (client, message) => {
     // Import globals
     let globalVars = require('../events/ready');
@@ -5,23 +7,24 @@ exports.run = async (client, message) => {
         const {  Equipments, Foods, KeyItems, Room, BattleItems } = require('../database/dbObjects');
         const input = message.content.slice(1).trim();
         const [, , biography] = input.match(/(\w+)\s*([\s\S]*)/);
+        const condition = {where:{cost:{[ne]:0}}}
         if(biography === 'items'){
-            const items = await BattleItems.findAll();
-            return message.channel.send(items.map(i => `${i.name}: ${i.cost}💰`).join('\n'), { code: true });
+            const items = await BattleItems.findAll(condition);
+            return message.channel.send(items.map(i => i.toString()).join('\n'), { code: true });
         }if(biography === 'equipment'){
-            const items = await Equipments.findAll();
-            return message.channel.send(items.map(i => `${i.name}: ${i.cost}💰`).join('\n'), { code: true });
+            const items = await Equipments.findAll(condition);
+            return message.channel.send(items.map(i => i.toString()).join('\n'), { code: true });
         }if(biography === 'food'){
-            const items = await Foods.findAll();
-            return message.channel.send(items.map(i => `${i.name}: ${i.cost}💰`).join('\n'), { code: true });
+            const items = await Foods.findAll(condition);
+            return message.channel.send(items.map(i => i.toString()).join('\n'), { code: true });
         }if(biography === 'key'){
-            const items = await KeyItems.findAll();
-            return message.channel.send(items.map(i => `${i.name}: ${i.cost}💰`).join('\n'), { code: true });
-        }if(biography === 'rooms'){
-            const items = await Room.findAll();
-            return message.channel.send(items.map(i => `${i.name}: ${i.cost}💰`).join('\n'), { code: true });
-        }
-        return message.channel.send('That is not a correct shop. Please use `?shop` followed by one of the available shops: items, equipment, food, key, rooms');
+            const items = await KeyItems.findAll(condition);
+            return message.channel.send(items.map(i => i.toString()).join('\n'), { code: true });
+        }/* if(biography === 'rooms'){
+            const items = await Room.findAll(condition);
+            return message.channel.send(items.map(i => i.toString()).join('\n'), { code: true });
+        } */
+        return message.channel.send('That is not a correct shop. Please use `?shop` followed by one of the available shops: items, equipment, food, key');
     } catch (e) {
         // log error
         console.log(e);
