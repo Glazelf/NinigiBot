4696378b4cbbc0bfeed6ea845f0d44b1559feb3a
Skaidus <aitor.izuzquiza@gmail.com>
2020-09-04 01:39:09 +0200

    Buy command adapted

--- commands/buy.js
+++ commands/buy.js
@@ -5,12 +5,13 @@ exports.run = async (client, message) => {
         const { bank } = require('../database/bank');
         const { Users, Equipments, Foods, KeyItems, Room, BattleItems } = require('../database/dbObjects');
         const { Op } = require('sequelize');
-        const shops = [Equipments, Foods, KeyItems, Room, BattleItems]
+        const shops = [Equipments, Foods, KeyItems, BattleItems, Room]
         const input = message.content.slice(1).trim();
         const [, , commandArgs] = input.match(/(\w+)\s*([\s\S]*)/);
         for(let i=0; i < shops.length; i++){
             const item = await shops[i].findOne({ where: { name: { [Op.like]: commandArgs } } });
             if (item) {
+                if(item.cost===0) return message.channel.send(`> That item doesn't exist, ${message.author}.`);
                 if (item.cost > bank.currency.getBalance(message.author.id)) {
                     return message.channel.send(`> You don't have enough currency, ${message.author}.
                     > The ${item.name} costs ${item.cost}💰 but you only have ${Math.floor(bank.currency.getBalance(message.author.id))}💰.`);
@@ -28,10 +29,11 @@ exports.run = async (client, message) => {
                     await user.addKey(item);
                     
                 }else if(i===3){
-                    await user.changeRoom(item);
-                }else{
                     await user.addItem(item);
-                }
+                }/* else{
+                    await user.changeRoom(item);
+                    
+                } */
                 
                 return message.channel.send(`> You've bought a ${item.name}, ${message.author}.`);    
 
