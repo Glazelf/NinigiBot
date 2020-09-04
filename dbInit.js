9d272ff5e494a3a6701247a07d91b9655c219b1d
Skaidus <aitor.izuzquiza@gmail.com>
2020-09-04 18:27:52 +0200

    Added default items for shops

--- dbInit.js
+++ dbInit.js
@@ -13,14 +13,14 @@ const UserItems = require('./database/models/UserItems')(sequelize, Sequelize.Da
 const UserFoods = require('./database/models/UserFoods')(sequelize, Sequelize.DataTypes);
 const UserEquipments = require('./database/models/UserEquipments')(sequelize, Sequelize.DataTypes);
 const UserKeys = require('./database/models/UserKeys')(sequelize, Sequelize.DataTypes);
-const UserRooms = require('./database/models/UserRooms')(sequelize, Sequelize.DataTypes);
+//const UserRooms = require('./database/models/UserRooms')(sequelize, Sequelize.DataTypes);
 
 const EligibleRoles = require('./database/models/EligibleRoles')(sequelize, Sequelize.DataTypes);
-const BattleItems = require('./database/models/BattleItems')(sequelize, Sequelize.DataTypes);
+const CurrencyShop = require('./database/models/CurrencyShop')(sequelize, Sequelize.DataTypes);
 const Equipments = require('./database/models/Equipments')(sequelize, Sequelize.DataTypes);
 const Foods = require('./database/models/Foods')(sequelize, Sequelize.DataTypes);
 const KeyItems = require('./database/models/KeyItems')(sequelize, Sequelize.DataTypes);
-const Room  = require('./database/models/Room')(sequelize, Sequelize.DataTypes);
+//const Room  = require('./database/models/Room')(sequelize, Sequelize.DataTypes);
 const Shinx = require('./database/models/Shinx')(sequelize, Sequelize.DataTypes);
 
 const syncDatabase = async() => {
@@ -29,29 +29,37 @@ const syncDatabase = async() => {
 
 		await EligibleRoles.sync({ alter: true });
 		
-		await UserItems.sync({ alter: true });
+		await UserItems.sync({ force: true });
 		await UserFoods.sync({ alter: true });
 		await UserEquipments.sync({ alter: true });
 		await UserKeys.sync({ alter: true });
-		await UserRooms.sync({ alter: true });
+		//await UserRooms.sync({ alter: true });
 
 		await Shinx.sync({alter:true})
-		await BattleItems.sync({ force: true })
+		await CurrencyShop.sync({force: true})
 		await Equipments.sync({ force: true })
 		await Foods.sync({ force: true })
 		await KeyItems.sync({ force: true })
-		await Room.sync({ force: true })
+		//await Room.sync({ force: true })
 		const shop = [
-			BattleItems.upsert({ name: 'potion', cost: 10, percentage:0.2 }),
-			BattleItems.upsert({ name: 'saiyan berry', cost: 50, saiyan: true  }),
+			CurrencyShop.upsert({ name: 'Lottery ticket', cost: 50, usage: 'allows to participate on Mondays weekly lottery' }),
 	
-			Equipments.upsert({ name: 'sun glasses', cost: 50, saiyan: true, guard: true  }),
+			Equipments.upsert({ name: 'Geass Orb', cost: 2500, geass: true }),
+			Equipments.upsert({ name: 'Choice Band', cost: 1500, food: 0.5, sleep: -0.1 }),
+			Equipments.upsert({ name: 'Focus Sash', cost: 1500, guard:true }),
+			Equipments.upsert({ name: 'Leftovers', cost: 1500, regen:0.2 }),
+			Equipments.upsert({ name: 'Life Orb', cost: 1500, regen:-0.1, food: 0.3  }),
 			
-			Foods.upsert({ name: 'Fried seedot', cost: 40, recovery: 1  }),
+			Foods.upsert({ name: 'Fried seedot', cost: 80, recovery: 0.2  }),
+			Foods.upsert({ name: 'Sweet pokeblock', cost: 150, recovery: 0.4  }),
+			Foods.upsert({ name: 'Purple poffin', cost: 220, recovery: 0.6  }),
+			Foods.upsert({ name: 'Chocolate dango', cost: 360, recovery: 1  }),
 	
-			KeyItems.upsert({ name: 'Skaidus poster', cost: 999999 }),
+			/* KeyItems.upsert({ name: 'Skaidus poster', cost: 999999 }),
+			KeyItems.upsert({ name: 'Glazes dick', cost: 0 }),
+			KeyItems.upsert({ name: 'Shiny charm', cost: 0 }) */
 	
-			Room.upsert({ name: 'Chalice chamber', cost: 999999, slots: 5 })
+			//Room.upsert({ name: 'Chalice chamber', cost: 999999, slots: 5 })
 		]
 		await Promise.all(shop);
 		console.log('Store updated');
@@ -63,18 +71,3 @@ const syncDatabase = async() => {
 }
 
 syncDatabase(); 
-//Run force : true ONLY IF YOU WANT TO RESET THE SAVED ITEMS
-
-/* Users.sync({ alter: true });
-EligibleRoles.sync({ alter: true });
-UserItems.sync({ alter: true });
-Shinx.sync({alter:true})
-BattleItems.sync({ force: true }).then(async () => {
-	const shop = [
-		BattleItems.upsert({ name: 'potion', cost: 10, percentage:0.2 }),
-		BattleItems.upsert({ name: 'saiyan berry', cost: 50, saiyan: true  }),
-
-
-			];
-	
-}).catch(console.error); */
