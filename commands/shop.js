d89bb668b531e9a38d001d0e1efd1d642a480ae6
Skaidus <aitor.izuzquiza@gmail.com>
2020-09-04 18:37:10 +0200

    Changed to currencyshop again

--- commands/shop.js
+++ commands/shop.js
@@ -4,12 +4,12 @@ exports.run = async (client, message) => {
     // Import globals
     let globalVars = require('../events/ready');
     try {
-        const {  Equipments, Foods, KeyItems, Room, BattleItems } = require('../database/dbObjects');
+        const {  Equipments, Foods, KeyItems, Room, CurrencyShop } = require('../database/dbObjects');
         const input = message.content.slice(1).trim();
         const [, , biography] = input.match(/(\w+)\s*([\s\S]*)/);
         const condition = {where:{cost:{[ne]:0}}}
         if(biography === 'items'){
-            const items = await BattleItems.findAll(condition);
+            const items = await CurrencyShop.findAll(condition);
             return message.channel.send(items.map(i => i.toString()).join('\n'), { code: true });
         }if(biography === 'equipment'){
             const items = await Equipments.findAll(condition);
@@ -17,14 +17,14 @@ exports.run = async (client, message) => {
         }if(biography === 'food'){
             const items = await Foods.findAll(condition);
             return message.channel.send(items.map(i => i.toString()).join('\n'), { code: true });
-        }if(biography === 'key'){
+        }/* if(biography === 'key'){
             const items = await KeyItems.findAll(condition);
             return message.channel.send(items.map(i => i.toString()).join('\n'), { code: true });
-        }/* if(biography === 'rooms'){
+        } *//* if(biography === 'rooms'){
             const items = await Room.findAll(condition);
             return message.channel.send(items.map(i => i.toString()).join('\n'), { code: true });
         } */
-        return message.channel.send('That is not a correct shop. Please use `?shop` followed by one of the available shops: items, equipment, food, key');
+        return message.channel.send('That is not a correct shop. Please use `?shop` followed by one of the available shops: items, equipment, food');
     } catch (e) {
         // log error
         console.log(e);
