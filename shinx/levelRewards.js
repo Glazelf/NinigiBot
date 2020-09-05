aad1aebbabc5058bb82277f91688c360a1aa5ac3
Glazelf <gideon.steenbergen@gmail.com>
2020-09-05 15:32:13 +0200

    minor clean

--- shinx/levelRewards.js
+++ shinx/levelRewards.js
@@ -5,29 +5,29 @@ let rewards = new Map([
     [100, 'Shiny charm'],
 ]);
 
-const { Users, Equipments, Foods, KeyItems, Room} = require('../database/dbObjects');
-const shops = [Equipments, Foods, KeyItems, Room]
+const { Users, Equipments, Foods, KeyItems, Room } = require('../database/dbObjects');
+const shops = [Equipments, Foods, KeyItems, Room];
 
 module.exports = async (shinxBattle) => {
-    const reward = rewards.get(shinxBattle.level)
-    if(!reward) return
-    for(let i=0; i < shops.length; i++){
+    const reward = rewards.get(shinxBattle.level);
+    if (!reward) return;
+    for (let i = 0; i < shops.length; i++) {
         const item = await shops[i].findOne({ where: { name: reward } });
         if (item) {
             const user = await Users.findOne({ where: { user_id: shinxBattle.owner.id } });
-            if(i===0){
+            if (i === 0) {
                 await user.addEquipment(item);
-                return ['equipment', reward]
-            }else if(i===1){
+                return ['equipment', reward];
+            } else if (i === 1) {
                 await user.addFood(item);
-                return ['food', reward]
-            }else if(i===2){
+                return ['food', reward];
+            } else if (i === 2) {
                 await user.addKey(item);
-                return ['key item', reward]
-            }else if(i===3){
+                return ['key item', reward];
+            } else if (i === 3) {
                 await user.changeRoom(item);
-                return ['room', reward]
-            }
-        }
-    }   
+                return ['room', reward];
+            };
+        };
+    };
 };
