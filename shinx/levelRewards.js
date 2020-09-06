b623cde7952c3b39fd5ea8eb4ede398c1fdc99f0
Skaidus <aitor.izuzquiza@gmail.com>
2020-09-06 16:58:59 +0200

    Now checks in all the leveled up levels

--- shinx/levelRewards.js
+++ shinx/levelRewards.js
@@ -8,26 +8,31 @@ let rewards = new Map([
 const { Users, Equipments, Foods, KeyItems, Room } = require('../database/dbObjects');
 const shops = [Equipments, Foods, KeyItems];
 
-module.exports = async (shinxBattle) => {
-    const reward = rewards.get(shinxBattle.level);
-    if (!reward) return;
-    for (let i = 0; i < shops.length; i++) {
-        const item = await shops[i].findOne({ where: { name: reward } });
-        if (item) {
-            const user = await Users.findOne({ where: { user_id: shinxBattle.owner.id } });
-            if (i === 0) {
-                await user.addEquipment(item);
-                return ['equipment', reward];
-            } else if (i === 1) {
-                await user.addFood(item);
-                return ['food', reward];
-            } else if (i === 2) {
-                await user.addKey(item);
-                return ['key item', reward];
-            } else if (i === 3) {
-                await user.changeRoom(item);
-                return ['room', reward];
+module.exports = async (shinxBattle, increasedLevels) => {
+    let userRewards = []
+    for(let i = shinxBattle.level-increasedLevels; i <=shinxBattle.level;i++){
+        const reward = rewards.get(i);
+        if (reward) {
+            for (let i = 0; i < shops.length; i++) {
+                const item = await shops[i].findOne({ where: { name: reward } });
+                if (item) {
+                    const user = await Users.findOne({ where: { user_id: shinxBattle.owner.id } });
+                    if (i === 0) {
+                        await user.addEquipment(item);
+                        userRewards.push(['equipment', reward]) ;
+                    } else if (i === 1) {
+                        await user.addFood(item);
+                        userRewards.push(['food', reward]);
+                    } else if (i === 2) {
+                        await user.addKey(item);
+                        userRewards.push(['key item', reward]);
+                    } else if (i === 3) {
+                        await user.changeRoom(item);
+                        userRewards.push(['room', reward]);
+                    };
+                };
             };
         };
-    };
+    }
+    return userRewards;
 };
