810263a62fd3f4b455966116421380b9b9627d9b
Glazelf <gideon.steenbergen@gmail.com>
2020-09-07 12:12:43 +0200

    test server leaderboard

--- commands/money/leaderboard.js
+++ commands/money/leaderboard.js
@@ -3,14 +3,28 @@ exports.run = async (client, message) => {
     let globalVars = require('../../events/ready');
     try {
         const { bank } = require('../../database/bank');
-        return message.channel.send(
-            bank.currency.sort((a, b) => b.balance - a.balance)
-                .filter(user => client.users.cache.has(user.user_id))
-                .first(10)
-                .map((user, position) => `(${position + 1}) ${(client.users.cache.get(user.user_id).tag)}: ${Math.floor(user.balance)}${globalVars.currency}`)
-                .join('\n'),
-            { code: true }
-        );
+        let args = message.content.split(` `);
+
+        if (args[1].toLowerCase() == "server") {
+            let memberFetch = await message.guild.members.fetch();
+            return message.channel.send(
+                bank.currency.sort((a, b) => b.balance - a.balance)
+                    .filter(user => client.users.cache.get(user.user_id) && memberFetch.get(user.user_id))
+                    .first(10)
+                    .map((user, position) => `(${position + 1}) ${(client.users.cache.get(user.user_id).tag)}: ${Math.floor(user.balance)}${globalVars.currency}`)
+                    .join('\n'),
+                { code: true }
+            )
+        } else {
+            return message.channel.send(
+                bank.currency.sort((a, b) => b.balance - a.balance)
+                    .filter(user => client.users.cache.has(user.user_id))
+                    .first(10)
+                    .map((user, position) => `(${position + 1}) ${(client.users.cache.get(user.user_id).tag)}: ${Math.floor(user.balance)}${globalVars.currency}`)
+                    .join('\n'),
+                { code: true }
+            );
+        };
 
     } catch (e) {
         // log error
