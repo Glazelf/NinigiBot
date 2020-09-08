a0ff9d5ef07e59e54bd2ffcb5708ba62a38e856c
Glazelf <gideon.steenbergen@gmail.com>
2020-09-08 17:10:06 +0200

    init magic conch command

---
+++ commands/other/magicconch.js
@@ -0,0 +1,17 @@
+exports.run = (client, message) => {
+    try {
+        let args = message.content.split(` `);
+        if (!args[1]) return message.channel.send(`> You need to provide something for the Magic Conch to consider, ${message.author}.`);
+
+        const answers = ["Maybe someday", "Nothing", "Neither", "I don't think so", "No", "Yes", "Try asking again"];
+        const randomAnswer = answers[Math.floor(Math.random() * answers.length)];
+
+        return message.channel.send(`> The Magic Conch says "${randomAnswer}.", ${message.author}.`);
+
+    } catch (e) {
+        // log error
+        const logger = require('../../util/logger');
+
+        logger(e, client, message);
+    };
+};
