1ce4020fc972e7027e180daa385587fcf740c7b0
Glazelf <gideon.steenbergen@gmail.com>
2020-09-08 12:25:29 +0200

    add link to message to error log

--- util/logger.js
+++ util/logger.js
@@ -6,7 +6,7 @@ module.exports = (exception, client, message = null) => {
     console.log(exception);
 
     // log to dev channel
-    let baseMessage = message ? `An error occurred in ${message.channel}!\n\`\`\`${exception}\`\`\`` : `An error occurred!\n\`\`\`${exception}\`\`\``;
+    let baseMessage = message ? `An error occurred in ${message.channel}! Link: ${message.url}\n\`\`\`${exception}\`\`\`` : `An error occurred!\n\`\`\`${exception}\`\`\``;
     if (baseMessage.length > 2000) baseMessage = baseMessage.substring(0, 1950) + `...\`\`\``;
     let devChannel = client.channels.cache.get(client.config.devChannelID);
     if (message) message.channel.send(`> An error has occurred trying to run the command. The error has already been logged, but please also report this as an issue on the Github page or send a message to Glaze#6669. For links and other information use ${globalVars.prefix}info.`);
