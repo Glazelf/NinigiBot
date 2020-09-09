312970f692be151424da0da33719f5041d671986
Glazelf <gideon.steenbergen@gmail.com>
2020-09-09 11:33:08 +0200

    add strings

--- commands/other/magicconch.js
+++ commands/other/magicconch.js
@@ -3,10 +3,10 @@ exports.run = (client, message) => {
         let args = message.content.split(` `);
         if (!args[1]) return message.channel.send(`> You need to provide something for the Magic Conch to consider, ${message.author}.`);
 
-        const answers = ["Maybe someday", "Nothing", "Neither", "I don't think so", "No", "Yes", "Try asking again"];
+        const answers = ["Maybe someday", "Nothing", "Neither", "I don't think so", "No", "Yes", "Try asking again", "Definetly", "Probably not"];
         const randomAnswer = answers[Math.floor(Math.random() * answers.length)];
 
-        return message.channel.send(`> The Magic Conch says "${randomAnswer}.", ${message.author}.`);
+        return message.channel.send(`> The Magic Conch says: "${randomAnswer}.", ${message.author}.`);
 
     } catch (e) {
         // log error
