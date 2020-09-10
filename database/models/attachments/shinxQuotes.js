2f17bfad716e712a422ac9d3fb6c3aaba76b5248
Skaidus <aitor.izuzquiza@gmail.com>
2020-09-10 19:41:10 +0200

    define new model

---
+++ database/models/attachments/shinxQuotes.js
@@ -0,0 +1,14 @@
+module.exports = (sequelize, DataTypes) => {
+	return sequelize.define('shinx_quote', {
+		quote: {
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		reaction: {
+			type: DataTypes.INTEGER,
+			allowNull: false
+		}
+	}, {
+		timestamps: false,
+	});
+};
