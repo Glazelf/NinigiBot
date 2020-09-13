da21ac8647c255bb57598a4bcef0cbee2bd716d7
Glazelf <gideon.steenbergen@gmail.com>
2020-09-13 13:13:52 +0200

    fix automod

--- util/autoMod.js
+++ util/autoMod.js
@@ -1,24 +1,34 @@
 module.exports = async (message) => {
-    let memberRoles = message.member.roles.cache.filter(element => element.name !== "@everyone");
+    try {
+        if (!message.member.hasPermission("KICK_MEMBERS")) return;
 
-    let reason = "Unspecified.";
+        let memberRoles = message.member.roles.cache.filter(element => element.name !== "@everyone");
 
-    if (memberRoles.size == 0) {
-        if (message.content.includes("https://glorysocial.com/profile/")) {
-            reason = "Posting scam links.";
-            await message.member.ban({ days: 1, reason: reason })
-            await message.author.send(`> You've been autobanned for the following reason: \`${reason}\`
+        let reason = "Unspecified.";
+
+        if (memberRoles.size == 0) {
+            if (message.content.includes("https://glorysocial.com/profile/")) {
+                reason = "Posting scam links.";
+                await message.member.ban({ days: 1, reason: reason })
+                await message.author.send(`> You've been autobanned for the following reason: \`${reason}\`
 \`\`\`${message.content}\`\`\``);
-            return message.channel.send(`> Successfully autobanned ${message.author} for the following reason: \`${reason}\``);
+                return message.channel.send(`> Successfully autobanned ${message.author} for the following reason: \`${reason}\``);
+            };
         };
-    };
 
-    if (message.content.includes("nigger") || message.content.includes("nigga") || message.content.includes("triceratops")) {
-        let reason = "Using offensive slurs.";
-        await message.delete();
-        await message.member.kick([reason]);
-        await message.author.send(`> You've been autokicked for the following reason: \`${reason}\`
+        if (message.content.includes("nigger") || message.content.includes("nigga")) {
+            let reason = "Using offensive slurs.";
+            await message.delete();
+            await message.member.kick([reason]);
+            await message.author.send(`> You've been autokicked for the following reason: \`${reason}\`
 \`\`\`${message.content}\`\`\``);
-        return message.channel.send(`> Successfully autokicked ${message.author} for the following reason: \`${reason}\``);
+            return message.channel.send(`> Successfully autokicked ${message.author} for the following reason: \`${reason}\``);
+        };
+
+    } catch (e) {
+        // log error
+        const logger = require('../util/logger');
+
+        logger(e, client, message);
     };
 };
