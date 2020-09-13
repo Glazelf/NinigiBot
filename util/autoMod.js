48f9ade4c4dbdcb61a2d25e7628b29b7507455b7
Glazelf <gideon.steenbergen@gmail.com>
2020-09-13 12:54:56 +0200

    add offensive slur catch and clean up some code

--- util/autoMod.js
+++ util/autoMod.js
@@ -1,9 +1,19 @@
 module.exports = async (message) => {
-    // Import globals
-    let globalVars = require('../events/ready');
-
+    let reason = "Unspecified."
     if (message.content.includes("https://glorysocial.com/profile/")) {
-        await message.member.ban({ days: 1, reason: message.content })
+        reason = "Posting scam links.";
+        await message.member.ban({ days: 1, reason: reason })
+        await message.author.send(`> You've been autobanned for the following reason: \`${reason}\`
+\`\`\`${message.content}\`\`\``);
         return message.channel.send(`> Successfully autobanned ${message.author}.`);
+    };
+
+    if(message.content.includes("nigger" || "nigga")) {
+        let reason = "Using offensive slurs.";
+        await message.delete();
+        await message.author.send(`> You've been autokicked for the following reason: \`${reason}\`
+\`\`\`${message.content}\`\`\``);
+        return message.channel.send(`> Successfully autokicked ${message.author} for the following reason:
+\`${reason}\``);
     };
 };
