da9e0ec459020412f1133fd7413a9e18a07a589d
Glazelf <gideon.steenbergen@gmail.com>
2020-09-14 21:40:53 +0200

    add terms and functions

--- util/autoMod.js
+++ util/autoMod.js
@@ -1,5 +1,5 @@
 module.exports = async (message) => {
-    if (message.member.hasPermission("KICK_MEMBERS")) return;
+    // if (message.member.hasPermission("KICK_MEMBERS")) return;
     if (!message.content) return;
 
     let memberRoles = message.member.roles.cache.filter(element => element.name !== "@everyone");
@@ -17,27 +17,47 @@ module.exports = async (message) => {
         "nigga",
         "niqqa",
         "nlgger",
-        "nlgga"
+        "nlgga",
+        "retard",
+        "faggot",
+        "tranny"
     ];
-    // const testArray = ["triceratops"];
+    const testArray = ["triceratops"];
 
     // Scam links
     if (scamLinks.some(v => messageNormalized.includes(v)) && message.member.bannable && memberRoles.size == 0) {
         reason = "Posting scam links.";
-        await message.member.ban({ days: 1, reason: reason });
-        await message.author.send(`> You've been autobanned for the following reason: \`${reason}\`
-\`\`\`${message.content}\`\`\``);
-        return message.channel.send(`> Successfully autobanned ${message.author} for the following reason: \`${reason}\``);
+        ban();
     };
 
     // Offensive slurs
     if (offensiveSlurs.some(v => messageNormalized.includes(v)) && message.member.kickable) {
         reason = "Using offensive slurs.";
-        await message.delete();
-        await message.member.kick([reason]);
-        await message.author.send(`> You've been autokicked for the following reason: \`${reason}\`
+        kick();
+    };
+
+    // Test 
+    // if(testArray.some(v => messageNormalized.includes(v))){
+    //     test()
+    // };
+
+    function kick() {
+        message.delete();
+        message.member.kick([reason]);
+        message.author.send(`> You've been autokicked for the following reason: \`${reason}\`
 \`\`\`${message.content}\`\`\``);
         return message.channel.send(`> Successfully autokicked ${message.author} for the following reason: \`${reason}\``);
+    };
+
+    function ban() {
+        message.member.ban({ days: 1, reason: reason });
+        message.author.send(`> You've been autobanned for the following reason: \`${reason}\`
+\`\`\`${message.content}\`\`\``);
+        return message.channel.send(`> Successfully autobanned ${message.author} for the following reason: \`${reason}\``);
+    };
+
+    function test(){
+        return message.channel.send(`banned lol`);
     };
 
     // if (testArray.some(v => messageNormalized.includes(v))) return message.channel.send("banned lol");
