08ea17134ba0fe6f64002f078d8006b8bd074a89
Glazelf <gideon.steenbergen@gmail.com>
2020-09-14 16:30:04 +0200

    fix and improve automod
    fixes a bug that made it not work
    catches accents and other transformations of characters

--- util/autoMod.js
+++ util/autoMod.js
@@ -1,12 +1,18 @@
 module.exports = async (message) => {
-    if (!message.member.hasPermission("KICK_MEMBERS")) return;
+    if (message.member.hasPermission("KICK_MEMBERS")) return;
+    if (!message.content) return;
 
     let memberRoles = message.member.roles.cache.filter(element => element.name !== "@everyone");
 
     let reason = "Unspecified.";
+    messageNormalized = message.content.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+
+    const scamLinks = ["https://glorysocial.com/profile/"];
+    const offensiveSlurs = ["nigger", "nigga", "niqqa"];
+    // const testArray = ["triceratops"];
 
     if (message.member.bannable && memberRoles.size == 0) {
-        if (message.content.includes("https://glorysocial.com/profile/")) {
+        if (scamLinks.some(v => messageNormalized.includes(v))) {
             reason = "Posting scam links.";
             await message.member.ban({ days: 1, reason: reason })
             await message.author.send(`> You've been autobanned for the following reason: \`${reason}\`
@@ -15,13 +21,14 @@ module.exports = async (message) => {
         };
     };
 
-    if (message.member.kickable && (message.content.includes("nigger") || message.content.includes("nigga"))) {
-        let reason = "Using offensive slurs.";
+    if (message.member.kickable && offensiveSlurs.some(v => messageNormalized.includes(v))) {
+        reason = "Using offensive slurs.";
         await message.delete();
         await message.member.kick([reason]);
         await message.author.send(`> You've been autokicked for the following reason: \`${reason}\`
 \`\`\`${message.content}\`\`\``);
         return message.channel.send(`> Successfully autokicked ${message.author} for the following reason: \`${reason}\``);
     };
-    return;
+
+    // if (testArray.some(v => messageNormalized.includes(v))) return message.channel.send("banned lol");
 };
