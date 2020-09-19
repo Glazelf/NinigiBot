27d55068226cf92dab70467268dc2b2c7a49fd26
Glazelf <gideon.steenbergen@gmail.com>
2020-09-19 17:17:58 +0200

    comment out ban logging for now

--- events/guildBanAdd.js
+++ events/guildBanAdd.js
@@ -1,27 +1,28 @@
 module.exports = (client, member) => {
     try {
-        const Discord = require("discord.js");
-        const log = member.guild.channels.cache.find(channel => channel.name === "log");
+        // const Discord = require("discord.js");
+        // const log = member.guild.channels.cache.find(channel => channel.name === "log");
 
-        // Import totals
-        let globalVars = require('./ready');
+        // // Import totals
+        // let globalVars = require('./ready');
 
-        if (!log) return;
-        user = client.users.cache.get(member.id);
+        // if (!log) return;
+        // user = client.users.cache.get(member.id);
 
-        let avatar = null;
-        if (user.avatarURL()) avatar = user.avatarURL({ format: "png", dynamic: true });
+        // let avatar = null;
+        // if (user.avatarURL()) avatar = user.avatarURL({ format: "png", dynamic: true });
 
-        const joinEmbed = new Discord.MessageEmbed()
-            .setColor(globalVars.embedColor)
-            .setAuthor(`Member got banned ☠️`, avatar)
-            .setThumbnail(avatar)
-            .addField(`User:`, user)
-            .setFooter(`Goodbye, ${user.tag}!`)
-            .setTimestamp();
+        // const joinEmbed = new Discord.MessageEmbed()
+        //     .setColor(globalVars.embedColor)
+        //     .setAuthor(`Member got banned ☠️`, avatar)
+        //     .setThumbnail(avatar)
+        //     .addField(`User:`, user)
+        //     .setFooter(`Goodbye, ${user.tag}!`)
+        //     .setTimestamp();
 
-        globalVars.totalLogs += 1;
-        return log.send(joinEmbed);
+        // globalVars.totalLogs += 1;
+        // return log.send(joinEmbed);
+        return;
 
     } catch (e) {
         // log error
