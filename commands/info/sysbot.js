fd981404301c0e864ab483f5f473bc18fc897bf3
Glazelf <gideon.steenbergen@gmail.com>
2020-10-29 16:21:37 +0100

    fix formatting

--- commands/info/sysbot.js
+++ commands/info/sysbot.js
@@ -18,17 +18,17 @@ module.exports.run = async (client, message) => {
         let Arkos = client.users.cache.get("702604221714923691");
         // ACNH
         let Ribbot = client.users.cache.get("739823632267608135");
-        
+
         let KonohanaStatus = "Online";
         let Flar3Status = "Online";
         let RibbotStatus = "Online";
 
-        if(Konohana.presence.status = "offline") KonohanaStatus = "Offline";
-        if(Flar3.presence.status = "offline") Flar3Status = "Offline";
-        if(Ribbot.presence.status = "offline") RibbotStatus = "Offline";
+        if (Konohana.presence.status = "offline") KonohanaStatus = "Offline";
+        if (Flar3.presence.status = "offline") Flar3Status = "Offline";
+        if (Ribbot.presence.status = "offline") RibbotStatus = "Offline";
 
         return message.channel.send(`> Hey, ${message.author}, here's a list of Sysbots and their status:
-> **Format:** Bot (Owner#0001) (prefix): status
+> **Format:** Bot (prefix): status (Host#0001)
 > **Pokémon bots:**
 > ${Konohana} (!): **${KonohanaStatus}** (${Glaze.tag})
 > ${Flar3} (3): **${Flar3Status}** (${Flare.tag})
