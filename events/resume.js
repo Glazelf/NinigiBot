67f3f84b711024db7e33db7de0b32e6e20eed778
Glazelf <gideon.steenbergen@gmail.com>
2020-11-09 16:12:06 +0100

    add status correction on resume

---
+++ events/resume.js
@@ -0,0 +1,16 @@
+module.exports = async (client) => {
+    try {
+        const Discord = require("discord.js");
+
+        // Set bot status
+        client.user.setPresence({ activity: { name: 'in Sinnoh' }, status: 'idle' });
+
+        // Console log status
+        return console.log("Successfully reconnected!");
+
+    } catch (e) {
+        // log error
+        console.log(e);
+    };
+};
+
