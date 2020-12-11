c12244068685baa19a97cf05582bb51d9dd273a2
Glazelf <gideon.steenbergen@gmail.com>
2020-12-12 00:28:03 +0100

    commit correction
    what the fuck is giig i

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -5,16 +5,17 @@ exports.run = (client, message) => {
         let pongString = `> Pong!'ed back at ${message.author} in`;
         let pauseString = `${pongString} (hold on, processing latency...)`;
 
+        // Replace string based on input. For some reason .replaceAll() doesn't work here. Whatever.
         if (message.content.startsWith(`${globalVars.prefix}pig`) || message.content.startsWith(`${globalVars.prefix}pog`)) {
             pongString = pongString.split("n").join("");
             pauseString = pauseString.split("n").join("");
         };
-
         if (message.content[2] == "o") {
             pongString = pongString.split("o").join("i");
             pauseString = pauseString.split("o").join("i");
         };
 
+        // Send message then edit message to reflect difference in creation timestamps
         return message.channel.send(pauseString).then(m => m.edit(`${pongString} ${m.createdTimestamp - message.createdTimestamp}ms.`));
 
     } catch (e) {
