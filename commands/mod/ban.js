e94f55bedd98b0fbf8a2f6d09985ccda6718d347
Glazelf <gideon.steenbergen@gmail.com>
2020-12-13 17:45:52 +0100

    fix permission checks for self and user role comparisons

--- commands/mod/ban.js
+++ commands/mod/ban.js
@@ -3,19 +3,18 @@ module.exports.run = async (client, message) => {
     let globalVars = require('../../events/ready');
     try {
         if (!message.member.hasPermission("BAN_MEMBERS") && message.author.id !== client.config.ownerID) return message.reply(globalVars.lackPerms);
-        if (!message.channel.permissionsFor(message.guild.me).has("BAN_MEMBERS")) return message.channel.send(`> I lack the required permissions to ban members, ${message.author}.`);
+        if (!message.guild.me.hasPermission("BAN_MEMBERS")) return message.channel.send(`> I lack the required permissions to ban members, ${message.author}.`);
 
         const args = message.content.split(' ');
 
         const member = message.mentions.members.first();
         let user = message.mentions.users.first();
         if (!member || !user) return message.channel.send(`> Please mention someone to ban, ${message.author}.`);
-
         if (!member.bannable) return message.channel.send(`> I lack the required permission to ban ${user.tag}, ${message.author}.`);
 
-        let userRole = message.member.roles.highest.position;
-        let targetRole = member.roles.highest.position;
-        if (targetRole.position >= userRole.position && message.author.id !== message.guild.ownerID) return message.channel.send(`> You don't have a high enough role to ban ${user.tag}, ${message.author}.`);
+        let userRole = message.member.roles.highest;
+        let targetRole = member.roles.highest;
+        if (targetRole.position >= userRole.position || member.hasPermission("ADMINISTRATOR")) return message.channel.send(`> You don't have a high enough role to ban ${user.tag}, ${message.author}.`);
 
         let reason = "Not specified.";
         if (args[2]) {
--- commands/mod/kick.js
+++ commands/mod/kick.js
@@ -3,19 +3,18 @@ module.exports.run = async (client, message) => {
     let globalVars = require('../../events/ready');
     try {
         if (!message.member.hasPermission("KICK_MEMBERS") && message.author.id !== client.config.ownerID) return message.reply(globalVars.lackPerms);
-        if (!message.channel.permissionsFor(message.guild.me).has("KICK_MEMBERS")) return message.channel.send(`> I lack the required permissions to kick members, ${message.author}.`);
+        if (!message.guild.me.hasPermission("KICK_MEMBERS")) return message.channel.send(`> I lack the required permissions to kick members, ${message.author}.`);
 
         const args = message.content.split(' ');
 
         const member = message.mentions.members.first();
         let user = message.mentions.users.first();
-        if (!member) return message.channel.send(`> Please mention someone to kick, ${message.author}.`);
-
+        if (!member || !user) return message.channel.send(`> Please mention someone to kick, ${message.author}.`);
         if (!member.kickable) return message.channel.send(`> I lack the required permission to kick ${user.tag}, ${message.author}.`);
 
-        let userRole = message.member.roles.highest.position;
-        let targetRole = member.roles.highest.position;
-        if (targetRole.position >= userRole.position && message.author.id !== message.guild.ownerID) return message.channel.send(`> You don't have a high enough role to kick ${user.tag}, ${message.author}.`);
+        let userRole = message.member.roles.highest;
+        let targetRole = member.roles.highest;
+        if (targetRole.position >= userRole.position || member.hasPermission("ADMINISTRATOR")) return message.channel.send(`> You don't have a high enough role to ban ${user.tag}, ${message.author}.`);
 
         let reason = "Not specified.";
         if (args[2]) {
