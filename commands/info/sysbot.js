173bd5371d6b59d37ca8cd6093e5e7b6b68e5fa6
Glazelf <gideon.steenbergen@gmail.com>
2020-12-26 01:56:07 +0100

    add Flare's AC bot and Shion's PKM bot
    Also some variable cleaning and stuff uwu

--- commands/info/sysbot.js
+++ commands/info/sysbot.js
@@ -18,22 +18,32 @@ module.exports.run = async (client, message) => {
         let Arkos = client.users.cache.get("702604221714923691");
         // ACNH
         let Ribbot = client.users.cache.get("739823632267608135");
+        let ACFlare = client.users.cache.get("792174299716386867");
 
-        let KonohanaStatus = "**Online**";
-        let Flar3Status = "**Online**";
-        let RibbotStatus = "**Online**";
+        let onlineString = "**Online**";
+        let offlineString = "Offline";
+        let offlineStatus = offlineString.toLowerCase();
+        let KonohanaStatus = onlineString;
+        let Flar3Status = onlineString;
+        let ArkosStatus = onlineString;
+        let RibbotStatus = onlineString;
+        let ACFlareStatus = onlineString;
 
-        if (Konohana.presence.status == "offline") KonohanaStatus = "Offline";
-        if (Flar3.presence.status == "offline") Flar3Status = "Offline";
-        if (Ribbot.presence.status == "offline") RibbotStatus = "Offline";
+        if (Konohana.presence.status == offlineStatus) KonohanaStatus = offlineString;
+        if (Flar3.presence.status == offlineStatus) Flar3Status = offlineString;
+        if (Arkos.presence.status == offlineStatus) ArkosStatus = offlineString;
+        if (Ribbot.presence.status == offlineStatus) RibbotStatus = offlineString;
+        if (ACFlare.presence.status == offlineStatus) ACFlareStatus = offlineString;
 
         return message.channel.send(`> Hey, ${message.author}, here's a list of Sysbots and their status:
 > **Format:** Bot (prefix): status (Host#0001)
 > **Pokémon bots:**
 > ${Konohana} (!): ${KonohanaStatus} (${Glaze.tag})
 > ${Flar3} (3): ${Flar3Status} (${Flare.tag})
+> ${Arkos} ($): ${ArkosStatus} (${Shion.tag})
 > **ACNH bots:**
 > ${Ribbot} (!): ${RibbotStatus} (${Glaze.tag})
+> ${ACFlare} (/): ${ACFlareStatus} (${Flare.tag})
 
 > Please don't ask dumb questions like "When will x bot will be online???". Bots will be on when they're on.
 > Check the pins in <#${globalVars.botChannelID}> for more information and ways to support more uptime or donate!`);
