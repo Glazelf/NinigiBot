6786f4a910b65ca59914564108b141ae1738624f
Glazelf <gideon.steenbergen@gmail.com>
2020-12-30 16:59:48 +0100

    small cleanup
    just declarations and spacing

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -4,9 +4,9 @@ module.exports = (client, member) => {
     try {
         const Discord = require("discord.js");
         const log = member.guild.channels.cache.find(channel => channel.name === "log");
-
         if (!log) return;
-        user = client.users.cache.get(member.id);
+
+        let user = client.users.cache.get(member.id);
 
         let avatar = user.displayAvatarURL({ format: "png", dynamic: true });
 
