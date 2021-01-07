45926dacafda880ded84bbdc8d5e0d2c84b79c1a
Glazelf <gideon.steenbergen@gmail.com>
2021-01-07 22:38:37 +0100

    add slowmode command

---
+++ commands/mod/slowmode.js
@@ -0,0 +1,26 @@
+module.exports.run = async (client, message, args) => {
+    // Import globals
+    let globalVars = require('../../events/ready');
+    try {
+        if (!message.member.hasPermission("MANAGE_CHANNELS") && message.author.id !== client.config.ownerID) return message.reply(globalVars.lackPerms);
+        if (!message.channel.permissionsFor(message.guild.me).has("MANAGE_ROLES")) return message.channel.send(`> I don't have permission to manage channels, ${message.author}.`);
+
+        let arg = args[0];
+        if (!arg || isNaN(arg)) return message.channel.send(`> You need to provide a valid number (seconds) to change the slowmode to, ${message.author}.`);
+        if (arg > 21600) arg = 21600;
+
+        await message.channel.setRateLimitPerUser(arg);
+        return message.channel.send(`> Slowmode changed to ${arg} seconds, ${message.author}.`);
+
+    } catch (e) {
+        // log error
+        const logger = require('../../util/logger');
+
+        logger(e, client, message);
+    };
+};
+
+module.exports.config = {
+    name: "slowmode",
+    aliases: ["slow"]
+};
