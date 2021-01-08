e04786a36a8dbf8af02abe577e466be7df05d086
Glazelf <gideon.steenbergen@gmail.com>
2021-01-08 22:33:46 +0100

    test lol

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -1,6 +1,5 @@
 module.exports = async (client, oldMember, newMember) => {
     try {
-        if(newMember.hasPermission("MANAGE_CHANNELS")) return;
         const { VCTextChannels } = require('../database/dbObjects');
         let oldID = null;
         let newID = null;
