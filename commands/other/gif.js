ed2a64293b42f60a925961942613d83ea83046a7
Glazelf <gideon.steenbergen@gmail.com>
2021-01-10 02:58:11 +0100

    revamp then revert gif code because giphy is a horrible site

--- commands/other/gif.js
+++ commands/other/gif.js
@@ -1,80 +1,109 @@
 exports.run = async (client, message) => {
-  // Import globals
-  let globalVars = require('../../events/ready');
-  try {
-    const Discord = require("discord.js");
-    const { search } = require('../../util/search');
-    const { Prefixes } = require('../../database/dbObjects');
-    let prefix = await Prefixes.findOne({ where: { server_id: message.guild.id } });
-    if (prefix) {
-      prefix = prefix.prefix;
-    } else {
-      prefix = globalVars.prefix;
-    };
+    // Import globals
+    let globalVars = require('../../events/ready');
+    try {
+        const Discord = require("discord.js");
+        const { search } = require('../../util/search');
+        const { Prefixes } = require('../../database/dbObjects');
+        let prefix = await Prefixes.findOne({ where: { server_id: message.guild.id } });
+        if (prefix) {
+            prefix = prefix.prefix;
+        } else {
+            prefix = globalVars.prefix;
+        };
 
-    let helpText = `> **Pokémon:**
+        let helpText = `> **Pokémon:**
     > Squirtle, Jigglypuff, Slowpoke, Flareon, Snorlax, Mewtwo, Mew, Wooper, Espeon, Scizor, Heracross, Celebi, Torchic, Lotad, Turtwig, Chimchar, Piplup, Shinx, Pachirisu, Gible, Glaceon, Gliscor, Gallade, Azelf, Oshawott, Maractus, Zweilous, Reshiram, Lurantis, Dracovish
     > **Not Pokémon:**
     > Dango, Jojo, Stitch, Kuzco
     > **Interactions:**
     > Hug`;
 
-    let user = message.mentions.users.first();
-    let gifArgumentUncased = message.content.split(` `, 3);
-    let missingGifString = `> You didn't provide a valid gif argument, ${message.author}.
+        let user = message.mentions.users.first();
+        let gifArgumentUncased = message.content.split(` `, 3);
+        let missingGifString = `> You didn't provide a valid gif argument, ${message.author}.
 > For a list of gif arguments, use \`${prefix}gif help\`.`;
-    if (!gifArgumentUncased[1]) return message.channel.send(missingGifString);
-    let gifArgument = gifArgumentUncased[1].toLowerCase();
-    let gifArgumentCapitalized = gifArgument[0].toUpperCase() + gifArgument.substr(1);
-    let gifString = `Here's your gif, ${message.author}:`;
+        if (!gifArgumentUncased[1]) return message.channel.send(missingGifString);
+        let gifArgument = gifArgumentUncased[1].toLowerCase();
+        let gifArgumentCapitalized = gifArgument[0].toUpperCase() + gifArgument.substr(1);
+        let gifString = `Here's your gif, ${message.author}:`;
 
-    const gif = search(gifArgument);
+        const gif = search(gifArgument);
 
-    if (gifArgument == "help") {
-      return message.channel.send(`> Here's a list for all gif arguments, ${message.author}:
+        if (gifArgument == "help") {
+            return message.channel.send(`> Here's a list for all gif arguments, ${message.author}:
 ${helpText}`);
-    } else if (!message.channel.permissionsFor(message.guild.me).has("ATTACH_FILES")) {
-      return message.channel.send(`> I can't send you gifs because I don't have permissions to attach files to my messages, ${message.author}.`);
-    } else if (!message.channel.permissionsFor(message.guild.me).has("EMBED_LINKS")) {
-      return message.channel.send(`> I can't send you gifs because I don't have permissions to embed messages, ${message.author}.`);
-    } else if (gif) {
+        } else if (!message.channel.permissionsFor(message.guild.me).has("ATTACH_FILES")) {
+            return message.channel.send(`> I can't send you gifs because I don't have permissions to attach files to my messages, ${message.author}.`);
+        } else if (!message.channel.permissionsFor(message.guild.me).has("EMBED_LINKS")) {
+            return message.channel.send(`> I can't send you gifs because I don't have permissions to embed messages, ${message.author}.`);
+        } else if (gif) {
 
-      if (gifArgument == "hug") {
-        if (user) {
-          gifString = `${message.author} gave ${user} a tight hug!`;
-          if (user == message.author) {
-            gifString = `${user} is hugging themselves... This is kind of sad...`;
-          };
+            if (gifArgument == "hug") {
+                if (user) {
+                    gifString = `${message.author} gave ${user} a tight hug!`;
+                    if (user == message.author) {
+                        gifString = `${user} is hugging themselves... This is kind of sad...`;
+                    };
+                } else {
+                    gifString = `It seems ${message.author} wants to hug...`;
+                };
+            };
+
+            let avatar = message.author.displayAvatarURL({ format: "png", dynamic: true });
+
+            const gifEmbed = new Discord.MessageEmbed()
+                .setColor(globalVars.embedColor)
+                .setAuthor(`${gifArgumentCapitalized} Gif`, avatar)
+                .setDescription(gifString)
+                .setImage(gif)
+                .setFooter(message.author.tag)
+                .setTimestamp();
+
+            return message.channel.send(gifEmbed);
+
         } else {
-          gifString = `It seems ${message.author} wants to hug...`;
+            return message.channel.send(missingGifString);
         };
-      };
 
-      let avatar = message.author.displayAvatarURL({ format: "png", dynamic: true });
+        // Using random giphy requests, but the matching is horrible if you even get a match at all
+        // const giphyRandom = require("giphy-random");
 
-      const gifEmbed = new Discord.MessageEmbed()
-        .setColor(globalVars.embedColor)
-        .setAuthor(`${gifArgumentCapitalized} Gif`, avatar)
-        .setDescription(gifString)
-        .setImage(gif)
-        .setFooter(message.author.tag)
-        .setTimestamp();
+        // args = args.join(" ");
 
-      return message.channel.send(gifEmbed);
+        // const getRandomGif = async () => {
+        //     const { data } = await giphyRandom(client.config.giphy, {
+        //         tag: args
+        //     });
+        //     return data.image_url;
+        // };
 
-    } else {
-      return message.channel.send(missingGifString);
+        // const randomGif = await getRandomGif();
+
+        // let avatar = message.author.displayAvatarURL({
+        //     format: "png",
+        //     dynamic: true
+        // });
+
+        // const gifEmbed = new Discord.MessageEmbed()
+        //     .setColor(globalVars.embedColor)
+        //     .setAuthor(`Gif (${args})`, avatar)
+        //     .setDescription(`Here's your gif, ${message.author}:`)
+        //     .setImage(randomGif)
+        //     .setFooter(message.author.tag)
+        //     .setTimestamp();
+
+        // return message.channel.send(gifEmbed);
+
+    } catch (e) {
+        // log error
+        const logger = require('../../util/logger');
+
+        logger(e, client, message);
     };
-
-  } catch (e) {
-    // log error
-    const logger = require('../../util/logger');
-
-    logger(e, client, message);
-  };
 };
 
 module.exports.config = {
-  name: "gif",
-  aliases: []
+    name: "gif",
+    aliases: []
 };
