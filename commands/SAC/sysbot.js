2379b3987a30de019784b544c71581ca4deb0429
Glazelf <gideon.steenbergen@gmail.com>
2021-01-12 16:25:44 +0100

    typo

--- commands/SAC/sysbot.js
+++ commands/SAC/sysbot.js
@@ -40,7 +40,7 @@ module.exports.run = async (client, message) => {
         if (MagicDoctor.presence.status == offlineStatus) MagicDoctorStatus = offlineString;
 
         return message.channel.send(`> Hey, ${message.author}, here's a list of Sysbots and their status:
-> **Format:** Bot (prefix): status (Availability when online) (Host#0001)
+> **Format:** Bot (prefix): status (Host#0001)
 > **Pokémon bots:**
 > ${Konohana} (!): ${KonohanaStatus} (${Glaze.tag})
 > ${Flar3} (3): ${Flar3Status} (${Flare.tag})
