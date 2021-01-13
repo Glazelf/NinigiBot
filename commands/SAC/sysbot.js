e060a43307b7af3c9148a7c6a1909bebb4904cfa
Glazelf <gideon.steenbergen@gmail.com>
2021-01-13 02:15:10 +0100

    add Miku

--- commands/SAC/sysbot.js
+++ commands/SAC/sysbot.js
@@ -11,12 +11,14 @@ module.exports.run = async (client, message) => {
         let Flare = client.users.cache.get("592353588685307914");
         let Shion = client.users.cache.get("685608164506337351");
         let Betty = client.users.cache.get("465892606417567747");
+        let Xenoseon = "Xenoseon#1604";
 
         // Bots
         // PKM
         let Konohana = client.users.cache.get(`696086046685003786`);
         let Flar3 = client.users.cache.get("734052437811527784");
         let Arkos = client.users.cache.get("702604221714923691");
+        let Miku = client.users.cache.get("752902915508666499");
         // ACNH
         let Ribbot = client.users.cache.get("739823632267608135");
         let ACFlare = client.users.cache.get("792174299716386867");
@@ -28,6 +30,7 @@ module.exports.run = async (client, message) => {
         let KonohanaStatus = onlineString;
         let Flar3Status = onlineString;
         let ArkosStatus = onlineString;
+        let MikuStatus = onlineString
         let RibbotStatus = onlineString;
         let ACFlareStatus = onlineString;
         let MagicDoctorStatus = onlineString;
@@ -35,6 +38,7 @@ module.exports.run = async (client, message) => {
         if (Konohana.presence.status == offlineStatus) KonohanaStatus = offlineString;
         if (Flar3.presence.status == offlineStatus) Flar3Status = offlineString;
         if (Arkos.presence.status == offlineStatus) ArkosStatus = offlineString;
+        if (Miku.presence.status == offlineStatus) MikuStatus = offlineString;
         if (Ribbot.presence.status == offlineStatus) RibbotStatus = offlineString;
         if (ACFlare.presence.status == offlineStatus) ACFlareStatus = offlineString;
         if (MagicDoctor.presence.status == offlineStatus) MagicDoctorStatus = offlineString;
@@ -42,9 +46,10 @@ module.exports.run = async (client, message) => {
         return message.channel.send(`> Hey, ${message.author}, here's a list of Sysbots and their status:
 > **Format:** Bot (prefix): status (Host#0001)
 > **Pokémon bots:**
-> ${Konohana} (!): ${KonohanaStatus} (${Glaze.tag})
+> ${Konohana} (&): ${KonohanaStatus} (${Glaze.tag})
 > ${Flar3} (3): ${Flar3Status} (${Flare.tag})
 > ${Arkos} ($): ${ArkosStatus} (${Shion.tag})
+> ${Miku} (!): ${MikuStatus} (${Xenoseon})
 > **ACNH bots:**
 > ${Ribbot} (!): ${RibbotStatus} (${Glaze.tag})
 > ${ACFlare} (/): ${ACFlareStatus} (${Flare.tag})
