f8a0f45744bbc38434fb8845ae22c824cbe800e6
Glazelf <gideon.steenbergen@gmail.com>
2021-01-15 16:15:26 +0100

    await

--- util/logger.js
+++ util/logger.js
@@ -1,4 +1,4 @@
-module.exports = (exception, client, message = null) => {
+module.exports = async (exception, client, message = null) => {
     // Import globals
     let globalVars = require('../events/ready');
 
@@ -16,7 +16,7 @@ Message by ${message.author.tag}:
 \`\`\`${message.content}\`\`\`` : `An error occurred:
 \`\`\`${exception}\`\`\``;
     if (baseMessage.length > 2000) baseMessage = baseMessage.substring(0, 1950) + `...\`\`\``;
-    let devChannel = client.shard.broadcastEval(`client.channels.cache.find("${client.config.devChannelID}")`);
+    let devChannel = await client.shard.broadcastEval(`client.channels.cache.find("${client.config.devChannelID}")`);
     console.log(devChannel)
     if (message) message.channel.send(`> An error has occurred. 
 > The error has already been logged but please also report this as an issue on Github: 
