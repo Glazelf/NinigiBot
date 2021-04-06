220dd80bed6855460bbe59602f2faf6072cccf45
Glazelf <gideon.steenbergen@gmail.com>
2021-04-06 10:42:03 +0200

    split off starboard logic

---
+++ util/starboard.js
@@ -0,0 +1,59 @@
+module.exports = async (message) => {
+    // Import globals
+    let globalVars = require('../events/ready');
+    try {
+        const Discord = require("discord.js");
+        const { StarboardChannels } = require('../database/dbObjects');
+
+        // Call image
+        let messageImage = null;
+        let messageVideo = null;
+        if (message.attachments.size > 0) {
+            messageImage = message.attachments.first().url;
+            if (messageImage.endsWith(".mp4")) {
+                messageVideo = messageImage;
+                messageImage = null;
+            };
+        };
+
+        // Starboard logic
+        message.awaitReactions(reaction => reaction.emoji.name == "⭐", { max: globalVars.starboardLimit, time: 3600000 }).then(async collected => {
+            let starboardChannel = await StarboardChannels.findOne({ where: { server_id: message.guild.id } });
+            // Check various permissions and channel existences
+            if (starboardChannel) {
+                let starboard = message.guild.channels.cache.find(channel => channel.id == starboardChannel.channel_id);
+                if (starboard) {
+                    if (message.channel !== starboard) {
+                        if (!starboard.permissionsFor(message.guild.me).has("EMBED_LINKS")) return message.channel.send(`> I don't have permissions to send embedded message to your starboard, ${message.author}.`);
+                        if (!collected.first()) return;
+                        if (collected.first().count >= globalVars.starboardLimit) {
+                            // Assemble embed
+                            let avatar = message.author.displayAvatarURL({ format: "png", dynamic: true });
+                            const starEmbed = new Discord.MessageEmbed()
+                                .setColor(globalVars.embedColor)
+                                .setAuthor(`⭐ ${message.author.username}`, avatar)
+                                .setDescription(message.content)
+                                .addField(`Sent:`, `By ${message.author} in ${message.channel}`, false)
+                                .addField(`Context:`, `[Link](${message.url})`, false)
+                                .setImage(messageImage)
+                                .setFooter(message.author.tag)
+                                .setTimestamp(message.createdTimestamp);
+                            // Sending logic
+                            if (messageVideo) {
+                                await starboard.send(starEmbed);
+                                starboard.send({ files: [messageVideo] });
+                            } else {
+                                starboard.send(starEmbed);
+                            };
+                        };
+                    };
+                };
+            };
+        });
+    } catch (e) {
+        // log error
+        const logger = require('../util/logger');
+
+        logger(e, client);
+    };
+};
