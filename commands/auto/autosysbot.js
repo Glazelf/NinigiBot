93313f0dcab4171568f857a9b3c7be7ea3e5b495
FlareHD <60042359+FlareHD@users.noreply.github.com>
2021-04-07 01:27:01 +0100

    Update autosysbot.js

--- commands/auto/autosysbot.js
+++ commands/auto/autosysbot.js
@@ -1,1 +1,8 @@
-console.log('test');
+module.exports.run = async (client, message) => {
+    try {
+        const { bank } = require('../../database/bank');
+        const input = message.content.slice(1).trim();
+        const [, , arguments] = input.match(/(\w+)\s*([\s\S]*)/);
+      
+      
+      if ('how do i use sysbot?') return message.channel.send(
