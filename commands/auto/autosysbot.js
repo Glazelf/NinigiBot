9e30510d0839556dba56a8bd18f5542180c7108c
FlareHD <60042359+FlareHD@users.noreply.github.com>
2021-04-07 01:29:01 +0100

    Update autosysbot.js

--- commands/auto/autosysbot.js
+++ commands/auto/autosysbot.js
@@ -1,8 +1,3 @@
-module.exports.run = async (client, message) => {
-    try {
-        const { bank } = require('../../database/bank');
-        const input = message.content.slice(1).trim();
-        const [, , arguments] = input.match(/(\w+)\s*([\s\S]*)/);
-      
+const Discord = require("discord.js");
       
       if ('how do i use sysbot?') return message.channel.send(
