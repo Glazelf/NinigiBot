a7f2d0875ba67d5abc7ed59a77740988c60a4d94
Glazelf <gideon.steenbergen@gmail.com>
2021-04-09 10:41:14 +0200

    add functionality to list all users without a role

--- commands/info/roleinfo.js
+++ commands/info/roleinfo.js
@@ -4,15 +4,38 @@ exports.run = async (client, message) => {
     let globalVars = require('../../events/ready');
     try {
         const Discord = require("discord.js");
+        let DefaultEmbedColor = "#99AB5";
 
         // Split off command
         let arg = message.content.slice(1).trim();
         let [, , textMessage] = arg.match(/(\w+)\s*([\s\S]*)/);
         let input = textMessage;
 
+        // Author avatar
+        let avatar = message.author.displayAvatarURL({ format: "png", dynamic: true });
+
         // Check for role
         let role = message.guild.roles.cache.find(role => role.name.toLowerCase() === input.toLowerCase());
         if (!role) role = message.guild.roles.cache.get(input);
+
+        if (input.toLowerCase() == "none") {
+            let fetchedMembers = await message.guild.members.fetch();
+            let noRoleMembers = 0;
+            fetchedMembers.forEach(member => {
+                if (member.roles.cache.size == 1) {
+                    noRoleMembers += 1;
+                };
+            });
+            const noRoleEmbed = new Discord.MessageEmbed()
+                .setColor(DefaultEmbedColor)
+                .setAuthor(`Users in ${message.guild.name} without a role`, avatar)
+                .addField("Members:", noRoleMembers, true)
+                .setFooter(message.author.tag)
+                .setTimestamp();
+
+            return message.channel.send(noRoleEmbed);
+        };
+
         if (!role) return message.channel.send(`> I couldn't find that role, ${message.author}. Make sure you provide a valid name or ID.`);
 
         // Role color
@@ -20,14 +43,11 @@ exports.run = async (client, message) => {
         let embedColor = roleColor;
         if (roleColor == "#0") {
             roleColor = "Default";
-            embedColor = globalVars.embedColor;
+            embedColor = DefaultEmbedColor;
         };
 
         // Member count
         let memberCount = message.guild.members.cache.filter(member => member.roles.cache.find(loopRole => loopRole == role)).size;
-
-        // Author avatar
-        let avatar = message.author.displayAvatarURL({ format: "png", dynamic: true });
 
         // Properties
         let roleProperties = "";
