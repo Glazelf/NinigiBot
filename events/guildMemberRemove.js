6198545a3f4c8cd0b3e3a8b3ff4c0dfd60f83b45
Glazelf <gideon.steenbergen@gmail.com>
2021-05-21 11:10:39 +0200

    add extra fetch
    should improve consistency

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -9,6 +9,8 @@ module.exports = async (client, member) => {
         let log = member.guild.channels.cache.find(channel => channel.id == logChannel.channel_id);
         if (!log) return;
 
+        await client.users.fetch(member.id);
+
         let user = client.users.cache.get(member.id);
         let avatar = user.displayAvatarURL({ format: "png", dynamic: true });
         let icon = member.guild.iconURL({ format: "png", dynamic: true });
