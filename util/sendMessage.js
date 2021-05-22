a2f1014cd7f6a4be9343a86809ed5b6dd701acd3
Glazelf <gideon.steenbergen@gmail.com>
2021-05-22 17:50:53 +0200

    refactor function, add channel check
    also small bugfixes with code blocks and file arrays

--- util/sendMessage.js
+++ util/sendMessage.js
@@ -1,12 +1,21 @@
-module.exports = async (client, message, replyText, ephimeral = true, files = [], code = false) => {
+module.exports = async (client, message, replyText, ephimeral = true, files = null, code = false) => {
     try {
+        const { DisabledChannels } = require('../database/dbObjects');
+        const dbChannels = await DisabledChannels.findAll();
+        const channels = dbChannels.map(channel => channel.channel_id);
+
+        // Force hidden if disabled channel
+        if (channels.includes(message.channel.id)) ephimeral = true;
+
         // 'DEFAULT' = text message, 'APPLICATION_COMMAND' = slash command
         if (message.type == 'DEFAULT') {
-            if (files.length > 0) return message.reply(replyText, { files: files });
-            return message.reply(replyText);
+            if (Array.isArray(files)) return message.reply(replyText, { files: files, code: code });
+            return message.reply(replyText, { code: code });
+
         } else if (message.type == 'APPLICATION_COMMAND') {
-            if (files.length > 0) return message.reply(replyText, { ephimeral: ephimeral, code: code, files: files })
+            if (Array.isArray(files)) return message.reply(replyText, { ephimeral: ephimeral, files: files, code: code })
             return message.reply(replyText, { ephimeral: ephimeral, code: code });
+
         } else {
             return message.reply(`Unknown message type.`);
         };
