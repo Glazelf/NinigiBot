32ce9fa5645cf4586b2ce1a2c5d43d99108d946f
Glazelf <gideon.steenbergen@gmail.com>
2021-05-23 19:36:16 +0200

    clean

--- util/isAdmin.js
+++ util/isAdmin.js
@@ -2,9 +2,13 @@ module.exports = async (member, client) => {
     // Import globals
     let globalVars = require('../events/ready');
     try {
-        if (member.guild.ownerID !== member.id) return true;
-        if (member.permissions.has("ADMINISTRATOR")) return true;
-        return false;
+        if (member.guild.ownerID !== member.id) {
+            return true
+        } else if (member.permissions.has("ADMINISTRATOR")) {
+            return true;
+        } else {
+            return false;
+        };
 
     } catch (e) {
         // log error
