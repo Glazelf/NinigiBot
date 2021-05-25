19d24697d9112aec4e3c3420b10f3660d6826022
Glazelf <gideon.steenbergen@gmail.com>
2021-05-25 20:19:31 +0200

    add error catching

--- affairs/birthday.js
+++ affairs/birthday.js
@@ -1,46 +1,53 @@
 module.exports = async (client) => {
     // Import globals
     let globalVars = require('../events/ready');
+    try {
 
-    const cron = require("cron");
-    const timezone = 'cest';
-    const time = '05 00 00 * * *'; //Sec Min Hour 
-    const guildID = client.config.botServerID;
-    const channelID = globalVars.eventChannelID;
-    const Discord = require("discord.js");
-    const { bank } = require('../database/bank');
-    const { search } = require('../util/search');
+        const cron = require("cron");
+        const timezone = 'cest';
+        const time = '05 00 00 * * *'; //Sec Min Hour 
+        const guildID = client.config.botServerID;
+        const channelID = globalVars.eventChannelID;
+        const Discord = require("discord.js");
+        const { bank } = require('../database/bank');
+        const { search } = require('../util/search');
 
-    new cron.CronJob(time, async () => {
-        let globalVars = require('../events/ready');
-        let guild = client.guilds.cache.get(guildID);
-        if (!guild) return;
-        const birthdayRole = guild.roles.cache.find(role => role.id === globalVars.birthdayRole);
-        if (!birthdayRole) return;
-        let yesterdayCuties = birthdayRole.members;
-        yesterdayCuties.forEach(cutie => cutie.roles.remove(birthdayRole));
-        const cuties = [];
-        for (m in guild.members.cache.array()) {
-            const member = guild.members.cache.array()[m];
-            const birthday = bank.currency.getBirthday(member.id);
-            if (birthday) {
-                let now = new Date();
-                if (now.getDate() === parseInt(birthday.substring(0, 2)) && (now.getMonth() + 1) === parseInt(birthday.substring(2))) {
-                    cuties.push(member.user.tag);
-                    await member.roles.add(birthdayRole);
+        new cron.CronJob(time, async () => {
+            let globalVars = require('../events/ready');
+            let guild = client.guilds.cache.get(guildID);
+            if (!guild) return;
+            const birthdayRole = guild.roles.cache.find(role => role.id === globalVars.birthdayRole);
+            if (!birthdayRole) return;
+            let yesterdayCuties = birthdayRole.members;
+            yesterdayCuties.forEach(cutie => cutie.roles.remove(birthdayRole));
+            const cuties = [];
+            for (m in guild.members.cache.array()) {
+                const member = guild.members.cache.array()[m];
+                const birthday = bank.currency.getBirthday(member.id);
+                if (birthday) {
+                    let now = new Date();
+                    if (now.getDate() === parseInt(birthday.substring(0, 2)) && (now.getMonth() + 1) === parseInt(birthday.substring(2))) {
+                        cuties.push(member.user.tag);
+                        await member.roles.add(birthdayRole);
+                    };
                 };
             };
-        };
 
-        if (cuties.length < 1) return;
+            if (cuties.length < 1) return;
 
-        let channel = guild.channels.cache.find(channel => channel.id === channelID);
+            let channel = guild.channels.cache.find(channel => channel.id === channelID);
 
-        const gifEmbed = new Discord.MessageEmbed()
-            .setColor(globalVars.embedColor)
-            .setDescription(`Today's is ${cuties.join(' and ')}'s birthday, everyone!`)
-            .setImage(search("birthday"))
-            .setTimestamp();
-        channel.send({ embed: gifEmbed });
-    }, timeZone = timezone, start = true);
+            const gifEmbed = new Discord.MessageEmbed()
+                .setColor(globalVars.embedColor)
+                .setDescription(`Today's is ${cuties.join(' and ')}'s birthday, everyone!`)
+                .setImage(search("birthday"))
+                .setTimestamp();
+            channel.send({ embed: gifEmbed });
+        }, timeZone = timezone, start = true);
+    } catch (e) {
+        // log error
+        const logger = require('../util/logger');
+
+        logger(e, client, message);
+    };
 };
--- affairs/stan.js
+++ affairs/stan.js
@@ -1,41 +1,49 @@
 module.exports = async (client) => {
-    const cron = require("cron");
-    const timezone = 'cest';
-    const time = '00 00 20 * * *'; //Sec Min Hour, 8pm
-    // const time = '* * * * *'; //Sec Min Hour testing
-    const guildID = client.config.botServerID;
-    const gifTags = ['pokemon', 'geass', 'dragon', 'game'];
-    const Discord = require("discord.js");
-    const config = require("../config.json");
+    try {
+        const cron = require("cron");
+        const timezone = 'cest';
+        const time = '00 00 20 * * *'; //Sec Min Hour, 8pm
+        // const time = '* * * * *'; //Sec Min Hour testing
+        const guildID = client.config.botServerID;
+        const gifTags = ['pokemon', 'geass', 'dragon', 'game'];
+        const Discord = require("discord.js");
+        const config = require("../config.json");
 
-    new cron.CronJob(time, async () => {
-        // Import globals
-        let globalVars = require('../events/ready');
-        let guild = client.guilds.cache.get(guildID);
-        if (!guild) return;
-        let candidates = guild.roles.cache.find(role => role.name.toLowerCase() === globalVars.stanRole).members.map(m => m.user);
-        if (candidates.length < 1) return;
+        new cron.CronJob(time, async () => {
+            // Import globals
+            let globalVars = require('../events/ready');
+            let guild = client.guilds.cache.get(guildID);
+            if (!guild) return;
+            let candidates = guild.roles.cache.find(role => role.name.toLowerCase() === globalVars.stanRole).members.map(m => m.user);
+            if (candidates.length < 1) return;
 
-        const giphyRandom = require("giphy-random");
-        const getRandomGif = async () => {
-            const randomElement = gifTags[Math.floor(Math.random() * gifTags.length)];
+            const giphyRandom = require("giphy-random");
+            const getRandomGif = async () => {
+                const randomElement = gifTags[Math.floor(Math.random() * gifTags.length)];
 
-            const { data } = await giphyRandom(config.giphy, {
-                tag: randomElement
-            });
-            return data.image_url;
-        };
+                const { data } = await giphyRandom(config.giphy, {
+                    tag: randomElement
+                });
+                return data.image_url;
+            };
 
-        const randomGif = await getRandomGif();
-        let randomPick = Math.floor((Math.random() * (candidates.length - 0.1)));
-        let candidateRandom = candidates[randomPick];
-        let channel = guild.channels.cache.find(channel => channel.id === globalVars.eventChannelID);
+            const randomGif = await getRandomGif();
+            let randomPick = Math.floor((Math.random() * (candidates.length - 0.1)));
+            let candidateRandom = candidates[randomPick];
+            let channel = guild.channels.cache.find(channel => channel.id === globalVars.eventChannelID);
 
-        const gifEmbed = new Discord.MessageEmbed()
-            .setColor(globalVars.embedColor)
-            .setDescription(`Today's most stannable person is ${candidateRandom.tag}, everyone!`)
-            .setImage(randomGif)
-            .setTimestamp();
-        channel.send(candidateRandom, { embed: gifEmbed });
-    }, timeZone = timezone, start = true);
+            const gifEmbed = new Discord.MessageEmbed()
+                .setColor(globalVars.embedColor)
+                .setDescription(`Today's most stannable person is ${candidateRandom.tag}, everyone!`)
+                .setImage(randomGif)
+                .setTimestamp();
+            channel.send(candidateRandom, { embed: gifEmbed });
+        }, timeZone = timezone, start = true);
+    } catch (e) {
+        // log error
+        const logger = require('../util/logger');
+
+        logger(e, client, message);
+    };
 };
+
