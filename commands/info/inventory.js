a6a33e64e2b18d26f95889e018e0a5617b560fc3
Glazelf <gideon.steenbergen@gmail.com>
2021-05-26 10:21:10 +0200

    split off target definitions for slash commands

--- commands/info/inventory.js
+++ commands/info/inventory.js
@@ -5,7 +5,9 @@ exports.run = async (client, message, args = []) => {
     try {
         const sendMessage = require('../../util/sendMessage');
         //items, food, equipment
-        const target = message.mentions.users.first() || message.member.user;
+        const target;
+        if (message.mentions) target = message.mentions.users.first();
+        if (!target) target = message.member.user;
 
         if (args[0] === 'items' || args[0] === 'food' || args[0] === 'equipment' || args[0] === 'keys' || !args[0]) {
             const user = await Users.findOne({ where: { user_id: target.id } });
