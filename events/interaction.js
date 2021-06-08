7dbd2c3290b14c32b44bea9463f6bf22d8504f1c
Glazelf <gideon.steenbergen@gmail.com>
2021-06-08 20:12:08 +0200

    log interaction errors

--- events/interaction.js
+++ events/interaction.js
@@ -82,7 +82,7 @@ module.exports = async (client, interaction) => {
                                     pkmEmbed = await getPokemon(client, interaction.message, response, interaction);
                                 });
                         } catch (e) {
-                            // console.log(e);
+                            console.log(e);
                             return;
                         };
                         if (!pkmEmbed) return;
