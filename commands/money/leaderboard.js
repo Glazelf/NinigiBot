9149c8e781c4421e59dc928875fdc8672f336931
Glazelf <gideon.steenbergen@gmail.com>
2021-06-28 01:01:03 +0200

    Restructure reply buildup
    Move reply into embed, redo sring structure, add check and some other minor changes

--- commands/money/leaderboard.js
+++ commands/money/leaderboard.js
@@ -3,6 +3,7 @@ exports.run = async (client, message, args = []) => {
     let globalVars = require('../../events/ready');
     try {
         const sendMessage = require('../../util/sendMessage');
+        const Discord = require("discord.js");
         const { bank } = require('../../database/bank');
         let memberFetch = await message.guild.members.fetch();
 
@@ -32,14 +33,25 @@ exports.run = async (client, message, args = []) => {
         };
 
         function serverLeaderboard() {
-            return sendMessage(client, message,
-                bank.currency.sort((a, b) => b.balance - a.balance)
-                    .filter(user => client.users.cache.get(user.user_id) && memberFetch.get(user.user_id))
-                    .filter(user => !client.users.cache.get(user.user_id).bot)
-                    .first(10)
-                    .map((user, position) => `(${position + 1}) ${(client.users.cache.get(user.user_id).tag)}: ${Math.floor(user.balance)}${globalVars.currency}`)
-                    .join('\n'), null, null, true, true
-            );
+            let leaderboardString = bank.currency.sort((a, b) => b.balance - a.balance)
+                .filter(user => client.users.cache.get(user.user_id) && memberFetch.get(user.user_id))
+                .filter(user => !client.users.cache.get(user.user_id).bot)
+                .first(10)
+                .map((user, position) => `${position + 1}. ${(client.users.cache.get(user.user_id).tag)}: ${Math.floor(user.balance)}${globalVars.currency}`)
+                .join('\n');
+
+            if (leaderboardString.length < 1) return sendMessage(client, message, "Noone in this server has any currency yet.");
+
+            let avatar = message.member.user.displayAvatarURL({ format: "png", dynamic: true });
+
+            const leaderboardEmbed = new Discord.MessageEmbed()
+                .setColor(globalVars.embedColor)
+                .setAuthor(`Currency leaderboard:`, avatar)
+                .setDescription(leaderboardString)
+                .setFooter(message.member.user.tag)
+                .setTimestamp();
+
+            return sendMessage(client, message, null, leaderboardEmbed);
         };
 
     } catch (e) {
