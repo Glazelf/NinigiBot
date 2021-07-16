dbd2b581ba0642f83b2cdac29e7d83336ebb7890
Glazelf <gideon.steenbergen@gmail.com>
2021-07-16 19:53:51 +0200

    uncomment

--- commands/exclusive/countdown.js
+++ commands/exclusive/countdown.js
@@ -3,7 +3,7 @@ module.exports.run = async (client, message) => {
     let globalVars = require('../../events/ready');
     try {
         const sendMessage = require('../../util/sendMessage');
-        // if (message.guild.id !== "861884184787550218") return;
+        if (message.guild.id !== "861884184787550218") return;
 
         let targetDate = new Date("Aug 1, 2021 8:25:00").getTime();
         let now = new Date().getTime();
