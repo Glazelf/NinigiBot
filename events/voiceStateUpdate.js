940ed681985952f9001edb0de24b96a19d0f0b32
Glazelf <gideon.steenbergen@gmail.com>
2021-07-29 19:23:42 +0200

    test

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -7,13 +7,13 @@ module.exports = async (client, oldMember, newMember) => {
         if (newMember.channelID) newID = newMember.channelID;
 
         let user = client.users.cache.get(newMember.id);
-        if (user.bot) return;
+        // if (user.bot) return;
 
         let VCTextChannel = await VCTextChannels.findOne({ where: { server_id: newMember.guild.id } });
-        if (!VCTextChannel) return;
+        // if (!VCTextChannel) return;
         await newMember.guild.channels.fetch();
         let textChannel = newMember.guild.channels.cache.find(channel => channel.id == VCTextChannel.channel_id);
-        if (!textChannel) return;
+        // if (!textChannel) return;
         await textChannel.fetch();
         let channelPermOverride = textChannel.permissionOverwrites.cache.get(newMember.id);
 
@@ -43,7 +43,7 @@ module.exports = async (client, oldMember, newMember) => {
             if (channelPermOverride) {
                 return channelPermOverride.delete();
             } else {
-                return;
+                // return;
             };
         };
 
