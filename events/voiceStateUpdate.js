d8e0f349e67a4f67e9997b22d6243f04730753e4
Glazelf <gideon.steenbergen@gmail.com>
2021-07-29 19:40:56 +0200

    Update voiceStateUpdate.js

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -9,14 +9,17 @@ module.exports = async (client, oldMember, newMember) => {
 
         let user = client.users.cache.get(newMember.id);
         // if (user.bot) return;
+        console.log(`user bot ${user.bot}`)
 
         let VCTextChannel = await VCTextChannels.findOne({ where: { server_id: newMember.guild.id } });
+        console.log(`vctextchannel ${VCTextChannel}`)
         // if (!VCTextChannel) return;
         await newMember.guild.channels.fetch();
         let textChannel = newMember.guild.channels.cache.find(channel => channel.id == VCTextChannel.channel_id);
+        console.log(`textchannel ${textChannel}`)
         // if (!textChannel) return;
         await textChannel.fetch();
-        let channelPermOverride = textChannel.permissionOverwrites.get(newMember.id);
+        let channelPermOverride = textChannel.permissionOverwrites.cache.get(newMember.id);
         console.log(channelPermOverride)
 
         // Joined VC
@@ -32,7 +35,7 @@ module.exports = async (client, oldMember, newMember) => {
                 };
             } else {
                 try {
-                    let permOverwrites = textChanel.permissionOverwrites.get(user.id);
+                    let permOverwrites = textChanel.permissionOverwrites.cache.get(user.id);
                     return permOverwrites.create(user, {
                         VIEW_CHANNEL: true,
                         READ_MESSAGE_HISTORY: true, user: user
