a61d0626f7db4bd1425efc9c8717185ff8b08cf1
Glazelf <gideon.steenbergen@gmail.com>
2021-08-11 22:12:48 +0200

    add ninigi star interaction

---
+++ commands/interactions/ninigistar.js
@@ -0,0 +1,27 @@
+exports.run = (client, message) => {
+    // Import globals
+    let globalVars = require('../../events/ready');
+    try {
+        // Interaction only
+        if (message.type == 'DEFAULT') return;
+
+        try {
+            await message.react('⭐');
+        } catch (e) {
+            // console.log(e);
+        };
+
+        return sendMessage(client, message, `Starred this message for you!`);
+
+    } catch (e) {
+        // log error
+        const logger = require('../../util/logger');
+
+        logger(e, client, message);
+    };
+};
+
+module.exports.config = {
+    name: "Ninigi Star",
+    type: "MESSAGE"
+};
