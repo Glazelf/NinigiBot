a6f29696b791b93ece83445a3d35dec553f4f05b
Glazelf <gideon.steenbergen@gmail.com>
2021-08-11 16:46:40 +0200

    add id to return

--- commands/mod/mute.js
+++ commands/mod/mute.js
@@ -35,10 +35,10 @@ module.exports.run = async (client, message, args = []) => {
         let isMuted = member.roles.cache.find(r => r.name.toLowerCase() == muteRoleName);
         if (isMuted) {
             await member.roles.remove(role);
-            return sendMessage(client, message, `Unmuted **${member.user.tag}**.`);
+            return sendMessage(client, message, `Unmuted **${member.user.tag}** (${member.id}).`);
         } else {
             await member.roles.add(role);
-            sendMessage(client, message, `Muted **${member.user.tag}** for ${muteTime} minute(s).`);
+            sendMessage(client, message, `Muted **${member.user.tag}** (${member.id}) for ${muteTime} minute(s).`);
             // sets a timeout to unmute the user.
             setTimeout(async () => { await member.roles.remove(role) }, muteTime * 60 * 1000);
         };
