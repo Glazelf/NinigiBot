e831593e29665b0c3acfb121b84e9ea8b9528549
Glazelf <gideon.steenbergen@gmail.com>
2021-08-11 14:57:34 +0200

    fix logging permission issues
    Message sending permissions checks aren't easily catchable, so now have proper checks

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -10,24 +10,31 @@ module.exports = async (client, member) => {
         let log = member.guild.channels.cache.find(channel => channel.id == logChannel.channel_id);
         if (!log) return;
 
-        let user = client.users.cache.get(member.id);
+        let botMember = await message.guild.members.fetch(client.user.id);
+        if (log.permissionsFor(botMember).has("SEND_MESSAGES") && log.permissionsFor(botMember).has("EMBED_LINKS")) {
+            let user = client.users.cache.get(member.id);
 
-        let icon = member.guild.iconURL({ format: "png", dynamic: true });
-        let avatar = user.displayAvatarURL({ format: "png", dynamic: true });
+            let icon = member.guild.iconURL({ format: "png", dynamic: true });
+            let avatar = user.displayAvatarURL({ format: "png", dynamic: true });
 
-        let daysCreated = await checkDays(user.createdAt);
+            let daysCreated = await checkDays(user.createdAt);
 
-        const joinEmbed = new Discord.MessageEmbed()
-            .setColor(globalVars.embedColor)
-            .setAuthor(`Member Joined ❤️`, icon)
-            .setThumbnail(avatar)
-            .setDescription(`**${member.guild.name}** now has ${member.guild.memberCount} members.`)
-            .addField(`User: `, `${user} (${user.id})`)
-            .addField("Created at:", `${user.createdAt.toUTCString().substr(5,)}\n${daysCreated}`, true)
-            .setFooter(member.user.tag)
-            .setTimestamp();
+            const joinEmbed = new Discord.MessageEmbed()
+                .setColor(globalVars.embedColor)
+                .setAuthor(`Member Joined ❤️`, icon)
+                .setThumbnail(avatar)
+                .setDescription(`**${member.guild.name}** now has ${member.guild.memberCount} members.`)
+                .addField(`User: `, `${user} (${user.id})`)
+                .addField("Created at:", `${user.createdAt.toUTCString().substr(5,)}\n${daysCreated}`, true)
+                .setFooter(member.user.tag)
+                .setTimestamp();
 
-        return log.send({ content: user.toString(), embeds: [joinEmbed] });
+            return log.send({ content: user.toString(), embeds: [joinEmbed] });
+        } else if (log.permissionsFor(botMember).has("SEND_MESSAGES") && !log.permissionsFor(botMember).has("EMBED_LINKS")) {
+            return log.send({ content: `I lack permissions to send embeds in your log channel.` });
+        } else {
+            return;
+        };
 
     } catch (e) {
         // log error
--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -15,51 +15,58 @@ module.exports = async (client, message, newMessage) => {
         let log = message.guild.channels.cache.find(channel => channel.id == logChannel.channel_id);
         if (!log) return;
 
-        if (!message || !message.author) return;
-        if (message.content === newMessage.content) return;
+        let botMember = await message.guild.members.fetch(client.user.id);
+        if (log.permissionsFor(botMember).has("SEND_MESSAGES") && log.permissionsFor(botMember).has("EMBED_LINKS")) {
+            if (!message || !message.author) return;
+            if (message.content === newMessage.content) return;
 
-        let messageImage = null;
-        if (message.attachments.size > 0) messageImage = message.attachments.first().url;
+            let messageImage = null;
+            if (message.attachments.size > 0) messageImage = message.attachments.first().url;
 
-        if (!messageImage && !newMessage.content) return;
+            if (!messageImage && !newMessage.content) return;
 
-        let messageContent = message.content;
-        let newMessageContent = newMessage.content
-        if (messageContent.length > 1024) messageContent = `${messageContent.substring(0, 1020)}...`;
-        if (newMessageContent.length > 1024) newMessageContent = `${newMessageContent.substring(0, 1020)}...`;
+            let messageContent = message.content;
+            let newMessageContent = newMessage.content
+            if (messageContent.length > 1024) messageContent = `${messageContent.substring(0, 1020)}...`;
+            if (newMessageContent.length > 1024) newMessageContent = `${newMessageContent.substring(0, 1020)}...`;
 
-        autoMod(newMessage);
+            autoMod(newMessage);
 
-        let isReply = false;
-        if (message.reference) isReply = true;
+            let isReply = false;
+            if (message.reference) isReply = true;
 
-        if (isReply) {
-            try {
-                let ReplyChannel = await client.channels.cache.get(message.reference.channelID);
-                if (!ReplyChannel) ReplyChannel = await client.channels.fetch(message.reference.channelID);
-                var ReplyMessage = await ReplyChannel.messages.fetch(message.reference.messageID);
-            } catch (e) {
-                isReply = false;
+            if (isReply) {
+                try {
+                    let ReplyChannel = await client.channels.cache.get(message.reference.channelID);
+                    if (!ReplyChannel) ReplyChannel = await client.channels.fetch(message.reference.channelID);
+                    var ReplyMessage = await ReplyChannel.messages.fetch(message.reference.messageID);
+                } catch (e) {
+                    isReply = false;
+                };
             };
+
+            let avatar = message.author.displayAvatarURL({ format: "png", dynamic: true });
+
+            const updateEmbed = new Discord.MessageEmbed()
+                .setColor(globalVars.embedColor)
+                .setAuthor(`Message Edited ⚒️`, avatar)
+                .setDescription(`Message sent by ${message.author} (${message.author.id}) edited in ${message.channel}.`);
+            if (messageContent.length > 0) updateEmbed.addField(`Before:`, messageContent, false);
+            updateEmbed
+                .addField(`After:`, newMessageContent, false)
+            if (isReply) updateEmbed.addField(`Replying to:`, `"${ReplyMessage.content}"\n-${ReplyMessage.author}`);
+            updateEmbed
+                .addField(`Jump to message:`, `[Link](${message.url})`, false)
+                .setImage(messageImage)
+                .setFooter(message.author.tag)
+                .setTimestamp(message.createdTimestamp);
+
+            return log.send({ embeds: [updateEmbed] });
+        } else if (log.permissionsFor(botMember).has("SEND_MESSAGES") && !log.permissionsFor(botMember).has("EMBED_LINKS")) {
+            return log.send({ content: `I lack permissions to send embeds in your log channel.` });
+        } else {
+            return;
         };
-
-        let avatar = message.author.displayAvatarURL({ format: "png", dynamic: true });
-
-        const updateEmbed = new Discord.MessageEmbed()
-            .setColor(globalVars.embedColor)
-            .setAuthor(`Message Edited ⚒️`, avatar)
-            .setDescription(`Message sent by ${message.author} (${message.author.id}) edited in ${message.channel}.`);
-        if (messageContent.length > 0) updateEmbed.addField(`Before:`, messageContent, false);
-        updateEmbed
-            .addField(`After:`, newMessageContent, false)
-        if (isReply) updateEmbed.addField(`Replying to:`, `"${ReplyMessage.content}"\n-${ReplyMessage.author}`);
-        updateEmbed
-            .addField(`Jump to message:`, `[Link](${message.url})`, false)
-            .setImage(messageImage)
-            .setFooter(message.author.tag)
-            .setTimestamp(message.createdTimestamp);
-
-        return log.send({ embeds: [updateEmbed] });
 
     } catch (e) {
         // log error
