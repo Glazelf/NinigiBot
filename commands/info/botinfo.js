346f39204ac1984f5bb00c699ff7b8954c2304f2
Glazelf <gideon.steenbergen@gmail.com>
2021-08-14 11:11:56 +0200

    clarify string

--- commands/info/botinfo.js
+++ commands/info/botinfo.js
@@ -102,7 +102,7 @@ exports.run = async (client, message) => {
         if (client.shard) botEmbed.addField("Shards:", ShardUtil.count.toString(), true);
         botEmbed
             .addField("Servers:", totalGuilds.toString(), true)
-            .addField("Users:", totalMembers.toString(), true)
+            .addField("Total Users:", totalMembers.toString(), true)
             .addField("Average Users:", Math.round(totalMembers / totalGuilds).toString(), true)
             .addField("Channels:", channelCount.toString(), true)
             .addField("Uptime:", uptime, false)
