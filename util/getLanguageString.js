000285f4c586906e9a44d1e12ba83ea3a0057eb6
Glazelf <gideon.steenbergen@gmail.com>
2021-08-30 20:17:19 +0200

    Write out code for getting language strings
    Including catch for if string doesn't exist.

--- util/getLanguageString.js
+++ util/getLanguageString.js
@@ -1,8 +1,10 @@
-module.exports = async (language, stringName) => {
+module.exports = async (client, language, stringName) => {
     // Import globals
     let globalVars = require('../events/ready');
     try {
-        return;
+        let languageString = client.languages[language][stringName];
+        if (!languageString || languageString.length < 1) languageString = client.languages['en'][stringName];
+        return languageString;
 
     } catch (e) {
         // log error
