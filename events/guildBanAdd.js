1e3badabc6c87667df6fd83e5eb3e01e211cd770
Glazelf <gideon.steenbergen@gmail.com>
2021-09-02 20:57:00 +0200

    import languages

--- events/guildBanAdd.js
+++ events/guildBanAdd.js
@@ -4,11 +4,15 @@ module.exports = async (client, guildBan) => {
     try {
         const getLanguageString = require('../util/getLanguageString');
         const Discord = require("discord.js");
-        const { LogChannels } = require('../database/dbObjects');
+        const { LogChannels, Languages } = require('../database/dbObjects');
         let logChannel = await LogChannels.findOne({ where: { server_id: guildBan.guild.id } });
         if (!logChannel) return;
         let log = guildBan.guild.channels.cache.find(channel => channel.id == logChannel.channel_id);
         if (!log) return;
+
+        let dbLanguage = await Languages.findOne({ where: { server_id: guildBan.guild.id } });
+        let language = globalVars.language;
+        if (dbLanguage) language = dbLanguage.language;
 
         const fetchedLogs = await guildBan.guild.fetchAuditLogs({
             limit: 1,
