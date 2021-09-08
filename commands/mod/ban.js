6452f8ae313d3db38b27cc98dfad832a68e5ea59
Glazelf <gideon.steenbergen@gmail.com>
2021-09-08 20:46:43 +0200

    add time and also clean up code

--- commands/mod/ban.js
+++ commands/mod/ban.js
@@ -4,6 +4,7 @@ exports.run = async (client, message, args = []) => {
     try {
         const sendMessage = require('../../util/sendMessage');
         const isAdmin = require('../../util/isAdmin');
+        const getTime = require('../../util/getTime');
         let adminBool = await isAdmin(message.member, client);
         if (!message.member.permissions.has("BAN_MEMBERS") && !adminBool) return sendMessage(client, message, globalVars.lackPerms);
 
@@ -27,6 +28,8 @@ exports.run = async (client, message, args = []) => {
             reason = reason.join(' ');
         };
 
+        let dmString = `You've been banned from **${message.guild.name}** for the following reason: \`${reason}\``;
+
         let author;
         if (message.type == 'DEFAULT') {
             author = message.author;
@@ -35,6 +38,8 @@ exports.run = async (client, message, args = []) => {
         };
 
         await message.guild.bans.fetch();
+
+        let time = await getTime(client);
 
         // If user is found
         if (member) {
@@ -48,12 +53,13 @@ exports.run = async (client, message, args = []) => {
             if (existingBan) return sendMessage(client, message, `**${member.user.tag}** (${member.id}) is already banned.`);
 
             // Ban
+            banReturn = `Successfully banned <@${memberID}> (${memberID}) for the following reason: \`${reason}\`.`;
             try {
-                await user.send({ content: `You've been banned from **${message.guild.name}** for the following reason: \`${reason}\`` });
-                banReturn = `Successfully banned **${member.user.tag}** (${member.id}) for the following reason: \`${reason}\`. (DM Succeeded)`;
+                await user.send({ content: dmString });
+                banReturn = `${banReturn} (DM Succeeded)`;
             } catch (e) {
                 // console.log(e);
-                banReturn = `Successfully banned **${member.user.tag}** (${member.id}) for the following reason: \`${reason}\`. (DM Failed)`;
+                banReturn = `${banReturn} (DM Failed)`;
             };
             await member.ban({ days: 0, reason: `${reason} -${author.tag}` });
 
@@ -65,6 +71,8 @@ exports.run = async (client, message, args = []) => {
             if (existingBan) return sendMessage(client, message, `<@${memberID}> (${memberID}) is already banned.`);
 
             banReturn = `Successfully banned <@${memberID}> (${memberID}) for the following reason: \`${reason}\`.`;
+
+            // Ban
             try {
                 await message.guild.members.ban(memberID, { days: 0, reason: `${reason} -${author.tag}` });
             } catch (e) {
