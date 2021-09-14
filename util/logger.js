5fa9b4b510dccde2fa0d32076c4f7a0e9ad77851
Glazelf <gideon.steenbergen@gmail.com>
2021-09-14 12:42:37 +0200

    Flip booleans for detecting message content
    lmao im an idiot

--- util/logger.js
+++ util/logger.js
@@ -19,7 +19,7 @@ module.exports = async (exception, client, message = null) => {
 
         let exceptionCode = Discord.Formatters.codeBlock(exception);
         let messageContentCode = "";
-        if (!message || !message.content || message.content.length > 0) messageContentCode = Discord.Formatters.codeBlock(message.content);
+        if (message && message.content && message.content.length > 0) messageContentCode = Discord.Formatters.codeBlock(message.content);
 
         // log to dev channel
         let baseMessage;
