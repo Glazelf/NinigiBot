6ba30f8124e3092807e6627a736e58924594557f
Glazelf <gideon.steenbergen@gmail.com>
2021-09-29 20:53:16 +0200

    add general command body skeleton

--- commands/mod/rule.js
+++ commands/mod/rule.js
@@ -8,6 +8,32 @@ exports.run = async (client, message, args) => {
         let adminBool = await isAdmin(message.member, client);
         if (!adminBool) return sendMessage(client, message, globalVars.lackPerms);
 
+        const { RulesChannels, ServerRules, Prefixes } = require('../../database/dbObjects');
+        let prefix = await Prefixes.findOne({ where: { server_id: message.guild.id } });
+        if (prefix) {
+            prefix = prefix.prefix;
+        } else {
+            prefix = globalVars.prefix;
+        };
+
+        let isFAQ = message.content.startsWith(`${prefix}faq`); // Use this boolean to distinguish FAQ and rule behaviour and text
+        let ruleNumber;
+        let ruleChannel;
+        let ruleTitle;
+        let ruleBody;
+
+        if (!args[0]) return sendMessage(client, message, 'Please provide a rule number.');
+        if (isNaN(args[0])) {
+            // No number provided, check for channel instead, if no channel match return.
+        } else {
+            if (args[1]) {
+                ruleNumber = args[0];
+                // check title/body and set them to db
+            } else {
+                // show rule by number provided
+            };
+        };
+
         return sendMessage(client, message, `Rules time.`);
 
     } catch (e) {
