619b21677160cbf2dfdf67f817b75e4075778f6d
Theodore <tkluge@tkluge.net>
2021-10-01 11:38:46 -0400

    add roleDelete event

---
+++ events/roleDelete.js
@@ -0,0 +1,47 @@
+module.exports = async (client, role) => {
+    const logger = require('../util/logger');
+    // Import globals
+    let globalVars = require('./ready');
+    try {
+        const Discord = require("discord.js");
+        const { LogChannels } = require('../database/dbObjects');
+
+        let logChannel = await LogChannels.findOne({ where: { server_id: role.guild.id } });
+        if (!logChannel) return;
+        let log = role.guild.channels.cache.find(channel => channel.id == logChannel.channel_id);
+        if (!log) return;
+
+        let botMember = await role.guild.members.fetch(client.user.id);
+
+        if (log.permissionsFor(botMember).has("SEND_MESSAGES") && log.permissionsFor(botMember).has("EMBED_LINKS")) {
+            const fetchedLogs = await role.guild.fetchAuditLogs({
+                limit: 1,
+                type: 'ROLE_DELETE',
+            });
+            const deleteLog = fetchedLogs.entries.first();
+            let executor;
+            if (deleteLog) {
+                const { executor: deleteExecutor, target } = deleteLog;
+                if (target.id !== role.id) return;
+                executor = deleteExecutor;
+            }
+
+            const deleteEmbed = new Discord.MessageEmbed()
+                .setColor(globalVars.embedColor)
+                .setAuthor(`Role Deleted`)
+                .addField(`Role name: `, role.name)
+                .addField('Deleted by: ', `${executor.username}#${executor.discriminator} (${executor.id})`)
+                .setTimestamp();
+
+            return log.send({ embeds: [deleteEmbed] });
+        } else if (log.permissionsFor(botMember).has("SEND_MESSAGES") && !log.permissionsFor(botMember).has("EMBED_LINKS")) {
+            return log.send({ content: `I lack permissions to send embeds in your log channel.` });
+        } else {
+            return;
+        };
+
+    } catch (e) {
+        // Log error
+        logger(e, client);
+    };
+};
