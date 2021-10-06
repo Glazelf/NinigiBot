f785dd3e9c156aff9b5776d0a3852dba21e1935f
Glazelf <gideon.steenbergen@gmail.com>
2021-10-06 07:37:13 +0200

    fix target declaration

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -26,7 +26,7 @@ module.exports = async (client, message) => {
             });
             let deleteLog = fetchedLogs.entries.first();
             if (deleteLog) executor = deleteLog.executor;
-            if (deleteLog.extra.channel != message.channel || executor.target.id != message.member.id) executor = null
+            if (deleteLog.extra.channel != message.channel || deleteLog.target.id != message.member.id) executor = null;
         } catch (e) {
             // console.log(e);
             if (e.toString().includes("Missing Permissions")) executor = null;
