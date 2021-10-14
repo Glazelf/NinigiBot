db6dac2e48c25c2673e1de36330f776283ff0027
Glazelf <gideon.steenbergen@gmail.com>
2021-10-14 09:05:57 +0200

    fix member update time check

--- events/guildMemberUpdate.js
+++ events/guildMemberUpdate.js
@@ -39,7 +39,7 @@ module.exports = async (client, member, newMember) => {
                 });
                 let memberUpdateLog = fetchedLogs.entries.first();
                 if (memberUpdateLog) executor = memberUpdateLog.executor;
-                if (executor.id == member.id || fetchedLogs.createdTimestamp < (Date.now() - 5000)) executor = null;
+                if (executor.id == member.id || memberUpdateLog.createdTimestamp < (Date.now() - 5000)) executor = null;
             } catch (e) {
                 // console.log(e);
                 if (e.toString().includes("Missing Permissions")) executor = null;
