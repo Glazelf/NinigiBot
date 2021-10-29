352d1f6088f68d0a98e0db41a8c0c1ef8da8b802
Glazelf <gideon.steenbergen@gmail.com>
2021-10-29 23:20:23 +0200

    remove unused escape character

--- util/autoMod.js
+++ util/autoMod.js
@@ -29,12 +29,12 @@ module.exports = async (client, message) => {
     if (message.content.length > 0) messageContentBlock = Discord.Formatters.codeBlock(message.content);
 
     const genericLinks = [
-        ".*http(.)?:\/\/[^s]*.*\..{1,}"
+        ".*http(.)?:\/\/[^s]*.*..{1,}"
     ];
     let genericLinkRegex = new RegExp(genericLinks.join("|"), "i");
 
     const scamLinks = [
-        ".*http(.)?:\/\/(dicsord-nitro|steamnitro|discordgift|discordc|discorcl|dizcord|dicsord|dlscord|dlcsorcl)\.(com|org|ru|click|gift|net).*" // Discord gift links
+        ".*http(.)?:\/\/(dicsord-nitro|steamnitro|discordgift|discordc|discorcl|dizcord|dicsord|dlscord|dlcsorcl).(com|org|ru|click|gift|net).*" // Discord gift links
         // ".*http(.)?:\/\/.*\.ru.*" // Russian websites, should fix, re-add and enable this for any servers that aren't russian when language is done. Currently matches any URL containing "ru" after a period. Can't seem to replicate this on online regex testers though
     ];
     let scamRegex = new RegExp(scamLinks.join("|"), "i");
