a534c4e63bd90bc9f3f271ba326f8c88ff2cbbbf
Glazelf <gideon.steenbergen@gmail.com>
2021-11-01 21:41:58 +0100

    swap win and loss string to reflect correct result

--- commands/money/rps.js
+++ commands/money/rps.js
@@ -33,14 +33,16 @@ exports.run = async (client, message, args = []) => {
 
         // Randomize bot and compare choices
         let botChoice = rps[Math.floor(Math.random() * rps.length)];
-        let result = (rps.length+1 + rps.indexOf(playerChoice) - rps.indexOf(botChoice)) %rps.length+1;
-        let returnString = `Sorry. You picked **${playerChoice}** while I picked **${botChoice}**.
-        > You lose ${amount}${currency}.`;
+        let result = (rps.length + 1 + rps.indexOf(playerChoice) - rps.indexOf(botChoice)) % rps.length + 1;
+        let returnString = `Congratulations. You picked **${playerChoice}** while I picked **${botChoice}**.
+You win ${amount}${currency}.`;
         switch (result) {
             case 0:
                 return sendMessage(client, message, `It's a tie. We both picked **${playerChoice}**.`); break;
-            case 1: returnString = `Congratulations. You picked **${playerChoice}** while I picked **${botChoice}**.
-            > You win ${amount}${currency}.`; break;
+            case 1:
+                returnString = `Sorry. You picked **${playerChoice}** while I picked **${botChoice}**.
+You lose ${amount}${currency}.`;
+                break;
             case 2: amount = Math.abs(amount) * -1; break;
         }
 
