a0884f6f8576effcc8f174b4b57b5d62bb1c1794
Glazelf <gideon.steenbergen@gmail.com>
2021-12-04 12:35:05 +0100

    Add casing exception for RKS System

--- util/pokemon/capitalizeString.js
+++ util/pokemon/capitalizeString.js
@@ -5,10 +5,14 @@ module.exports = async (str) => {
     let splitStr = str;
     if (!exceptionsLowercase.includes(str)) splitStr = str.split('-');
 
-    for (let i = 0; i < splitStr.length; i++) {
-        // You do not need to check if i is larger than splitStr length, as your for does that for you
-        // Assign it back to the array
-        splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+    if (str.toLowerCase() == "rks-system") {
+        splitStr = "RKS System";
+    } else {
+        for (let i = 0; i < splitStr.length; i++) {
+            // You do not need to check if i is larger than splitStr length, as your for does that for you
+            // Assign it back to the array
+            splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+        };
     };
 
     // Return the joined string
