5d4371042b351060bffe1092a9174347f74175ac
Glazelf <gideon.steenbergen@gmail.com>
2021-12-08 22:29:20 +0100

    pre-emptive banner rewrite
    might not be accurate to final implementation

--- commands/info/userinfo.js
+++ commands/info/userinfo.js
@@ -128,7 +128,7 @@ exports.run = async (client, message, args = []) => {
         let serverAvatar = member.displayAvatarURL(globalVars.displayAvatarSettings);
         let avatar = user.displayAvatarURL(globalVars.displayAvatarSettings);
         let banner = null;
-        if (user.banner) banner = user.bannerURL({ format: "png", dynamic: true, size: 256 });
+        if (user.banner) banner = member.bannerURL(globalVars.displayAvatarSettings);
 
         // Accent color
         let embedColor = globalVars.embedColor;
