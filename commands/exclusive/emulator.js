8141791ea1ec08c701d14f60c43fe7a34f79f8ac
Glazelf <gideon.steenbergen@gmail.com>
2021-12-12 00:03:56 +0100

    load absolute path

--- commands/exclusive/emulator.js
+++ commands/exclusive/emulator.js
@@ -6,10 +6,25 @@ exports.run = async (client, message) => {
         const sendMessage = require('../../util/sendMessage');
         const isAdmin = require('../../util/isAdmin');
 
-        let adminBool = await isAdmin(client, message.member);
-        if (message.guild.id !== "861884184787550218" || !adminBool) return;
+        const fs = require('fs');
+        const path = require('path');
+        const Gameboy = await import('serverboy');
+        const gameboy = new Gameboy.default();
 
-        return sendMessage(client, message, returnString);
+        let currentRomName = "PokemonCrystal.gbc"; // Rom name
+        let absoluteRomPath = path.resolve(`./assets/roms/${currentRomName}`);
+        let rom = fs.readFileSync(absoluteRomPath); // Read Rom
+
+        let adminBool = await isAdmin(client, message.member);
+        if (message.guild.id !== client.config.botServerID || !adminBool) return;
+
+        // Emulator channel
+        // if (message.channel.id !== "919360126450819102") return;
+
+        console.log(Gameboy)
+        console.log(rom)
+
+        return sendMessage(client, message, "test");
 
     } catch (e) {
         // Log error
