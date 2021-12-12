aa96facad61a0b9b94e8a1c88d77dc5bbcc4dd62
Glazelf <gideon.steenbergen@gmail.com>
2021-12-12 01:12:22 +0100

    add some initial logic
    current hurdle is loading images properly

--- commands/exclusive/emulator.js
+++ commands/exclusive/emulator.js
@@ -4,27 +4,55 @@ exports.run = async (client, message) => {
     let globalVars = require('../../events/ready');
     try {
         const sendMessage = require('../../util/sendMessage');
+        const Discord = require("discord.js");
         const isAdmin = require('../../util/isAdmin');
 
         const fs = require('fs');
         const path = require('path');
+        const { promisify } = require('util');
+        const Canvas = require('canvas');
+        const { PassThrough } = require('stream');
+        const stream = new PassThrough();
         const Gameboy = await import('serverboy');
         const gameboy = new Gameboy.default();
 
         let currentRomName = "PokemonCrystal.gbc"; // Rom name
         let absoluteRomPath = path.resolve(`./assets/roms/${currentRomName}`);
-        let rom = fs.readFileSync(absoluteRomPath); // Read Rom
+        let absoluteScreenPath = path.resolve(`./assets/images/roms/screenshot.png`);
+        let rom = fs.readFileSync(absoluteRomPath); // Read rom
 
         let adminBool = await isAdmin(client, message.member);
         if (message.guild.id !== client.config.botServerID || !adminBool) return;
 
         // Emulator channel
-        // if (message.channel.id !== "919360126450819102") return;
+        let emuChannelID = "919360126450819102";
+        // if (message.channel.id !== emuChannelID) return;
 
-        console.log(Gameboy)
-        console.log(rom)
+        sendMessage(client, message, `Starting emulator...`);
+        gameboy.loadRom(rom);
 
-        return sendMessage(client, message, "test");
+        setTimeout(async function () {
+            // Whatever custom logic you need
+            let memory = gameboy.getMemory();
+            if (memory[3000] === 0) {
+                gameboy.pressKeys([Gameboy.KEYMAP.RIGHT]);
+            };
+            gameboy.doFrame();
+
+            await sendScreenshot(gameboy, absoluteScreenPath);
+        }, 0);
+
+        async function sendScreenshot(gameboy, absoluteScreenPath) {
+            const writeFileAsync = promisify(fs.writeFile);
+            let screenData = gameboy.getScreen();
+            let screenBuffer = Buffer.from(screenData);
+            console.log(screenBuffer);
+            await writeFileAsync(absoluteScreenPath, screenBuffer, function (e) {
+                if (e) logger(e, client, message);
+            });
+            let imageCanvas = await Canvas.loadImage(absoluteScreenPath);
+            sendMessage(client, message, null, null, imageCanvas);
+        };
 
     } catch (e) {
         // Log error
