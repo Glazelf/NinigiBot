ba39b0f21551dd527dc24a3a15afedf3841aa259
Glazelf <gideon.steenbergen@gmail.com>
2021-12-12 19:18:07 +0100

    remove redundant code
    getting muted role etc.

--- commands/mod/mute.js
+++ commands/mod/mute.js
@@ -6,11 +6,10 @@ exports.run = async (client, message, args = []) => {
         const sendMessage = require('../../util/sendMessage');
         const isAdmin = require('../../util/isAdmin');
         let adminBool = await isAdmin(client, message.member);
-        if (!message.member.permissions.has("MANAGE_ROLES") && !adminBool) return sendMessage(client, message, globalVars.lackPerms);
+        if (!message.member.permissions.has("MANAGE_MEMBERS") && !adminBool) return sendMessage(client, message, globalVars.lackPerms);
 
-        // Minutes the user is muted
+        // Default time the user is muted in minutes
         let muteTime = 60;
-        let muteRoleName = "muted";
 
         // Get user
         if (!args[0]) return sendMessage(client, message, `Please provide a mentioned user as an argument.`);
@@ -32,25 +31,22 @@ exports.run = async (client, message, args = []) => {
             } catch (e) {
                 // console.log(e);
             };
-            if (!member) member = message.guild.members.cache.find(member => member.user.username.toLowerCase() == args[0].toString().toLowerCase());
         };
         if (!member) return sendMessage(client, message, `Please use a proper mention if you want to mute someone.`);
-        const role = member.guild.roles.cache.find(role => role.name.toLowerCase() == muteRoleName);
-        if (!role) return sendMessage(client, message, `There is no mute role. In order to mute someone, you need to create a role called "Muted".`);
 
         // Check permissions
         let userRole = message.member.roles.highest;
         let targetRole = member.roles.highest;
-        if (targetRole.position >= userRole.position && message.guild.ownerId !== message.member.id) return sendMessage(client, message, `You don't have a high enough role to mute **${member.user.tag}** (${member.id}).`);
+        if (targetRole.position >= userRole.position && !adminBool) return sendMessage(client, message, `You don't have a high enough role to mute **${member.user.tag}** (${member.id}).`);
 
         // If already muted, unmute. If not, mute.
         let isMuted = member.roles.cache.find(r => r.name.toLowerCase() == muteRoleName);
         if (isMuted) {
-            // Replace with timeout logic
+            // Remove timeout (if possible)
             // await member.roles.remove(role);
             return sendMessage(client, message, `Unmuted **${member.user.tag}** (${member.id}).`);
         } else {
-            // Remove timeout (if possible)
+            // Timeout logic
             // await member.roles.add(role);
             // sendMessage(client, message, `Muted **${member.user.tag}** (${member.id}) for ${muteTime} minute(s).`);
             // // sets a timeout to unmute the user.
