32008aa5f4811d42dfa5ecbaac6a16c77e59e8b6
Glazelf <gideon.steenbergen@gmail.com>
2021-12-13 13:30:40 +0100

    globalize and initiate switch tree

--- commands/exclusive/emulator.js
+++ commands/exclusive/emulator.js
@@ -6,68 +6,77 @@ exports.run = async (client, message, args = []) => {
         const sendMessage = require('../../util/sendMessage');
         const Discord = require("discord.js");
         const isAdmin = require('../../util/isAdmin');
+        let adminBool = await isAdmin(client, message.member);
+
+        if (message.guild.id !== globalVars.botServerID) return;
 
         const fs = require('fs');
         const path = require('path');
         const Canvas = require('canvas');
         const PNG = require('pngjs').PNG;
-        const Gameboy = require('serverboy');
-        client.gameboy = new Gameboy();
-
-        let currentRomName = "PokemonBlue"; // Rom file name. Supported roms: .gb & .gbc
-        let romType = ".gb";
-        let absoluteRomPath = path.resolve(`./assets/roms/${currentRomName}${romType}`);
-        let absoluteSavePath = absoluteRomPath.replace(romType, ".sav");
-
-        // Read rom
-        let rom = null;
-        try {
-            rom = fs.readFileSync(absoluteRomPath);
-        } catch (e) {
-            // console.log(e);
-            return sendMessage(client, message, `No rom could be found!`);
-        };
-        // Read save
-        let save = null;
-        try {
-            save = fs.readFileSync(absoluteSavePath);
-        } catch (e) {
-            // console.log(e);
-        };
-
-        let adminBool = await isAdmin(client, message.member);
-        if (message.guild.id !== client.config.botServerID || !adminBool) return;
 
         // Emulator channel
-        let emuChannelID = "919360126450819102";
-        // if (message.channel.id !== emuChannelID) return;
+        let emuChannel = "919360126450819102";
 
-        // Loading rom
-        sendMessage(client, message, `Starting emulator...`);
+        if (!args[0]) return sendMessage(client, message, `This command requires a subcommand.`);
+        let subCommand = args[0].toLowerCase();
 
-        client.gameboy.loadRom(rom, save);
+        switch (subCommand) {
+            case "start":
+                if (message.guild.id !== client.config.botServerID || !adminBool) return;
+                // Init global variables
+                const Gameboy = require('serverboy');
+                client.gameboy = new Gameboy(); // Global instance of emulator
+                client.gameboyInput = false; // Whether or not an input has been made since last screenshot.
 
-        // Logic examples
-        let memory = gameboy.getMemory();
-        if (memory[3000] === 0) {
-            client.gameboy.pressKeys([Gameboy.KEYMAP.RIGHT]);
+                let currentRomName = "PokemonBlue"; // Rom file name. Supported roms: .gb & .gbc
+                let romType = ".gb";
+                let absoluteRomPath = path.resolve(`./assets/roms/${currentRomName}${romType}`);
+                let absoluteSavePath = absoluteRomPath.replace(romType, ".sav");
+
+                // Read rom
+                let rom = null;
+                try {
+                    rom = fs.readFileSync(absoluteRomPath);
+                } catch (e) {
+                    // console.log(e);
+                    return sendMessage(client, message, `No rom could be found!`);
+                };
+                // Read save
+                let save = null;
+                try {
+                    save = fs.readFileSync(absoluteSavePath);
+                } catch (e) {
+                    // console.log(e);
+                };
+
+                // Loading rom
+                sendMessage(client, message, `Starting emulator...`);
+                client.gameboy.loadRom(rom, save);
+
+                // Logic examples
+                let memory = gameboy.getMemory();
+                if (memory[3000] === 0) {
+                    client.gameboy.pressKeys([Gameboy.KEYMAP.RIGHT]);
+                };
+                client.gameboy.pressKey(Gameboy.KEYMAP.A);
+
+                // Advance frame
+                setInterval(function () {
+                    client.gameboy.doFrame();
+                }, 1000 / 60); // 60 FPS
+
+                // Sending screenshot
+                setInterval(function () {
+                    if (client.gameboyInput) sendScreenshot(client.gameboy);
+                }, 10000); // 10 seconds, but only if an input has been made (WIP)
+
+                // Auto-saving
+                setInterval(function () {
+                    saveGame(client.gameboy, absoluteSavePath)
+                }, 900000) // 15 minutes
+                break;
         };
-        client.gameboy.pressKey(Gameboy.KEYMAP.A);
-
-        // Advance frame
-        setInterval(function () {
-            client.gameboy.doFrame();
-        }, 1000 / 60); // 60 FPS
-
-        // Sending screenshot
-        setInterval(function () {
-            sendScreenshot(client.gameboy);
-        }, 10000); // 10 seconds, but only if an input has been made (WIP)
-
-        // Auto-saving
-        setInterval(function () {
-            saveGame(client.gameboy, absoluteSavePath)
-        }, 900000) // 15 minutes
 
         function sendScreenshot() {
             let screen = client.gameboy.getScreen();
@@ -88,6 +97,7 @@ exports.run = async (client, message, args = []) => {
             };
             let buffer = PNG.sync.write(png);
 
+            client.gameboyInput = false;
             sendMessage(client, message, "test", null, buffer);
         };
 
