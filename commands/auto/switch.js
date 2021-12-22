8002b973bb6e7be63ad9b87189020b9bc3b0b18a
Glazelf <gideon.steenbergen@gmail.com>
2021-12-22 14:15:54 +0100

    update command for slash commands

--- commands/auto/switch.js
+++ commands/auto/switch.js
@@ -1,4 +1,4 @@
-exports.run = async (client, message, args = []) => {
+exports.run = async (client, interaction, args = interaction.options._hoistedOptions) => {
     const logger = require('../../util/logger');
     // Import globals
     let globalVars = require('../../events/ready');
@@ -6,25 +6,28 @@ exports.run = async (client, message, args = []) => {
         const sendMessage = require('../../util/sendMessage');
         const { bank } = require('../../database/bank');
 
-        let switchCodeGet = await bank.currency.getSwitchCode(message.member.id);
+        let switchCodeGet = await bank.currency.getSwitchCode(interaction.member.id);
+        let fcArgument = args.find(element => element.name == 'switch-fc');
+        let switchFC;
+        if (fcArgument) switchFC = fcArgument.value;
 
         // Present code if no code is supplied as an argument
-        if (args.length < 1) {
-            if (switchCodeGet && switchCodeGet !== "None") return sendMessage(client, message, `Your Nintendo Switch friend code is ${switchCodeGet}.`);
-            return sendMessage(client, message, `Please specify a valid Nintendo Switch friend code.`);
+        if (!switchFC) {
+            if (switchCodeGet && switchCodeGet !== "None") return sendMessage(client, interaction, `Your Nintendo Switch friend code is ${switchCodeGet}.`);
+            return sendMessage(client, interaction, `Please specify a valid Nintendo Switch friend code.`);
         };
 
         // Check and sanitize input
-        let switchcode = /^(?:SW)?[- ]?([0-9]{4})[- ]?([0-9]{4})[- ]?([0-9]{4})$/.exec(args);
-        if (!switchcode) return sendMessage(client, message, `Please specify a valid Nintendo Switch friend code.`);
+        let switchcode = /^(?:SW)?[- ]?([0-9]{4})[- ]?([0-9]{4})[- ]?([0-9]{4})$/.exec(switchFC);
+        if (!switchcode) return sendMessage(client, interaction, `Please specify a valid Nintendo Switch friend code.`);
 
         switchcode = `SW-${switchcode[1]}-${switchcode[2]}-${switchcode[3]}`;
-        bank.currency.switchCode(message.member.id, switchcode);
-        return sendMessage(client, message, `Successfully updated your Nintendo Switch friend code.`);
+        bank.currency.switchCode(interaction.member.id, switchcode);
+        return sendMessage(client, interaction, `Successfully updated your Nintendo Switch friend code.`);
 
     } catch (e) {
         // Log error
-        logger(e, client, message);
+        logger(e, client, interaction);
     };
 };
 
