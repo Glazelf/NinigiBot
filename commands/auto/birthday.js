72e4603a65eac42a49da8b607faf848cad6fc2b7
Glazelf <gideon.steenbergen@gmail.com>
2021-12-22 13:02:05 +0100

    refactor birthday to work with slash commands
    mostly renaming

--- commands/auto/birthday.js
+++ commands/auto/birthday.js
@@ -1,4 +1,4 @@
-exports.run = async (client, message, args = []) => {
+exports.run = async (client, interaction, args = interaction.options._hoistedOptions) => {
     const logger = require('../../util/logger');
     // Import globals
     let globalVars = require('../../events/ready');
@@ -7,15 +7,15 @@ exports.run = async (client, message, args = []) => {
         const { bank } = require('../../database/bank');
 
         // Check and sanitize birthday
-        let birthday = /^(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])/.exec(args);
-        if (!birthday) return sendMessage(client, message, `Please specify a valid birthday in dd-mm format.`);
+        let birthday = /^(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])/.exec(args[0].value);
+        if (!birthday) return sendMessage(client, interaction, `Please specify a valid birthday in dd-mm format.`);
 
-        bank.currency.birthday(message.member.id, birthday[1] + birthday[2]);
-        return sendMessage(client, message, `Successfully updated your birthday.`);
+        bank.currency.birthday(interaction.member.id, birthday[1] + birthday[2]);
+        return sendMessage(client, interaction, `Successfully updated your birthday.`);
 
     } catch (e) {
         // Log error
-        logger(e, client, message);
+        logger(e, client, interaction);
     };
 };
 
