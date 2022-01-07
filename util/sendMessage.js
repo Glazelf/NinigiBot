98c2ee92d8268d3d2a6614461f47f67c94292e79
Glazelf <gideon.steenbergen@gmail.com>
2022-01-07 23:06:50 +0100

    add a check to see if components is not empty

--- util/sendMessage.js
+++ util/sendMessage.js
@@ -23,7 +23,7 @@ module.exports = async (client, message, replyText, embeds = null, files = null,
         };
 
         // Don't add components to slash commands unless specifically told to do so
-        if (components && ((slashComponents && message.type == 'APPLICATION_COMMAND') || message.type != 'APPLICATION_COMMAND')) {
+        if (components && components.components.length != 0 && ((slashComponents && message.type == 'APPLICATION_COMMAND') || message.type != 'APPLICATION_COMMAND')) {
             // Components, i.e. buttons
             if (Array.isArray(components)) {
                 messageObject['components'] = components;
