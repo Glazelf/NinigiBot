748578062ab512bdb8e03fb768da68f6681decb8
Glazelf <gideon.steenbergen@gmail.com>
2022-01-30 00:00:03 +0100

    update sysbots

--- commands/exclusive/sysbot.js
+++ commands/exclusive/sysbot.js
@@ -11,8 +11,8 @@ exports.run = async (client, message) => {
         // Bot hosts
         let Glaze = await client.users.fetch(client.config.ownerID);
         let Artic = await client.users.fetch("353184551096418316");
-        let Mingus = await client.users.fetch("175994984493744129");
         let Phoenix = await client.users.fetch("289213131094229002");
+        // let Mingus = await client.users.fetch("175994984493744129");
         // let Flare = await client.users.fetch("592353588685307914");
         // let Shion = client.users.fetch("685608164506337351");
 
@@ -20,13 +20,13 @@ exports.run = async (client, message) => {
         // PKM
         let Konohana = await message.guild.members.fetch("696086046685003786", { force: true }); // Glaze
         let Glaceon = await message.guild.members.fetch("777555048104067082", { force: true }); // Artic
-        let BingoBot = await message.guild.members.fetch("898605924057481288", { force: true }); // Mingus
         let PsyBot = await message.guild.members.fetch("909111019585028177", { force: true }); // Phoenix
+        // let BingoBot = await message.guild.members.fetch("898605924057481288", { force: true }); // Mingus
         // let Flar3 = await message.guild.members.fetch("734052437811527784", { force: true }); // Flare
         // let Arkos = await message.guild.members.fetch("702604221714923691", { force: true }); // Shion
         // ACNH
         let Ribbot = await message.guild.members.fetch("739823632267608135", { force: true }); // Glaze
-        let TimTomBot = await message.guild.members.fetch("898608573943263312", { force: true }); // Mingus
+        // let TimTomBot = await message.guild.members.fetch("898608573943263312", { force: true }); // Mingus
         // let ACFlare = await message.guild.members.fetch("792174299716386867", { force: true }); // Flare
 
         let onlineString = "**Online**";
@@ -35,18 +35,14 @@ exports.run = async (client, message) => {
         // PKM
         let KonohanaStatus = onlineString;
         let GlaceonStatus = onlineString;
-        let BingoBotStatus = onlineString;
         let PsyBotStatus = onlineString;
         // ACNH
         let RibbotStatus = onlineString;
-        let TimTomBotStatus = onlineString;
 
         if (!Konohana || !Konohana.presence || Konohana.presence.status == offlineStatus) KonohanaStatus = offlineString;
         if (!Glaceon || !Glaceon.presence || Glaceon.presence.status == offlineStatus) GlaceonStatus = offlineString;
-        if (!BingoBot || !BingoBot.presence || BingoBot.presence.status == offlineStatus) BingoBotStatus = offlineString;
         if (!PsyBot || !PsyBot.presence || PsyBot.presence.status == offlineStatus) PsyBotStatus = offlineString
         if (!Ribbot || !Ribbot.presence || Ribbot.presence.status == offlineStatus) RibbotStatus = offlineString;
-        if (!TimTomBot || !TimTomBot.presence || TimTomBot.presence.status == offlineStatus) TimTomBotStatus = offlineString;
 
         // Buttons
         let sysbotButtons = new Discord.MessageActionRow()
@@ -61,11 +57,9 @@ Format: Bot Name (optional mode) (prefix): status (\`Host#0001\`)
 **Pokémon bots:**
 ${Konohana.nickname || Konohana.user.username} (&): ${KonohanaStatus} (\`${Glaze.tag}\`)
 ${Glaceon.nickname || Glaceon.user.username} (.): ${GlaceonStatus} (\`${Artic.tag}\`)
-${BingoBot.nickname || BingoBot.user.username} (%): ${BingoBotStatus} (\`${Mingus.tag}\`)
 ${PsyBot.nickname || PsyBot.user.username} (^): ${PsyBotStatus} (\`${Phoenix.tag}\`)
 **ACNH bots:**
 ${Ribbot.nickname || Ribbot.user.username} (;): ${RibbotStatus} (\`${Glaze.tag}\`)
-${TimTomBot.nickname || TimTomBot.user.username} (%): ${TimTomBotStatus} (\`${Mingus.tag}\`)
 
 Before asking a question make sure your question isn't already answered in either ${message.guild.rulesChannel} or <#${globalVars.botChannelID}>.
 **Bots will be hosted when hosts feel like it and have time**, there is no schedule. Asking or even begging for bots is often useless and can be annoying.
