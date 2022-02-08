2fb0f6d48b51ac82536c24eadb820ce900cb031c
Glazelf <gideon.steenbergen@gmail.com>
2022-02-08 19:36:55 +0100

    restructure roleinfo to work with role input
    untested

--- commands/info/roleinfo.js
+++ commands/info/roleinfo.js
@@ -7,41 +7,17 @@ exports.run = async (client, interaction, args = interaction.options._hoistedOpt
         const Discord = require("discord.js");
         let DefaultEmbedColor = globalVars.embedColor;
 
-        if (!args[0]) return sendMessage({ client: client, interaction: interaction, content: `Please provide a role name or ID.` });
-
         // Split off command
-        let input = args.join(" ");
+        let role = args.find(element => element.name == "role").value;
 
-        let user = interaction.member.user;
+        let user = interaction.user;
 
         // Author avatar
         let avatar = interaction.member.displayAvatarURL(globalVars.displayAvatarSettings);
 
-        // Check for role
-        let role = interaction.guild.roles.cache.find(role => role.name.toLowerCase() === input.toLowerCase());
-        if (!role) role = await interaction.guild.roles.fetch(input);
-
         let roleEmbed = new Discord.MessageEmbed()
             .setFooter({ text: user.tag })
             .setTimestamp();
-
-        if (input.toLowerCase() == "none" && !role) {
-            let fetchedMembers = await interaction.guild.members.fetch();
-            let noRoleMembers = 0;
-            fetchedMembers.forEach(member => {
-                if (member.roles.cache.size == 1) {
-                    noRoleMembers += 1;
-                };
-            });
-            roleEmbed
-                .setColor(DefaultEmbedColor)
-                .setAuthor({ name: `Users in ${interaction.guild.name} without a role`, iconURL: avatar })
-                .addField("Members:", noRoleMembers.toString(), true);
-
-            return sendMessage({ client: client, interaction: interaction, embeds: roleEmbed });
-        };
-
-        if (!role) return sendMessage({ client: client, interaction: interaction, content: `I couldn't find that role. Make sure you provide a valid name or ID.` });
 
         // Role visuals
         let icon = role.iconURL(globalVars.displayAvatarSettings);
@@ -53,9 +29,9 @@ exports.run = async (client, interaction, args = interaction.options._hoistedOpt
 
         // Properties
         let roleProperties = "";
-        if (role.hoist) roleProperties = `${roleProperties}\nSorted seperately`;
-        if (role.mentionable) roleProperties = `${roleProperties}\nCan be mentioned`;
-        if (role.managed) roleProperties = `${roleProperties}\nManaged by integration`;
+        if (role.hoist) roleProperties = `${roleProperties}Sorted seperately\n`;
+        if (role.mentionable) roleProperties = `${roleProperties}Can be mentioned\n`;
+        if (role.managed) roleProperties = `${roleProperties}Managed by integration\n`;
         if (roleProperties.length == 0) roleProperties = "None";
 
         // Embed
@@ -81,9 +57,9 @@ module.exports.config = {
     name: "roleinfo",
     description: "Sends info about a role.",
     options: [{
-        name: "role-name",
-        type: "STRING",
-        description: "Specify role by name or ID."
-
+        name: "role",
+        type: "ROLE",
+        description: "Specify role.",
+        required: true
     }]
 };
