60472769b9a5d150c5a684ee7399385865ee28cb
Glazelf <gideon.steenbergen@gmail.com>
2022-05-01 10:01:13 +0200

    only reply on error to message commands
    pretty much useless (and also doesnt work) for slash commands

--- util/logger.js
+++ util/logger.js
@@ -45,7 +45,7 @@ ${messageContentCode}` : `An error occurred:\n${exceptionCode}`;
                 let errorReturnText = `An error has occurred. 
 The error has already been logged but please also report this as an issue on Github: 
 <https://github.com/Glazelf/NinigiBot/issues>`;
-                if (message) message.reply(errorReturnText);
+                if (message && message.type == 'DEFAULT') message.reply(errorReturnText);
                 return devChannel.send({ content: baseMessage });
             };
         };
