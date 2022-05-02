23df3768b3bb03eb3ba4c85ed474df28f939e410
Glazelf <gideon.steenbergen@gmail.com>
2022-05-02 21:42:08 +0200

    Add guidance for faulty inputs

--- commands/other/minesweeper.js
+++ commands/other/minesweeper.js
@@ -7,23 +7,43 @@ exports.run = async (client, interaction, args = interaction.options._hoistedOpt
         const Discord = require("discord.js");
         const Minesweeper = require('discord.js-minesweeper');
 
+        let correctionString = "";
         let rows = 5;
         let columns = 5;
+        let axisFloor = 2;
+        let axisCap = 5;
+        let minesFloor = 1;
+        let minesCapPercentage = 50;
         let rowsArg = args.find(element => element.name == "rows");
         let columnsArg = args.find(element => element.name == "columns");
         if (rowsArg) {
-            if (rowsArg.value <= 5 && rowsArg.value >= 2) rows = rowsArg.value;
+            if (rowsArg.value > 5 || rowsArg.value < 2) {
+                correctionString += `\nAmount of rows has to be between ${axisFloor} and ${axisCap}.`;
+                if (rowsArg.value > 5) rows = 5;
+                if (rowsArg.value < 2) rows = 2
+            } else {
+                rows = rowsArg.value;
+            };
         };
         if (columnsArg) {
-            if (columnsArg.value <= 5 && columnsArg.value >= 2) columns = columnsArg.value;
+            if (columnsArg.value > 5 || columnsArg.value < 2) {
+                correctionString += `\nAmount of columns has to be between ${axisFloor} and ${axisCap}.`;
+                if (columnsArg.value > 5) columns = 5;
+                if (columnsArg.value < 2) columns = 2;
+            } else {
+                columns = columnsArg.value;
+            };
         };
 
         let mines = Math.ceil((rows * columns) / 5); // ~20% mine ratio by default
         let minesArg = args.find(element => element.name == "mines");
         if (minesArg) {
-            if (minesArg.value >= Math.ceil((rows * columns) / 100 * 50 - 1)) {
-                mines = Math.ceil((rows * columns) / 100 * 50 - 1); // Cap at 50% mine ratio (otherwise board generation fails idk why)
-            } else if (minesArg.value >= 1) {
+            let minesCap = Math.ceil((rows * columns) / 2 - 1); // Cap at 50% mine ratio (otherwise board generation fails idk why)
+            if (minesArg.value > minesCap || minesArg.value < minesFloor) {
+                correctionString += `\nAmount of mines has to be between ${minesFloor} mine and ${minesCapPercentage}% (${minesCap} in this scenario) of the board.`;
+                if (minesArg.value > minesCap) mines = minesCap;
+                if (minesArg.value < minesFloor) mines = minesFloor;
+            } else {
                 mines = minesArg.value;
             };
         };
@@ -59,7 +79,8 @@ exports.run = async (client, interaction, args = interaction.options._hoistedOpt
             buttonRowArray.push(buttonRow);
         });
 
-        let returnString = `Here is your minesweeper grid, **${interaction.member.user.tag}**.`;
+        let returnString = `Here is your minesweeper grid!`;
+        if (correctionString.length > 0) returnString += `\n${correctionString}`;
         return sendMessage({ client: client, interaction: interaction, content: returnString, components: buttonRowArray });
 
     } catch (e) {
