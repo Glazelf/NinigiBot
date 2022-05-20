72cd408d9bb416fa3ed529d2208f2fce544c3369
Glazelf <gideon.steenbergen@gmail.com>
2022-05-20 10:01:08 +0200

    clean up google command

--- commands/other/google.js
+++ commands/other/google.js
@@ -6,29 +6,9 @@ exports.run = async (client, interaction, args = interaction.options._hoistedOpt
         const sendMessage = require('../../util/sendMessage');
         const Discord = require("discord.js");
 
-        let replyMessage;
-        let input;
-        let questionAskUser;
-        let interaction = null;
-
-        // Reply
-        if (message.reference) {
-            if (message.reference) replyMessage = await message.channel.messages.fetch(message.reference.messageId);
-            input = replyMessage.content;
-            questionAskUser = replyMessage.author;
-
-            // Text in command
-        } else {
-            if (message.type == "APPLICATION_COMMAND") {
-                interaction = message;
-                message = await message.channel.messages.fetch(args[0]);
-                input = message.content;
-                questionAskUser = message.member;
-            } else {
-                questionAskUser = `**${message.author.tag}**`;
-                input = args.join(" ");
-            };
-        };
+        let message = await interaction.channel.messages.fetch(interaction.targetId);
+        let input = message.content;
+        let questionAskUser = message.author;
 
         // Swap interaction and message if command is used through apps menu, makes the interaction finish properly by replying to the interaction instead of the message.
         if (interaction) message = interaction;
