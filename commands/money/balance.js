561a41da511de4f72dbd41990d8714bec4d44e8c
Glazelf <gideon.steenbergen@gmail.com>
2022-05-20 09:50:29 +0200

    update balance command

--- commands/money/balance.js
+++ commands/money/balance.js
@@ -6,34 +6,11 @@ exports.run = async (client, interaction, args = interaction.options._hoistedOpt
         const sendMessage = require('../../util/sendMessage');
         const { bank } = require('../../database/bank');
 
-        let target;
-        if (message.mentions && (message.mentions.members.size > 0 || message.mentions.repliedUser)) {
-            target = message.mentions.users.first();
-        };
-
-        // Get user
-        if (!target && args[0]) {
-            let userID = args[0];
-            try {
-                target = await client.users.fetch(userID);
-            } catch (e) {
-                // console.log(e);
-                target = interaction.user;
-            };
-        };
-
-        if (!target) target = interaction.user;
-
-        let member;
-        try {
-            member = await message.guild.members.fetch(target);
-        } catch (e) {
-            // console.log(e);
-            return sendMessage({ client: client, interaction: interaction, content: `No member information could be found for this user.` });
-        };
+        let target = interaction.user;
+        let member = await message.guild.members.fetch(target);
 
         let dbBalance = await bank.currency.getBalance(target.id);
-        return sendMessage({ client: client, interaction: interaction, content: `**${target.tag}** has ${Math.floor(dbBalance)}${globalVars.currency}.` });
+        return sendMessage({ client: client, interaction: interaction, content: `You have ${Math.floor(dbBalance)}${globalVars.currency}.` });
 
     } catch (e) {
         // Log error
