87c93e570c4db9ae9c0bd129757e8cacc0c304a7
Glazelf <gideon.steenbergen@gmail.com>
2022-05-20 18:36:39 +0200

    split args into two integers

--- commands/other/rng.js
+++ commands/other/rng.js
@@ -5,17 +5,12 @@ exports.run = async (client, interaction, args = interaction.options._hoistedOpt
     try {
         const sendMessage = require('../../util/sendMessage');
         const randomNumber = require('../../util/randomNumber');
-        let inputNumbers = args.join(" ").replace(", ", " ").split(" ");
 
-        if (!inputNumbers[1]) return sendMessage({ client: client, interaction: interaction, content: `You need to provide 2 numbers.` });
-        let lowNumber = inputNumbers[0];
-        let highNumber = inputNumbers[1];
+        let lowNumber = args.find(element => element.name == "number-min").value;
+        let highNumber = args.find(element => element.name == "number-max").value;
+
         if (lowNumber.startsWith("-")) lowNumber = lowNumber.substring(1, lowNumber.length + 1) * -1;
         if (highNumber.startsWith("-")) highNumber = highNumber.substring(1, highNumber.length + 1) * -1;
-
-        if (isNaN(lowNumber) || isNaN(highNumber)) return sendMessage({ client: client, interaction: interaction, content: `Make sure both values provided are numbers.` });
-        lowNumber = parseInt(lowNumber);
-        highNumber = parseInt(highNumber);
         if (lowNumber > highNumber) return sendMessage({ client: client, interaction: interaction, content: `Make sure the first number is lower than the second number.` });
 
         let randomValue = randomNumber(lowNumber, highNumber);
@@ -32,9 +27,14 @@ module.exports.config = {
     name: "rng",
     description: "Generate a random number.",
     options: [{
-        name: "numbers",
-        type: "STRING",
-        description: "Two numbers seperated by a comma.",
+        name: "number-min",
+        type: "INTEGER",
+        description: "Minimal number.",
+        required: true
+    }, {
+        name: "number-max",
+        type: "INTEGER",
+        description: "Maximum number.",
         required: true
     }]
 };
