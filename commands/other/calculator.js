6923779331c15b6ee885252e779896e5881dced0
Glazelf <gideon.steenbergen@gmail.com>
2022-05-20 18:26:56 +0200

    update calculator arg handling

--- commands/other/calculator.js
+++ commands/other/calculator.js
@@ -7,12 +7,9 @@ exports.run = async (client, interaction, args = interaction.options._hoistedOpt
         const Discord = require("discord.js");
 
         let maxMessageLength = 2000;
+        let noInputString = `You need to provide a valid input.`;
 
-        let noInputString = `You need to provide something to calculate`;
-        if (!args[0]) return sendMessage({ client: client, interaction: interaction, content: noInputString });
-
-        // Split input
-        const input = args.join(' ');
+        let input = args.find(element => element.name == "input").value;
 
         // Sanitize input
         let sanitizeValues = [
@@ -41,7 +38,7 @@ exports.run = async (client, interaction, args = interaction.options._hoistedOpt
             var evaled = eval(calcInput);
         } catch (e) {
             // console.log(e);
-            return sendMessage({ client: client, interaction: interaction, content: `You need to provide a valid input.` });
+            return sendMessage({ client: client, interaction: interaction, content: noInputString });
         };
 
         // Test out rounding based on remainder sometime
