51d026b1ecbd72ec1eea1e7c6aab63e7b804516f
Glazelf <gideon.steenbergen@gmail.com>
2022-05-22 17:59:05 +0200

    merge starlimit command into starboard

--- commands/mod/starboard.js
+++ commands/mod/starboard.js
@@ -6,33 +6,42 @@ exports.run = async (client, interaction, args = interaction.options._hoistedOpt
         const sendMessage = require('../../util/sendMessage');
         const isAdmin = require('../../util/isAdmin');
         let adminBool = await isAdmin(client, interaction.member);
+        const { StarboardChannels } = require('../../database/dbObjects');
+        const { StarboardLimits } = require('../../database/dbObjects');
         if (!interaction.member.permissions.has("MANAGE_CHANNELS") && !adminBool) return sendMessage({ client: client, interaction: interaction, content: globalVars.lackPerms });
 
-        const { StarboardChannels } = require('../../database/dbObjects');
         let oldChannel = await StarboardChannels.findOne({ where: { server_id: interaction.guild.id } });
-
-        // Check input
-        let subCommand = args[0];
-        if (!subCommand) {
-            if (oldChannel) {
-                return sendMessage({ client: client, interaction: interaction, content: `The current starboard channel is <#${oldChannel.channel_id}>. ${globalVars.starboardLimit} stars are required for a message to appear there.` });
-            };
-            return sendMessage({ client: client, interaction: interaction, content: `Please provide a valid channel or \`disable\`.` });
+        let oldStarLimitDB = await StarboardLimits.findOne({ where: { server_id: interaction.guild.id } });
+        let starlimit = null;
+        if (oldStarLimitDB) {
+            starlimit = oldStarLimitDB.star_limit;
+        } else {
+            starlimit = globalVars.starboardLimit;
         };
-        subCommand = subCommand.toLowerCase();
 
-        // Get channel
-        let targetChannel = interaction.guild.channels.cache.find(channel => channel.name == subCommand);
-        if (!targetChannel) targetChannel = interaction.guild.channels.cache.find(channel => subCommand.includes(channel.id));
-        if (!targetChannel && subCommand !== "disable") return sendMessage({ client: client, interaction: interaction, content: `That channel does not exist in this server.` });
+        let targetChannel;
+        let channelArg = args.find(element => element.name == "channel");
+        if (channelArg) targetChannel = channelArg.channel;
+        let disableBool = false;
+        let disableArg = args.find(element => element.name == "disable");
+        if (disableArg) disableBool = disableArg.value;
+
+        let starlimitArg = args.find(element => element.name == "starlimit");
+        if (starlimitArg) {
+            starlimit = starlimitArg.value;
+            if (oldStarLimitDB) await oldStarLimitDB.destroy();
+            await StarboardLimits.upsert({ server_id: interaction.guild.id, star_limit: starlimit });
+        };
+
+        if (!targetChannel && !disableBool) return sendMessage({ client: client, interaction: interaction, content: `That channel does not exist in this server.` });
 
         // Database
         if (oldChannel) await oldChannel.destroy();
-        if (subCommand == "disable") return sendMessage({ client: client, interaction: interaction, content: `Disabled starboard functionality in **${interaction.guild.name}**.` });
+        if (disableBool) return sendMessage({ client: client, interaction: interaction, content: `Disabled starboard functionality.` });
 
         await StarboardChannels.upsert({ server_id: interaction.guild.id, channel_id: targetChannel.id });
 
-        return sendMessage({ client: client, interaction: interaction, content: `${targetChannel} is now **${interaction.guild.name}**'s starboard. ${globalVars.starboardLimit} stars are required for a message to appear there.` });
+        return sendMessage({ client: client, interaction: interaction, content: `${targetChannel} is now **${interaction.guild.name}**'s starboard. ${starlimitt} stars are required for a message to appear there.` });
 
     } catch (e) {
         // Log error
@@ -47,6 +56,10 @@ module.exports.config = {
         name: "channel",
         type: "CHANNEL",
         description: "Specify channel."
+    }, {
+        name: "starlimit",
+        type: "INTEGER",
+        description: "Required amount of stars on a message."
     }, {
         name: "disable",
         type: "BOOLEAN",
