1c939d9141e6d51ef93144e97f88554c734ccf36
Glazelf <gideon.steenbergen@gmail.com>
2022-05-22 15:16:03 +0200

    update purge command

--- commands/mod/purge.js
+++ commands/mod/purge.js
@@ -6,61 +6,39 @@ exports.run = async (client, interaction, args = interaction.options._hoistedOpt
         const sendMessage = require('../../util/sendMessage');
         const isAdmin = require('../../util/isAdmin');
         let adminBool = await isAdmin(client, interaction.member);
-        if (!message.member.permissions.has("MANAGE_MESSAGES") && !adminBool) return sendMessage({ client: client, interaction: interaction, content: globalVars.lackPerms });
+        if (!interaction.member.permissions.has("MANAGE_MESSAGES") && !adminBool) return sendMessage({ client: client, interaction: interaction, content: globalVars.lackPerms });
 
-        let numberFromMessage = args[0];
-
-        let numberFromMessagestoNumber = Number(numberFromMessage);
-        if (!numberFromMessagestoNumber && args[1]) numberFromMessagestoNumber = Number(args[1]);
-        if (!numberFromMessagestoNumber) return sendMessage({ client: client, interaction: interaction, content: `Please provide a valid number.` });
+        let amount = args.find(element => element.name == "amount").value;
         let maxNumberOfMessages = 100;
-        let numberOfMessages = numberFromMessagestoNumber + 1;
-        if (isNaN(numberOfMessages)) numberFromMessage = args[1];
-        if (isNaN(numberOfMessages)) return sendMessage({ client: client, interaction: interaction, content: `Please provide a valid number.` });
+        if (amount > maxNumberOfMessages) amount = maxNumberOfMessages;
+        if (amount < 1) return sendMessage({ client: client, interaction: interaction, content: `Please provide a valid number.` });
 
-        // Max number of messages allowed to be deleted at once
-        if (numberOfMessages > maxNumberOfMessages) numberOfMessages = maxNumberOfMessages;
-
-        let amount = parseInt(numberOfMessages);
-
-        // Get user
-        let user;
-        let userTag;
-        let userID;
-        if (message.mentions && (message.mentions.members.size > 0 || message.mentions.repliedUser)) {
-            user = message.mentions.users.first();
-            userID = user.id;
-            userTag = user.tag;
-        };
-
-        if (!user && args[1]) {
-            userID = args[1];
-            userTag = userID;
-        };
-
+        // Get users
+        let user = null;
         let author = interaction.user;
-        let maxMessageFetch = 100;
-        if (numberFromMessage > maxMessageFetch) numberFromMessage = maxMessageFetch;
+        let userArg = args.find(element => element.name == "user");
+        if (userArg) user = userArg.user;
 
-        await message.channel.messages.fetch();
+        let oldMessagesString = `An error occurred while bulk deleting. You are likely trying to bulk delete messages older than 14 days.`;
 
         // Fetch 100 messages (will be filtered and lowered up to max amount requested), delete them and catch errors
-        if (userTag && userID) {
+        if (user) {
             try {
-                amount -= 1; // Correct amount
-                let messagesAll = await message.channel.messages.fetch({ limit: maxMessageFetch });
+                let messagesAll = await interaction.channel.messages.fetch({ limit: maxNumberOfMessages });
                 let messagesFiltered = await messagesAll.filter(m => m.author.id == user.id);
                 let messages = Object.values(Object.fromEntries(messagesFiltered)).slice(0, amount);
 
+                console.log(messages)
+
                 try {
-                    await message.channel.bulkDelete(messages);
-                    return message.channel.send({ content: `Deleted ${numberFromMessagestoNumber} messages from ${user.tag}, ${author}.` });
+                    await interaction.channel.bulkDelete(messages);
+                    return sendMessage({ client: client, interaction: interaction, content: `Deleted ${amount} messages from ${user.tag} within the last ${maxNumberOfMessages} messages.` });
                 } catch (e) {
                     if (e.toString().includes("Missing Permissions")) {
                         return logger(e, client, interaction);
                     } else {
                         // console.log(e);
-                        return message.channel.send({ content: `An error occurred while bulk deleting. You are likely trying to bulk delete messages older than 14 days, ${author}.` });
+                        return sendMessage({ client: client, interaction: interaction, content: oldMessagesString });
                     };
                 };
             } catch (e) {
@@ -68,23 +46,14 @@ exports.run = async (client, interaction, args = interaction.options._hoistedOpt
                     return logger(e, client, interaction);
                 } else {
                     // console.log(e);
-                    return message.channel.send({ content: `An error occurred while bulk deleting.` });
+                    return sendMessage({ client: client, interaction: interaction, content: `An error occurred while bulk deleting.` });
                 };
             };
         } else {
             try {
-                let messages = await message.channel.messages.fetch({ limit: amount });
-                await message.channel.bulkDelete(messages);
-                await message.channel.send({ content: `Deleted ${numberFromMessage} messages, ${author}.\nThis message will be deleted in 10 seconds.`, ephemeral: false }).then(message => {
-                    setTimeout(function () {
-                        try {
-                            message.delete();
-                        } catch (e) {
-                            // console.log(e);
-                        };
-                    }, 10000); // delete after 10 seconds
-                });
-                return;
+                let messages = await interaction.channel.messages.fetch({ limit: amount });
+                await interaction.channel.bulkDelete(messages);
+                return sendMessage({ client: client, interaction: interaction, content: `Deleted ${amount} messages` });
             } catch (e) {
                 if (e.toString().includes("Missing Permissions")) {
                     return logger(e, client, interaction);
@@ -93,7 +62,7 @@ exports.run = async (client, interaction, args = interaction.options._hoistedOpt
                         return logger(e, client, interaction);
                     } else {
                         // console.log(e);
-                        return message.channel.send({ content: `An error occurred while bulk deleting. You are likely trying to bulk delete messages older than 14 days, ${author}.` });
+                        return interaction.channel.send({ content: oldMessagesString });
                     };
                 };
             };
