4a921116fc8dc622cf5b1a1362d149fde329ecae
Glazelf <gideon.steenbergen@gmail.com>
2022-05-22 23:01:31 +0200

    fix leaderboard

--- commands/money/leaderboard.js
+++ commands/money/leaderboard.js
@@ -8,50 +8,29 @@ exports.run = async (client, interaction, args = interaction.options._hoistedOpt
         const { bank } = require('../../database/bank');
 
         let memberFetch = await interaction.guild.members.fetch();
-
-        let author = interaction.user;
-
-        let avatar = author.displayAvatarURL(globalVars.displayAvatarSettings);
+        let avatar = interaction.user.displayAvatarURL(globalVars.displayAvatarSettings);
+        let global = false;
+        let globalArg = args.find(element => element.name == "global");
+        if (globalArg) global = globalArg.value;
 
         const leaderboardEmbed = new Discord.MessageEmbed()
             .setColor(globalVars.embedColor);
 
-        if (args[0]) {
-            // Global leaderboard
-            if (args[0].toLowerCase() == "global") {
-                let leaderboardStringGlobal = bank.currency.sort((a, b) => b.balance - a.balance)
-                    .filter(user => client.users.cache.has(user.user_id))
-                    .filter(user => !client.users.cache.get(user.user_id).bot)
-                    .first(10)
-                    .map((user, position) => `${position + 1}. ${(client.users.cache.get(user.user_id).tag)}: ${Math.floor(user.balance)}${globalVars.currency}`)
-                    .join('\n');
+        // Global leaderboard
+        if (global) {
 
-                leaderboardEmbed
-                    .setDescription(leaderboardStringGlobal)
-                    .setAuthor({ name: `Global Leaderboard:`, iconURL: avatar });
+            let leaderboardStringGlobal = bank.currency.sort((a, b) => b.balance - a.balance)
+                .filter(user => client.users.cache.has(user.user_id))
+                .filter(user => !client.users.cache.get(user.user_id).bot)
+                .first(10)
+                .map((user, position) => `${position + 1}. ${(client.users.cache.get(user.user_id).tag)}: ${Math.floor(user.balance)}${globalVars.currency}`)
+                .join('\n');
 
-                // Leaderboard with IDs
-            } else if (args[0].toLowerCase() == "id" && interaction.user.id == client.config.ownerID) {
-                let leaderboardStringID = bank.currency.sort((a, b) => b.balance - a.balance)
-                    .filter(user => client.users.cache.has(user.user_id))
-                    .first(10)
-                    .map((user, position) => `${position + 1}. ${(client.users.cache.get(user.user_id).tag)}: ${Math.floor(user.balance)}${globalVars.currency} (${(client.users.cache.get(user.user_id).id)})`)
-                    .join('\n');
-
-                leaderboardEmbed
-                    .setDescription(leaderboardStringID)
-                    .setAuthor({ name: `ID Leaderboard:`, iconURL: avatar });
-
-            } else {
-                serverLB();
-            };
+            leaderboardEmbed
+                .setDescription(leaderboardStringGlobal)
+                .setAuthor({ name: `Global Leaderboard:`, iconURL: avatar });
         } else {
-            serverLB();
-        };
-
-        return sendMessage({ client: client, interaction: interaction, embeds: leaderboardEmbed });
-
-        function serverLB() {
+            avatar = interaction.guild.iconURL(globalVars.displayAvatarSettings);
             let leaderboardString = bank.currency.sort((a, b) => b.balance - a.balance)
                 .filter(user => client.users.cache.get(user.user_id) && memberFetch.get(user.user_id))
                 .filter(user => !client.users.cache.get(user.user_id).bot)
@@ -65,6 +44,8 @@ exports.run = async (client, interaction, args = interaction.options._hoistedOpt
                 .setDescription(leaderboardString)
                 .setAuthor({ name: `Leaderboard:`, iconURL: avatar });
         };
+
+        return sendMessage({ client: client, interaction: interaction, embeds: leaderboardEmbed });
 
     } catch (e) {
         // Log error
