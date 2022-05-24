18898d03728e8a86cd35f45d5a92c2778995ef06
Glazelf <gideon.steenbergen@gmail.com>
2022-05-24 15:27:55 +0200

    add guildcreate and guilddelete events

---
+++ events/guildCreate.js
@@ -0,0 +1,33 @@
+module.exports = async (client, guild) => {
+    const logger = require('../util/logger');
+    // Import globals
+    let globalVars = require('./ready');
+    try {
+        const Discord = require("discord.js");
+
+        let log = client.channels.fetch(client.config.devChannelID);
+        if (!log) return;
+
+        let icon = guild.iconURL(globalVars.displayAvatarSettings);
+        let guildOwner = await guild.fetchOwner();
+
+        const guildEmbed = new Discord.MessageEmbed()
+            .setColor(globalVars.embedColor)
+            .setAuthor({ name: `Guild Joined ⭐`, iconURL: icon })
+            .setThumbnail(icon);
+            .setDescription(`${guild.name}`)
+            .addField(`Owner:`, `${guildOwner.tag} (${guildOwner.id})`, true)
+            .addField(`Users:`, guild.memberCount.toString(), true)
+            .setFooter({ text: guild.id })
+            .setTimestamp();
+
+        return log.send({ embeds: [guildEmbed] });
+
+    } else {
+        return;
+    };
+
+} catch (e) {
+    // Log error
+    logger(e, client);
+};
---
+++ events/guildDelete.js
@@ -0,0 +1,34 @@
+module.exports = async (client, guild) => {
+    const logger = require('../util/logger');
+    // Import globals
+    let globalVars = require('./ready');
+    try {
+        const Discord = require("discord.js");
+
+        let log = client.channels.fetch(client.config.devChannelID);
+        if (!log) return;
+
+        let icon = guild.iconURL(globalVars.displayAvatarSettings);
+        let guildOwner = await guild.fetchOwner();
+
+        const guildEmbed = new Discord.MessageEmbed()
+            .setColor(globalVars.embedColor)
+            .setAuthor({ name: `Guild Left ❌`, iconURL: icon })
+            .setThumbnail(icon);
+            .setDescription(`${guild.name}`);
+        if (guildOwner) guildEmbed.addField(`Owner:`, `${guildOwner.tag} (${guildOwner.id})`, true);
+        guildEmbed
+            .addField(`Users:`, guild.memberCount.toString(), true)
+            .setFooter({ text: guild.id })
+            .setTimestamp();
+
+        return log.send({ embeds: [guildEmbed] });
+
+    } else {
+        return;
+    };
+
+} catch (e) {
+    // Log error
+    logger(e, client);
+};
