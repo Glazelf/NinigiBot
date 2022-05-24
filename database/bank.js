bd111f2b6f8cfb54f7ae9f1082316ded11fa2a37
Glazelf <gideon.steenbergen@gmail.com>
2022-05-24 14:59:55 +0200

    test

--- database/bank.js
+++ database/bank.js
@@ -113,6 +113,8 @@ module.exports = {
                             user.balance = Math.floor(user.balance);
                             return user.save();
                         };
+                        console.log(id)
+                        console.log(amount)
                         const newUser = await Users.create({ user_id: id, balance: amount });
                         money.set(id, newUser);
                         return newUser;
