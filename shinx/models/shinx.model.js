d21e8e80045d9faa4db9583701734629425a7bc0
Aitor <aitor.izuzquiza@gmail.com>
2022-06-18 18:33:07 +0200

    Added sqlite models

---
+++ shinx/models/shinx.model.js
@@ -0,0 +1,101 @@
+const MIN_RANGE = 0;
+const MAX_RANGE = 10;
+
+module.exports = (sequelize, DataTypes) => {
+    const Shinx = sequelize.define('Shinx', {
+        user_id: DataTypes.STRING,
+        nickname: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            'default': 'Shinx',
+        },
+        fullness: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            'default': 0,
+        },
+        happiness: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            'default': 0,
+        },
+        experience: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            'default': 0,
+        },
+    }, {
+        timestamps: false,
+    });
+    // Class Methods
+
+    // Instance Methods
+    //  Experience
+    Shinx.Instance.prototype.addExperience = function(experience){
+        this.experience += experience;
+    }
+    Shinx.Instance.prototype.getExperience = function(){
+        return this.experience;
+    }
+    
+    Shinx.Instance.prototype.getLevel = function(){
+        return Math.floor(Math.cbrt(1.25*this.experience))
+    }
+    // Fullness
+    Shinx.Instance.prototype.feed = function(amount){
+        if(this.fullness<MAX_RANGE){
+            this.fullness = Math.min(MAX_RANGE, this.fullness+amount);
+            return true;
+          } else {
+            return false;
+        }
+    }
+    Shinx.Instance.prototype.unfeed = function(amount){
+        if(this.fullness>MIN_RANGE){
+            this.fullness = Math.max(MIN_RANGE, this.fullness-amount);
+            return true;
+          } else {
+            return false;
+        }
+    }
+
+    Shinx.Instance.prototype.getFullness = function(){
+        this.fullness
+    }
+    // Happiness
+    Shinx.Instance.prototype.addHappiness = function(amount){
+        if(this.happiness<MAX_RANGE){
+            this.happiness = Math.min(MAX_RANGE, this.happiness+amount);
+            return true;
+          } else {
+            return false;
+        }
+    }
+    Shinx.Instance.prototype.removeHappiness = function(amount){
+        if(this.happiness>MIN_RANGE){
+            this.happiness = Math.max(MIN_RANGE, this.happiness-amount);
+            return true;
+          } else {
+            return false;
+        }
+    }
+
+    Shinx.Instance.prototype.getHappiness = function(){
+        this.happiness
+    }
+
+    return Shinx;
+};
+
+
+
+
+const Shinx = db.define('Shinx', {
+    name: sequelize.STRING,
+    age: sequelize.INTEGER
+  });
+  
+  User.Instance.prototype.test = function() {
+    return `Name: ${this.name}, Age: ${this.age}`
+  }
+  
