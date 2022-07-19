42510b39be95a287e7b054a9075aaf1ee291594e
Skaidus <aitor.izuzquiza@gmail.com>
2022-07-19 23:08:53 +0200

    Renamed model files

--- database/dbObjects/server.model.js
+++ database/dbObjects/server.model.js
@@ -2,14 +2,14 @@ module.exports = (sequelize, DataTypes) => {
 
 	const shinxQuotes = require('./dbObjects/models/attachments/shinxQuotes')(attatchments, Sequelize.DataTypes);
 
-	const EligibleRoles = require('./models/server/EligibleRoles')(sequelize, Sequelize.DataTypes);
-	const PersonalRoles = require('./models/server/PersonalRoles')(sequelize, Sequelize.DataTypes);
-	const PersonalRoleServers = require('./models/global/PersonalRoleServers')(sequelize, Sequelize.DataTypes);
-	const ModEnabledServers = require('./models/global/ModEnabledServers')(sequelize, Sequelize.DataTypes);
-	const LogChannels = require('./models/global/LogChannels')(sequelize, Sequelize.DataTypes);
-	const StarboardChannels = require('./models/global/StarboardChannels')(sequelize, Sequelize.DataTypes);
-	const StarboardMessages = require('./models/global/StarboardMessages')(sequelize, Sequelize.DataTypes);
-	const StarboardLimits = require('./models/server/StarboardLimits')(sequelize, Sequelize.DataTypes);
+	const EligibleRoles = require('./models/server/eligibleRoles.model')(sequelize, Sequelize.DataTypes);
+	const PersonalRoles = require('./models/server/personalRoles.model')(sequelize, Sequelize.DataTypes);
+	const PersonalRoleServers = require('./models/global/personalRoleServers.model')(sequelize, Sequelize.DataTypes);
+	const ModEnabledServers = require('./models/global/modEnabledServers.model')(sequelize, Sequelize.DataTypes);
+	const LogChannels = require('./models/global/logChannels.model')(sequelize, Sequelize.DataTypes);
+	const StarboardChannels = require('./models/global/starboardChannels.model')(sequelize, Sequelize.DataTypes);
+	const StarboardMessages = require('./models/global/starboardMessages.model')(sequelize, Sequelize.DataTypes);
+	const StarboardLimits = require('./models/server/starboardLimits.model')(sequelize, Sequelize.DataTypes);
 	
 	sequelize.sync();
 	return {shinxQuotes, EligibleRoles, PersonalRoles, PersonalRoleServers, LogChannels, StarboardChannels, StarboardMessages, StarboardLimits, ModEnabledServers};
