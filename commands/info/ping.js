0d7ed114264ab835098fa453f99abec450d98547
Glazelf <gideon.steenbergen@gmail.com>
2022-07-31 22:45:46 +0200

    Update ping.js

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -8,7 +8,7 @@ exports.run = async (client, interaction) => {
         let ephemeral = true;
         await interaction.deferReply({ ephemeral: ephemeral });
 
-        let commands = await client.application.commands.fetch();
+        let commands = client.application.commands.cache.get("bet");
         return sendMessage({ client: client, interaction: interaction, content: commands.toString() });
 
         let pongString = `Pong!'ed back in`;
