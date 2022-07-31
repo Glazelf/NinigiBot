328a221ee566d5ee3466a072d9cdb7f4482b2f64
Glazelf <gideon.steenbergen@gmail.com>
2022-07-31 22:50:48 +0200

    Update ping.js

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -10,6 +10,7 @@ exports.run = async (client, interaction) => {
 
         console.log(client.application.commands.cache)
         let commands = client.application.commands.cache.find(command => command.name == "bet");
+        console.log(commands)
         return sendMessage({ client: client, interaction: interaction, content: commands.toString() });
 
         let pongString = `Pong!'ed back in`;
