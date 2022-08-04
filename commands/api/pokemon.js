ca48514a4178d4ea8e00e22aa78df286af167b92
Glazelf <gideon.steenbergen@gmail.com>
2022-08-04 23:42:50 +0200

    re-add random pokemon search

--- commands/api/pokemon.js
+++ commands/api/pokemon.js
@@ -187,7 +187,11 @@ exports.run = async (client, interaction) => {
             // Pokémon
             case "pokemon":
                 let pokemon = Dex.species.get(pokemonName);
-                if (!pokemon || !pokemon.exists || pokemon.isNonstandard == "Custom" || pokemon.isNonstandard == "CAP") return sendMessage({ client: client, interaction: interaction, content: `Sorry, I could not find a Pokémon by that name.` });
+                if (pokemonName.toLowerCase() == "random") {
+                    let allPokemon = Dex.species.all();
+                    let allKeys = Object.keys(allPokemon);
+                    pokemon = allPokemon[allKeys[allKeys.length * Math.random() << 0]];
+                } else if (!pokemon || !pokemon.exists || pokemon.isNonstandard == "Custom" || pokemon.isNonstandard == "CAP") return sendMessage({ client: client, interaction: interaction, content: `Sorry, I could not find a Pokémon by that name.` });
                 let messageObject = await getPokemon(client, interaction, pokemon, ephemeral);
                 return sendMessage({ client: client, interaction: interaction, embeds: messageObject.embeds, components: messageObject.components, ephemeral: ephemeral });
                 break;
