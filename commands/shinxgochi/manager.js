9ec0980d55d9db6241fbe27c7deeacf401cf865c
Glazelf <gideon.steenbergen@gmail.com>
2022-08-16 09:17:37 +0200

    make manager command limited to bot owner
    also some formatting i kinda hate autoformatting sometimes

--- commands/shinxgochi/manager.js
+++ commands/shinxgochi/manager.js
@@ -1,4 +1,4 @@
-const Canvas = require('canvas');
+
 
 exports.run = async (client, interaction) => {
     const logger = require('../../util/logger');
@@ -10,85 +10,83 @@ exports.run = async (client, interaction) => {
     try {
         const sendMessage = require('../../util/sendMessage');
         const Discord = require("discord.js");
-
+        const Canvas = require('canvas');
+        const isOwner = require('../../util/isOwner');
         const api_shinx = require('../../database/dbServices/shinx.api');
         const api_user = require('../../database/dbServices/user.api');
-        const api_trophy = require('../../database/dbServices/trophy.api');        
+        const api_trophy = require('../../database/dbServices/trophy.api');
         let messageFile = null;
         let ephemeral = true;
-        let embed,trophy_name,res, returnString;
+        let embed, trophy_name, res, returnString;
         let canvas, ctx, img, shinx;
+
+        let ownerBool = await isOwner(client, interaction.user);
+        if (!ownerBool) return sendMessage({ client: client, interaction: interaction, content: globalVars.lackPerms });
+
         let ephemeralArg = interaction.options.getBoolean("ephemeral");
         let emotesAllowed = true;
         if (ephemeralArg === false) ephemeral = false;
         if (ephemeral == true && !interaction.guild.roles.everyone.permissions.has("USE_EXTERNAL_EMOJIS")) emotesAllowed = false;
 
-        let master = interaction.user
+        let master = interaction.user;
 
-        let  trophies;
+        let trophies;
         switch (interaction.options.getSubcommand()) {
             case "addshoptrophy":
                 let error = '';
                 trophy_name = interaction.options.getString("name").trim();
-                switch(checker(trophy_name, 25)){
-                    case "TooShort": 
-                        error += 'Name too short\n'
+                switch (checker(trophy_name, 25)) {
+                    case "TooShort":
+                        error += 'Name too short\n';
                     case "TooLong":
-                        error += 'Name exceeds 25 characters\n'
+                        error += 'Name exceeds 25 characters\n';
                     case "InvalidChars":
-                        error += 'Name has invalid characters\n'
-                }
-                res = await api_trophy.checkTrophyExistance(trophy_name);
-                if(res==true){
-                    error+= 'Name already used\n'
+                        error += 'Name has invalid characters\n';
                 };
+                res = await api_trophy.checkTrophyExistance(trophy_name);
+                if (res == true) error += 'Name already used\n';
                 const trophy_desc = interaction.options.getString("description").trim();
-                switch(checker(trophy_desc, 1024,false)){
-                    case "TooShort": 
-                        error += 'Description too short\n'
+                switch (checker(trophy_desc, 1024, false)) {
+                    case "TooShort":
+                        error += 'Description too short\n';
                     case "TooLong":
-                        error += 'Description exceeds 25 characters\n'
+                        error += 'Description exceeds 25 characters\n';
                     case "InvalidChars":
-                        error += 'Description has invalid characters\n'
-                }
+                        error += 'Description has invalid characters\n';
+                };
                 let trophy_emote = interaction.options.getString("emote").trim().replace(/^:+/, '').replace(/:+$/, '');
                 let parsed_emote = trophy_emote.match(regexpDiscord);
-                if(!parsed_emote){
+                if (!parsed_emote) {
                     parsed_emote = trophy_emote.match(regexpUnicode)
-                    if(!parsed_emote) {
-                        error+='Emote is not a valid Unicode Emoji or Discord custom emote'
-                    }
-                }
-                if(parsed_emote){
-                    trophy_emote = parsed_emote[0];
-                }
-                
-                const trophy_price = interaction.options.getInteger("price");
-                if(trophy_price<1){
-                    error+='Price cannot be lower than 1'
-                }
-                
+                    if (!parsed_emote) error += 'Emote is not a valid Unicode Emoji or Discord custom emote';
+                };
+                if (parsed_emote) trophy_emote = parsed_emote[0];;
 
-                if(error.length>0){
-                    returnString = 'Could not add the trophy due to the following issues:```\n'+error+'\n```'
+                const trophy_price = interaction.options.getInteger("price");
+                if (trophy_price < 1) error += 'Price cannot be lower than 1'
+
+                if (error.length > 0) {
+                    returnString = 'Could not add the trophy due to the following issues:```\n' + error + '\n```'
                 } else {
                     await api_trophy.createShopTrophy(trophy_name, trophy_emote, trophy_desc, trophy_price);
                     returnString = `${trophy_name} added successfully to the shop!`;
-                }
-                return sendMessage({ 
-                    client: client, 
-                    interaction: interaction, 
-                    content: returnString, 
-                    ephemeral: true });
+                };
+                return sendMessage({
+                    client: client,
+                    interaction: interaction,
+                    content: returnString,
+                    ephemeral: true
+                });
             case "deleteshoptrophy":
                 trophy_name = interaction.options.getString("name").trim();
                 res = await api_trophy.deleteShopTrophy(trophy_name);
-                returnString = res? `${trophy_name} deleted successfully from the shop!`: `${trophy_name} does not exist in the __shop__`
-                return sendMessage({ 
-                    client: client, 
-                    interaction: interaction, 
-                    content: returnString, 
-                    ephemeral: true });
+                returnString = res ? `${trophy_name} deleted successfully from the shop!` : `${trophy_name} does not exist in the __shop__`
+                return sendMessage({
+                    client: client,
+                    interaction: interaction,
+                    content: returnString,
+                    ephemeral: true
+                });
         };
 
     } catch (e) {
@@ -101,6 +99,7 @@ exports.run = async (client, interaction) => {
 module.exports.config = {
     name: "manager",
     description: "Manage multiple aspects about the shop",
+    serverID: ["759344085420605471"],
     options: [{
         name: "addshoptrophy",
         type: "SUB_COMMAND",
@@ -110,24 +109,24 @@ module.exports.config = {
             type: "STRING",
             description: "Name of the trophy. Make sure it's unique with less than 25 characters!",
             required: true
-        },{
+        }, {
             name: "emote",
             type: "STRING",
             description: "Icon of the trophy. Make sure it's a valid emote.",
             required: true
-        },{
+        }, {
             name: "description",
             type: "STRING",
             description: "Description of the trophy",
             required: true
-        },{
+        }, {
             name: "price",
             type: "INTEGER",
             description: "Amount of money to charge for it",
             required: true,
         },]
-        },
-        {
+    },
+    {
         name: "deleteshoptrophy",
         type: "SUB_COMMAND",
         description: "Delete a trophy from the shop",
@@ -137,6 +136,7 @@ module.exports.config = {
             autocomplete: true,
             description: "Name of the trophy. Make sure it's valid!",
             required: true
-        }]}
+        }]
+    }
     ]
 };
