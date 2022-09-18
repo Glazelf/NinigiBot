486bcdae3f5281f58a081a70d73ecd135466408f
Glazelf <gideon.steenbergen@gmail.com>
2022-09-18 03:43:47 +0200

    Add a bunch of comments

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -293,10 +293,13 @@ module.exports = async (client, interaction) => {
                                 };
                                 break;
                             case "mode":
+                                // choices.push({name: "Splatfest", value: "Splatfest|festSchedules"}); // Should be tested during a splatfest to see data format. Replace all non-available modes with this option when a Splatfest is active.
                                 choices.push({ name: "Turf War", value: "Turf War|regularSchedules" });
                                 choices.push({ name: "Anarchy Battle (Series)", value: "Anarchy Battle (Series)|bankaraSchedules|series" });
                                 choices.push({ name: "Anarchy Battle (Open)", value: "Anarchy Battle (Open)|bankaraSchedules|open" });
-                                choices.push({ name: "Salmon Run", value: "Salmon Run|coopGroupingSchedule" });
+                                // choices.push({ name: "X Battle", value: "X Battle|xSchedules" }); // Uncomment when X Battle is available.
+                                // choices.push({ name: "League Battle", value: "League Battle|leagueSchedules" }); // Uncomment when League Battle is available.
+                                choices.push({ name: "Salmon Run", value: "Salmon Run|coopGroupingSchedule" }); // Make name "Salmon Run (Big Run)" if Big Run is active, assuming all other modes are also available. If not, do something similar to Splatfests.
                                 break;
                         };
                         break;
