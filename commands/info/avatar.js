36fc43eb8844551e3e1ca1a0aed9234e402eb636
Glazelf <gideon.steenbergen@gmail.com>
2022-11-04 09:18:16 +0100

    fix lack of member avatar
    Closes #411

--- commands/info/avatar.js
+++ commands/info/avatar.js
@@ -13,7 +13,7 @@ exports.run = async (client, interaction) => {
         let avatar = null;
         let serverAvatar = null;
         if (user.avatarURL()) avatar = await user.avatarURL({ format: "png", dynamic: true, size: 512 });
-        if (member.avatarURL()) serverAvatar = await member.avatarURL(globalVars.displayAvatarSettings);
+        if (member && member.avatarURL()) serverAvatar = await member.avatarURL(globalVars.displayAvatarSettings);
         if (!avatar && !serverAvatar) return sendMessage({ client: client, interaction: interaction, content: `${user.tag} doesn't have an avatar.` });
         if (!serverAvatar) {
             serverAvatar = avatar;
