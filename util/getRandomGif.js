0d3e2769ae4a30dc8d0623d968550337b43a17b6
Glazelf <gideon.steenbergen@gmail.com>
2022-11-07 19:14:39 +0100

    fix tag

--- util/getRandomGif.js
+++ util/getRandomGif.js
@@ -1,12 +1,12 @@
 module.exports = async (gifTags = []) => {
     const axios = require("axios");
     const config = require("../config.json");
-    const randomElement = gifTags[Math.floor(Math.random() * gifTags.length)];
+    const randomTag = gifTags[Math.floor(Math.random() * gifTags.length)];
     let giphyParams = {
         api_key: config.giphy,
         rating: "g"
     };
-    if (gifTags.length > 0) giphyParams.tag = gifTags;
+    if (gifTags.length > 0) giphyParams.tag = randomTag;
     const { data } = await axios.get("https://api.giphy.com/v1/gifs/random", {
         giphyParams
     }).catch(e => {
