34194abe5dbbc9d388f3ac2e2cf15eb3b6b85838
Glazelf <gideon.steenbergen@gmail.com>
2022-11-08 08:04:06 +0100

    Replacement API call to circumvent error
    Mysterious 401 when calling api the "clean" way, so I made an uglier way, that for some reason works. Might be a  bit slower bc of the forEach loop.

--- util/getRandomGif.js
+++ util/getRandomGif.js
@@ -1,20 +1,35 @@
 module.exports = async (gifTags = []) => {
     const axios = require("axios");
     const config = require("../config.json");
+    let giphyAPI = "https://api.giphy.com/v1/gifs/random";
     const randomTag = gifTags[Math.floor(Math.random() * gifTags.length)];
     let giphyParams = {
         api_key: config.giphy,
         rating: "g"
     };
     if (gifTags.length > 0) giphyParams.tag = randomTag;
-    const giphyResponse = await axios.get("https://api.giphy.com/v1/gifs/random", {
-        giphyParams
-    }).catch(e => {
+
+    //// Uglier replacement function for the one below
+    let giphyURL = `${giphyAPI}?`;
+    await Object.entries(giphyParams).forEach(([key, value]) => {
+        giphyURL += `${key}=${value}&`;
+    });
+    let giphyResponse = await axios.get(giphyURL).catch(e => {
         console.log(e);
         return null;
     });
+
+    //// Ideally would use this, but for some reason this method returns a 401 error. The above works the same, but is slightly slower and uglier :)
+    // let giphyResponse = await axios.get(giphyAPI, {
+    //     giphyParams
+    // }).catch(e => {
+    //     console.log(e);
+    //     return null;
+    // });
+
     let data = null;
-    if (giphyResponse) data = giphyResponse.data;
+    // if (giphyResponse) data = giphyResponse.data; // Information depth with original function
+    if (giphyResponse) data = giphyResponse.data.data; // Information depth with replacement function
     if (!data) return null;
     let images = data.images;
     if (data.images && data.images.original) {
