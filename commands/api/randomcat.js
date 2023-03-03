d874f96db68540f0dd98a506e1ff4b34fdc8de04
Glazelf <gideon.steenbergen@gmail.com>
2023-03-03 09:03:10 +0100

    give all the cats random names

--- commands/api/randomcat.js
+++ commands/api/randomcat.js
@@ -6,6 +6,7 @@ exports.run = async (client, interaction) => {
         const sendMessage = require('../../util/sendMessage');
         const Discord = require("discord.js");
         const axios = require("axios");
+        const { uniqueNamesGenerator, names } = require('unique-names-generator'); // Random name generator that can be seeded
 
         let ephemeral = interaction.options.getBoolean("ephemeral");
         if (ephemeral === null) ephemeral = true;
@@ -15,10 +16,15 @@ exports.run = async (client, interaction) => {
         let response = await axios.get(catAPI);
         let catImage = response.data.file;
 
+        let catName = uniqueNamesGenerator({
+            dictionaries: [names],
+            seed: catImage // Seed the random name generator with the cat image URL to get the same name every time
+        });
+
         const catEmbed = new Discord.MessageEmbed()
             .setColor(globalVars.embedColor)
             .setImage(catImage)
-            .setFooter({ text: `Meow` });
+            .setFooter({ text: `"Meow" -${catName}` });
         return sendMessage({ client: client, interaction: interaction, embeds: catEmbed, ephemeral: ephemeral });
 
     } catch (e) {
