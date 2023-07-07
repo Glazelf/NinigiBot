9b2d9779ff0a9fe7ff15726237f285ccdaf0fbc9
Glazelf <gideon.steenbergen@gmail.com>
2023-07-07 19:23:18 +0200

    Cleaning(?)

--- util/convertMeterFeet.js
+++ util/convertMeterFeet.js
@@ -1,13 +1,10 @@
 module.exports = (input) => {
     input = input * 3.28084;
     let inputSplit = input.toString().split(".");
-    inputSplit[1] = inputSplit[1] * 1.2;
-    inputSplit[1] = Math.round(inputSplit[1]);
+    inputSplit[1] = Math.round(inputSplit[1] * 1.2);
     let twoDecimals = inputSplit[1].toString().startsWith("1");
-    let output = inputSplit.join(".");
     let roundingMultiplier = 10;
     if (twoDecimals) roundingMultiplier = 100;
-    output = Math.round(output * roundingMultiplier) / roundingMultiplier;
-    output = output.toString().replace(".", "'") + "\"";
+    let output = (Math.round(inputSplit.join(".") * roundingMultiplier) / roundingMultiplier).toString().replace(".", "'") + "\"";
     return output;
 };
