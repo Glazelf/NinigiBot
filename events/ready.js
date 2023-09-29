cbcb7f110b7389ddeb94e27043a53229bba5959e
Glazelf <gideon.steenbergen@gmail.com>
2023-09-29 21:46:28 +0200

    set commands globally
    manage your own bots silly

--- events/ready.js
+++ events/ready.js
@@ -12,7 +12,7 @@ module.exports = async (client) => {
             try {
                 let commandServerID = null;
                 if (command.config.serverID) commandServerID = command.config.serverID;
-                if (client.user.id != module.exports.NinigiID) commandServerID = client.config.devServerID; // set to test server for test build
+                // if (client.user.id != module.exports.NinigiID) commandServerID = client.config.devServerID; // set to test server for test build
                 slashCommand = await client.application.commands.create(command.config, commandServerID);
             } catch (e) {
                 console.log(e);
