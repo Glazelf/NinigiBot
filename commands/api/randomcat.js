1fadd5ccd5a821b8d682528513f735124fda6dac
Gideon <35176230+Glazelf@users.noreply.github.com>
2023-11-01 10:23:54 +0100

    drop hanging logs

--- commands/api/randomcat.js
+++ commands/api/randomcat.js
@@ -38,9 +38,6 @@ exports.run = async (client, interaction) => {
             catImage = response.data.file;
             catNameSeed = catImage;
         } else if (catAPI.includes(catAAS)) {
-            console.log(response.data)
-            console.log(response.data.url)
-            console.log(response.data.url.split)
             catImage = `${catAAS}/${response.data._id}`;
             if (catText !== standardCatText) catImage += `/says/${encodeURIComponent(catText)}`;
             catImage += `.${response.data.file.split(".")[1]}`; // Split is to get the file extension used
