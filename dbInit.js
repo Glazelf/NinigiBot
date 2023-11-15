4ee48bca2cb3f8ffa5a6a141586de033b94beddf
Glazelf <gideon.steenbergen@gmail.com>
2023-11-15 18:19:38 +0100

    formatting

--- dbInit.js
+++ dbInit.js
@@ -1,20 +1,18 @@
 const initDB = async (reset_db) => {
     try {
-        inits = [ require('./database/dbInit/userdata.init')(reset_db),
-                require('./database/dbInit/serverdata.init')(reset_db)];
+        inits = [require('./database/dbInit/userdata.init')(reset_db),
+        require('./database/dbInit/serverdata.init')(reset_db)];
         await Promise.all(inits);
     } catch (e) {
         console.log(e)
     };
 };
 
-
-
-(async function main () {
+(async function main() {
     let argv = require('minimist')(process.argv.slice(2));
-    if(argv.delete){
-        if(argv.delete.toLowerCase()=='true')
-        console.log('All data will be deleted')
+    if (argv.delete) {
+        if (argv.delete.toLowerCase() == 'true')
+            console.log('All data will be deleted')
         await initDB(true);
     } else {
         await initDB(false);
