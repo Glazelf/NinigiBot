147fa93a7f71f54a7ad0963b5093a6f12700a807
Glazelf <gideon.steenbergen@gmail.com>
2024-03-07 18:55:13 +0100

    Rewrite /ping

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -2,11 +2,9 @@ const Discord = require("discord.js");
 exports.run = async (client, interaction, logger, globalVars) => {
     try {
         const sendMessage = require('../../util/sendMessage');
-        let pongString = `Pong!'ed back in`;
-        let pauseString = `${pongString} (hold on, processing latency...)`;
-        let wsLatencyString = `Websocket latency is ${client.ws.ping}ms`;
-        let replyText = `Pong! Slash command latency is ${client.ws.ping}ms.`;
-        return sendMessage({ client: client, interaction: interaction, content: replyText });
+        let commandPing = Math.abs(Date.now() - interaction.createdTimestamp);
+        let replyString = `Pong!\n- Websocket ping is ${client.ws.ping}ms.\n- Command ping is ${commandPing}ms.`;
+        return sendMessage({ client: client, interaction: interaction, content: replyString });
 
     } catch (e) {
         // Log error
