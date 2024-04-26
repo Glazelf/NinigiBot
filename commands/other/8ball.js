2d23c078b7ad189dd66138bb38b4023df8c0cb4b
Glazelf <gideon.steenbergen@gmail.com>
2024-04-26 11:13:49 +0200

    Add ephemerality arg to 8ball
    its funny

--- commands/other/8ball.js
+++ commands/other/8ball.js
@@ -1,11 +1,13 @@
 const Discord = require("discord.js");
-exports.run = async (client, interaction, logger, globalVars) => {
+exports.run = async (client, interaction, logger, globalVars, ephemeral = true) => {
     try {
         const sendMessage = require('../../util/sendMessage');
         let input = interaction.options.getString("input");
+        let ephemeralArg = interaction.options.getBoolean("ephemeral");
+        if (ephemeralArg !== null) ephemeral = ephemeralArg;
         const answers = ["Maybe someday", "Nothing", "Neither", "I don't think so", "No", "Yes", "Try asking again", "Definitely", "Probably not"];
         const randomAnswer = answers[Math.floor(Math.random() * answers.length)];
-        return sendMessage({ client: client, interaction: interaction, content: `The 8ball says: "${randomAnswer}.".` });
+        return sendMessage({ client: client, interaction: interaction, content: `The 8ball says: "${randomAnswer}.".`, ephemeral: ephemeral });
 
     } catch (e) {
         // Log error
@@ -21,5 +23,9 @@ module.exports.config = {
         type: Discord.ApplicationCommandOptionType.String,
         description: "Your burning question.",
         required: true
+    }, {
+        name: "ephemeral",
+        type: Discord.ApplicationCommandOptionType.Boolean,
+        description: "Whether the response should be ephemeral.",
     }]
 };
