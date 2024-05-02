e35c82f6fdd812c68a08a1896c82d8bbeaafee2c
Tibor P. <TiborPeter1996@gmail.com>
2024-05-02 23:46:43 +0200

    Update dictionary.js

--- commands/api/dictionary.js
+++ commands/api/dictionary.js
@@ -11,17 +11,41 @@ exports.run = async (client, interaction, logger, globalVars, ephemeral = true) 
         let dictionaryEmbed = new Discord.EmbedBuilder()
             .setColor(globalVars.embedColor);
 
-            let inputWord = interaction.options.getString("word");
-            wordStatus = await axios.get(`${api}entries/en/${inputWord}`);
-            wordStatus = wordStatus.data;
-            await wordStatus.forEach(async word => {
-                let wordStatusTitle = word.word;
-                let wordPhonetic =  word.phonetic
-                
-            });
-            dictionaryEmbed.setTitle(wordStatusTitle);
-            dictionaryEmbed.setDescription(wordPhonetic);
-    return sendMessage({ client: client, interaction: interaction, embeds: dictionaryEmbed, ephemeral: ephemeral });
+        let inputWord = interaction.options.getString("word");
+        wordStatus = await axios.get(`${api}entries/en/${inputWord}`);
+        wordStatus = wordStatus.data;
+
+        let inputWordType = interaction.options.getString("wordtype");
+
+        let wordMeaning;
+        for (let i; i < wordStatus.length; i++) {
+        if (inputWordType) {
+            for (let meaning of wordStatus[i].meanings) {
+                if (meaning.partOfSpeech.toLowerCase() === inputWordType.toLowerCase()) {
+                    wordMeaning = meaning;
+                    break;
+                }
+            }
+        }
+    }
+        if (!wordMeaning) {
+            wordMeaning = wordStatus[0].meanings[0];
+        }
+
+        let wordStatusTitle = wordStatus[0].word;
+        let wordPhonetic = wordStatus[0].phonetic;
+        let wordDefinition = wordMeaning.definitions[0].definition;
+        let wordSynonyms = wordMeaning.definitions[0].synonyms;
+
+        dictionaryEmbed.setAuthor({ name: 'DictionaryAPI', url: 'https://dictionaryapi.dev' });
+        dictionaryEmbed.setTitle(`${wordStatusTitle} - ${wordPhonetic}`);
+        dictionaryEmbed.setURL(`https://www.oxfordlearnersdictionaries.com/definition/english/${wordStatusTitle}_1`);
+        dictionaryEmbed.setDescription(`${wordDefinition}`);
+        if (wordSynonyms.length > 0) {
+            dictionaryEmbed.addFields([{ name: "Synonyms:", value: wordSynonyms.join(', '), inline: false }])
+        }
+
+        return sendMessage({ client: client, interaction: interaction, embeds: dictionaryEmbed, ephemeral: ephemeral });
 
     } catch (e) {
         // Log error
@@ -37,6 +61,15 @@ module.exports.config = {
         type: Discord.ApplicationCommandOptionType.String,
         description: "Specify word to look up.",
         required: true
+    }, {
+        name: "wordtype",
+        type: Discord.ApplicationCommandOptionType.String,
+        description: "noun, verb, adjective, etc.",
+        choices: [
+            { name: "noun", value: "noun" },
+            { name: "verb", value: "verb" },
+            { name: "adjective", value: "adjective" }
+        ]
     }, {
         name: "ephemeral",
         type: Discord.ApplicationCommandOptionType.Boolean,
