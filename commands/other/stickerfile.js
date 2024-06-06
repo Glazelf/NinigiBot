0816ea3907158fc385738ec14bbe419fa783f94f
Glazelf <35176230+Glazelf@users.noreply.github.com>
2024-06-06 23:13:14 +0200

    Rework sticker loop
    Remove obsolete checks
    Includes workaround for https://github.com/discordjs/discord.js/issues/10329

--- commands/other/stickerfile.js
+++ commands/other/stickerfile.js
@@ -9,15 +9,12 @@ export default async (client, interaction) => {
         let noStickerString = `This only works for messages with stickers attached.`;
         if (!message.stickers || !message.stickers.first()) return sendMessage({ client: client, interaction: interaction, content: noStickerString });
 
-        if (message.stickers.size == 1) {
-            returnString += `\n[${message.stickers.first().name}](${message.stickers.first().url})`;
-        } else if (message.stickers.size > 1) {
-            await message.stickers.forEach(sticker => {
-                returnString += `\n${sticker.name} ${sticker.url}`;
-            });
-        } else {
-            return sendMessage({ client: client, interaction: interaction, content: noStickerString });
-        };
+        await message.stickers.forEach(sticker => {
+            // stickerURL variable becomes obsolete when Discord.JS gif sticker URLs get fixed; https://github.com/discordjs/discord.js/issues/10329
+            let stickerURL = sticker.url;
+            if (stickerURL.endsWith(".gif")) stickerURL = stickerURL.replace("cdn.discordapp.com", "media.discordapp.net");
+            returnString += `\n[${sticker.name}](${stickerURL})`;
+        });
         return sendMessage({ client: client, interaction: interaction, content: returnString });
 
     } catch (e) {
