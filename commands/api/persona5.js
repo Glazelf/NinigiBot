ff19f5b05d71d12d3d41ad13c2001f65e15f6bbe
Glazelf <35176230+Glazelf@users.noreply.github.com>
2024-06-15 23:37:45 +0200

    Fix comment

--- commands/api/persona5.js
+++ commands/api/persona5.js
@@ -13,7 +13,7 @@ let personaWiki = "https://static.wikia.nocookie.net/megamitensei/images/";
 // specialCombosRoyal; special fusions
 // dlcPersonaRoyal; list of DLC Persona names
 let rarePersonaeRoyal, rareCombosRoyal, arcana2CombosRoyal, specialCombosRoyal, dlcPersonaRoyal, inheritanceChartRoyal;
-// (0, eval)(fs.readFileSync("submodules/persona5_calculator/data/Data5Royal.js", "utf8").replace(/var /g, ""));
+// eval(fs.readFileSync("submodules/persona5_calculator/data/Data5Royal.js", "utf8").replace(/var /g, ""));
 let itemMapRoyal; // Object including all item names mapped to item type/descriptions
 eval(fs.readFileSync("submodules/persona5_calculator/data/ItemDataRoyal.js", "utf8").replace("var", ""));
 let personaMapRoyal; // Object including all persona data (incl. DLC)
