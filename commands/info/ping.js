72c34230dedc70117be01904ab362659ee4600fc
Glazelf <35176230+Glazelf@users.noreply.github.com>
2024-07-11 19:41:10 +0200

    Remove testing code

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -1,12 +1,7 @@
-import { SlashCommandBuilder, PermissionFlagsBits, PermissionsBitField } from "discord.js";
+import { SlashCommandBuilder } from "discord.js";
 import sendMessage from "../../util/sendMessage.js";
 
 export default async (interaction) => {
-    let perms = new PermissionsBitField().add(PermissionFlagsBits.KickMembers).toArray();
-    console.log(PermissionFlagsBits[PermissionFlagsBits.KickMembers])
-
-    console.log(Object.keys(PermissionFlagsBits)[parseInt(PermissionFlagsBits.KickMembers) - 1])
-
     let commandPing = Math.abs(Date.now() - interaction.createdTimestamp);
     let websocketPing = Math.abs(interaction.client.ws.ping);
     let replyString = `Pong!\n- Websocket ping is ${websocketPing}ms.\n- Command ping is ${commandPing}ms.`;
