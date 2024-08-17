45a3cf7ce75aad8270ddb7f469d3a37fc4b12df3
Glazelf <35176230+Glazelf@users.noreply.github.com>
2024-08-18 00:47:30 +0200

    Clean up variables for affairs

--- affairs/stan.js
+++ affairs/stan.js
@@ -1,26 +1,27 @@
 import { EmbedBuilder } from "discord.js";
-import logger from '../util/logger.js';
-import globalVars from "../objects/globalVars.json" with { type: "json" };
-import getRandomGif from "../util/math/getRandomGif.js";
 import cron from "cron";
+import logger from '../util/logger.js';
+import getRandomGif from "../util/math/getRandomGif.js";
 import {
     incrementStanAmount,
     checkEvents
 } from "../database/dbServices/history.api.js";
+import globalVars from "../objects/globalVars.json" with { type: "json" };
+
+const timezone = "utc";
+const time = '00 00 18 * * *'; // Sec Min Hour
+const gifTags = ['pokemon', 'geass', 'dragon', 'game'];
+const guildID = globalVars.ShinxServerID;
+const stanRoleID = "743144948328562729";
 
 export default async (client) => {
     try {
-        let timezone = "utc";
-        const time = '00 00 18 * * *'; // Sec Min Hour
-        const gifTags = ['pokemon', 'geass', 'dragon', 'game'];
-        const guildID = globalVars.ShinxServerID;
-
         if (client.user.id != globalVars.NinigiID) return;
         // Create cronjob
         new cron.CronJob(time, async () => {
             let guild = await client.guilds.fetch(guildID);
             if (!guild) return;
-            let stanRoleID = "743144948328562729";
+
             let stanRole = await guild.roles.fetch(stanRoleID, { force: true });
             let candidates = stanRole.members.map(m => m.user);
             if (candidates.length < 1) return;
