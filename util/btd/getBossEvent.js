7f06c9c28831904f498773dc70001099066bc49a
Glaze <35176230+Glazelf@users.noreply.github.com>
2024-10-20 10:24:27 +0200

    Find current event more accurately
    Done to try and fix a bug on our end, but instead the API is not updated for Blastapopulous yet. This is still a good change, though.

--- util/btd/getBossEvent.js
+++ util/btd/getBossEvent.js
@@ -58,20 +58,21 @@ const towersOrdered = [
 export default async ({ elite = false, emojis }) => {
     let bossEventsResponse = await axios.get(`${btd6api}bosses`);
     if (!bossEventsResponse.data.success) return bossEventsResponse.data.error;
-    let mostRecentBossEvent = bossEventsResponse.data.body[0];
+    let bossEventSelected = bossEventsResponse.data.body.find(event => event.start < Date.now() && event.end > Date.now());
+    if (!bossEventSelected) bossEventSelected = bossEventsResponse.data.body[0];
 
-    let metadataURL = mostRecentBossEvent.metadataStandard;
-    if (elite) metadataURL = mostRecentBossEvent.metadataElite;
+    let metadataURL = bossEventSelected.metadataStandard;
+    if (elite) metadataURL = bossEventSelected.metadataElite;
     let bossEventMetadataResponse = await axios.get(metadataURL);
     if (!bossEventMetadataResponse.data.success) return bossEventMetadataResponse.data.error;
 
     let bossEventMetadata = bossEventMetadataResponse.data.body;
     let bossEventTitle = "Boss Event - ";
     if (elite) bossEventTitle += "Elite ";
-    bossEventTitle += mostRecentBossEvent.bossType.charAt(0).toUpperCase() + mostRecentBossEvent.bossType.slice(1); // Capitalize first character
+    bossEventTitle += bossEventSelected.bossType.charAt(0).toUpperCase() + bossEventSelected.bossType.slice(1); // Capitalize first character
 
     let rulesArray = [
-        `Score: ${mostRecentBossEvent.scoringType}`,
+        `Score: ${bossEventSelected.scoringType}`,
         `Map: ${bossEventMetadata.map}`,
         `Difficulty: ${bossEventMetadata.difficulty}`,
         `Lives: ${bossEventMetadata.lives}`,
@@ -91,7 +92,7 @@ export default async ({ elite = false, emojis }) => {
     if (bossEventMetadata._bloonModifiers.allRegen) rulesArray.push(`All bloons are regen.`);
     let rulesString = rulesArray.join("\n");
 
-    let bossEventDescription = `${time(Math.floor(mostRecentBossEvent.start / 1000), TimestampStyles.ShortDateTime)} to ${time(Math.floor(mostRecentBossEvent.end / 1000), TimestampStyles.ShortDateTime)}\n${rulesString}`;
+    let bossEventDescription = `${time(Math.floor(bossEventSelected.start / 1000), TimestampStyles.ShortDateTime)} to ${time(Math.floor(bossEventSelected.end / 1000), TimestampStyles.ShortDateTime)}\n${rulesString}`;
 
     let allowedHeroesArray = [];
     let allowedTowersArray = [];
@@ -130,16 +131,16 @@ export default async ({ elite = false, emojis }) => {
         .setTitle(bossEventTitle)
         .setDescription(bossEventDescription)
         .setThumbnail(bossEventMetadata.mapURL)
-        .setImage(mostRecentBossEvent.bossTypeURL)
+        .setImage(bossEventSelected.bossTypeURL)
         .addFields([
             { name: "Banned:", value: bannedString, inline: false },
             { name: "Allowed Heroes:", value: allowedHeroesString, inline: true },
             { name: "Allowed Towers:", value: allowedTowersString, inline: true },
 
         ]);
-    if (mostRecentBossEvent.end < Date.now()) {
+    if (bossEventSelected.end < Date.now()) {
         bossEventEmbed.setAuthor({ name: "No boss event is currently ongoing.\nHere is info on the most recent one instead:" });
-    } else if (mostRecentBossEvent.start > Date.now()) {
+    } else if (bossEventSelected.start > Date.now()) {
         bossEventEmbed.setAuthor({ name: "No boss event is currently ongoing.\nHere is info on the next one instead:" });
     };
 
