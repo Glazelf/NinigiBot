b9ab5ba7808b6d52195dae87ae34730db5a3ea1e
Glaze <35176230+Glazelf@users.noreply.github.com>
2024-12-04 19:07:33 +0100

    Allow '

--- util/string/checkFormat.js
+++ util/string/checkFormat.js
@@ -4,7 +4,7 @@ export default (string, length, regex_check = true) => {
         return 'TooShort';
     } else if (tstring.length > length) {
         return 'TooLong';
-    } else if (regex_check && !tstring.match(/^[\w\- ]+$/)) { // Filter to alphanumeric, dashes and spaces
+    } else if (regex_check && !tstring.match(/^[\w\-' ]+$/)) { // Filter to alphanumeric, ', - and spaces. ' is allowed mostly because of trophy names.
         return 'InvalidChars';
     };
     return 'Ok';
