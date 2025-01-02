736bb7af09caadf4fbdb0bb26ab0fee1ceef9075
Glaze <35176230+Glazelf@users.noreply.github.com>
2025-01-02 08:07:05 +0100

    Rename and clarify string

--- commands/info/messageAssets.js
+++ commands/info/messageAssets.js
@@ -6,13 +6,13 @@ import {
 } from "discord.js";
 import sendMessage from "../../util/discord/sendMessage.js";
 
-const noStickerString = `This only works for messages with stickers attached.`;
+const noAssetsString = `This only works for messages with stickers attached. Note that regular images are not included.`;
 
 export default async (interaction, messageFlags) => {
     messageFlags.add(MessageFlags.Ephemeral);
     let returnString = `Here's the link(s) to the assets you requested:`;
     let message = interaction.options._hoistedOptions[0].message;
-    if (!message.stickers || !message.stickers.first()) return sendMessage({ interaction: interaction, content: noStickerString, flags: messageFlags });
+    if (!message.stickers || !message.stickers.first()) return sendMessage({ interaction: interaction, content: noAssetsString, flags: messageFlags });
 
     await message.stickers.forEach(sticker => {
         returnString += `\n${hyperlink(sticker.name, sticker.url)}`;
