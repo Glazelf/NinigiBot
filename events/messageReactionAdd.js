13e22b8874382127fe9eebf62d90dfa95ef53e5f
Glaze <35176230+Glazelf@users.noreply.github.com>
2025-01-04 23:10:53 +0100

    add trace logs

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -9,17 +9,21 @@ const altboardEmoteID = altboardEmote.replace(/[^0-9]+/g, "");
 
 export default async (client, messageReaction) => {
     try {
+        console.log(1)
         let boardEmote = starboardEmote;
         // Check if message has reactions and if reaction is a star
         if (!messageReaction.count) messageReaction = await messageReaction.fetch().catch(e => { return null; });
         if (!messageReaction) return;
+        console.log(2)
         // Check if message is reacting to nostar in Shinx server
         const isNoStar = (messageReaction.emoji.id === altboardEmoteID && messageReaction.message.guildId == globalVars.ShinxServerID);
         if (messageReaction.emoji.name !== boardEmote && !isNoStar) return;
+        console.log(3)
         // Try to fetch message
         // let targetMessage = await messageReaction.message.channel.messages.fetch(messageReaction.message.id, { force: true });
         let targetMessage = messageReaction.message; // No fetch is a test, if this doesn't work, try fetching with { force: true }
         if (!targetMessage) return;
+        console.log(4)
         // Try to find the starboard channel, won't exist if server hasn't set one
         let starboardChannel, starboard;
         let serverApi = await import("../database/dbServices/server.api.js");
@@ -32,8 +36,11 @@ export default async (client, messageReaction) => {
             if (!starboardChannel) return;
             starboard = await targetMessage.guild.channels.fetch(starboardChannel.channel_id);
         };
+        console.log(5)
         if (!starboard) return;
+        console.log(6)
         if (targetMessage.channel == starboard) return;
+        console.log(7)
         // Try to find the starred message in database
         let messageDB = await serverApi.StarboardMessages.findOne({ where: { channel_id: targetMessage.channel.id, message_id: targetMessage.id } });
         // Try to find the star requirement. If it doesn't exist, use the default
@@ -47,6 +54,7 @@ export default async (client, messageReaction) => {
         // Check if message already existed in database (was posted to starboard) or if star amount simply changed
         if (messageReaction.count >= starLimit && !messageDB) {
             // Send message then push data to database
+            console.log(11)
             await sendStarboardMessage(starboardMessage, targetMessage);
             return;
         } else if (messageDB) {
@@ -54,19 +62,25 @@ export default async (client, messageReaction) => {
             let starChannel = null;
             let starMessage = null;
             try {
+                console.log(10)
                 starChannel = await client.channels.fetch(messageDB.starboard_channel_id);
                 starMessage = await starChannel.messages.fetch(messageDB.starboard_message_id);
             } catch (e) {
                 if (starChannel && !starMessage) {
+                    console.log(8)
                     await messageDB.destroy();
                     await sendStarboardMessage(starboardMessage, targetMessage);
                 };
+                console.log(9)
                 return;
             };
+            console.log(12)
             if (!starMessage) return;
+            console.log(13)
             if (starChannel !== starboard) return; // Fix cross-updating between starboard and evil starboard
             await starMessage.edit(starboardMessage);
             // Try to pin messages with double stars
+            console.log(14)
             if (messageReaction.count >= starLimit * 2) starMessage.pin().catch(e => {
                 // console.log(e);
                 return;
