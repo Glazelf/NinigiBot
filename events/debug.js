fb063669e92aabe20a4ce1236f09a53738aa42b4
Glaze <35176230+Glazelf@users.noreply.github.com>
2025-01-06 19:18:39 +0100

    Debug logging
    Closes #1012

--- events/debug.js
+++ events/debug.js
@@ -1,4 +1,6 @@
 export default async (client, info) => {
-    // log info
-    // console.log(info);
+    // return; // Comment out to enable debugging, uncomment to disable
+    let infoLimited = info.substring(0, 2000);
+    let devChannel = await client.channels.fetch("1325890140517826580"); // Replace ID with specific debug channel ID
+    return devChannel.send({ content: infoLimited });
 };
