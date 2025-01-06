f740d7e340b5dc0b127296c5555673389084d055
Glaze <35176230+Glazelf@users.noreply.github.com>
2025-01-06 19:21:29 +0100

    Only fetch debug channel if null

--- events/debug.js
+++ events/debug.js
@@ -1,6 +1,8 @@
 export default async (client, info) => {
     // return; // Comment out to enable debugging, uncomment to disable
     let infoLimited = info.substring(0, 2000);
-    let devChannel = await client.channels.fetch("1325890140517826580"); // Replace ID with specific debug channel ID
-    return devChannel.send({ content: infoLimited });
+    let debugChannelID = "1325890140517826580"; // Replace ID with specific debug channel ID
+    let debugChannel = client.channels.cache.get(debugChannelID);
+    if (!debugChannel) debugChannel = await client.channels.fetch("1325890140517826580");
+    return debugChannel.send({ content: infoLimited });
 };
