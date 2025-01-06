a099899b3b0c9520d346c83a4664061a62f03664
Glaze <35176230+Glazelf@users.noreply.github.com>
2025-01-06 19:50:42 +0100

    Rebuild debug into embed

--- events/debug.js
+++ events/debug.js
@@ -1,8 +1,31 @@
+import {
+    EmbedBuilder
+} from "discord.js";
+import globalVars from "../objects/globalVars.json" with { type: "json" };
+
+const maxEmbedLength = 6000;
+const maxDescriptionLength = 2000;
+const maxFieldValueLength = 1024;
+const debugChannelID = "1325890140517826580"; // Replace ID with specific debug channel ID
+
 export default async (client, info) => {
     // return; // Comment out to enable debugging, uncomment to disable
-    let infoLimited = info.substring(0, 2000);
-    let debugChannelID = "1325890140517826580"; // Replace ID with specific debug channel ID
+    // Format strings
+    let description = info;
+    if (info.length > maxDescriptionLength) description = info.substring(0, maxDescriptionLength);
+    let debugInfoIndex = maxDescriptionLength + 1;
+    // Channel
     let debugChannel = client.channels.cache.get(debugChannelID);
     if (!debugChannel) debugChannel = await client.channels.fetch(debugChannelID);
-    return debugChannel.send({ content: infoLimited });
+    // Build embed
+    let debugEmbed = new EmbedBuilder()
+        .setTitle("Debug")
+        .setColor(globalVars.embedColor)
+        .setDescription(description);
+    // Fill fields
+    while (debugEmbed.length <= maxEmbedLength && info[debugInfoIndex]) {
+        debugEmbed.addFields([{ name: "Field", value: info.substring(debugInfoIndex, debugInfoIndex + maxFieldValueLength), inline: false }]);
+    };
+    // Send embed
+    return debugChannel.send({ embeds: [debugEmbed] });
 };
