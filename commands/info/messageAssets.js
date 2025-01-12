8f69d5d23cc6f90d22efeb693afb943a21c47f47
Glaze <35176230+Glazelf@users.noreply.github.com>
2025-01-12 15:55:41 +0100

    Refactor message assets
    Also incorporate regular attachments

--- commands/info/messageAssets.js
+++ commands/info/messageAssets.js
@@ -2,7 +2,9 @@ import {
     MessageFlags,
     ContextMenuCommandBuilder,
     ApplicationCommandType,
-    hyperlink
+    hyperlink,
+    hideLinkEmbed,
+    HeadingLevel
 } from "discord.js";
 import sendMessage from "../../util/discord/sendMessage.js";
 
@@ -10,14 +12,26 @@ const noAssetsString = `This only works for messages with supported assets, like
 
 export default async (interaction, messageFlags) => {
     messageFlags.add(MessageFlags.Ephemeral);
-    let returnString = `Here's the link(s) to the assets you requested:`;
+    let returnString = `Here's the assets you requested.\n`;
     let message = interaction.options._hoistedOptions[0].message;
-    if (!message.stickers || !message.stickers.first()) return sendMessage({ interaction: interaction, content: noAssetsString, flags: messageFlags });
 
-    await message.stickers.forEach(sticker => {
-        returnString += `\n${hyperlink(sticker.name, sticker.url)}`;
-    });
+    const messageHasStickers = message.stickers.size > 0;
+    const messageHasAttachments = message.attachments.size > 0;
+    if (!messageHasAttachments && !messageHasStickers) return sendMessage({ interaction: interaction, content: noAssetsString, flags: messageFlags });
+
+    if (messageHasStickers) returnString += `Sticker(s):\n${addAttachmentString(message.stickers)}\n`;
+    if (messageHasAttachments) returnString += `Attachment(s):\n${addAttachmentString(message.attachments)}\n`;
     return sendMessage({ interaction: interaction, content: returnString, flags: messageFlags });
+};
+
+function addAttachmentString(collection) {
+    let string = "";
+    collection.forEach(item => {
+        let url = item.url;
+        if (item.proxyURL) url = item.proxyURL;
+        string += `${hyperlink(item.name, hideLinkEmbed(url))}\n`;
+    });
+    return string;
 };
 
 export const commandObject = new ContextMenuCommandBuilder()
