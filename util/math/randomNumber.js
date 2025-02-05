11cc433fb9204bbf01f3f13d9661207bd0a8c9da
Glaze <35176230+Glazelf@users.noreply.github.com>
2025-02-05 07:21:01 +0100

    Catch equal boundaries in random number

--- util/math/randomNumber.js
+++ util/math/randomNumber.js
@@ -2,6 +2,7 @@ import crypto from "crypto";
 
 export default (min, max) => {
     if (min > max) [min, max] = [max, min]; // Flip variables if min is higher
+    if (min === max) return max; // If equal return
     min = Math.ceil(min);
     max = Math.floor(max);
     return crypto.randomInt(min, max);
