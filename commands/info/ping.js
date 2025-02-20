85612e14a1464e29068992f6f1577a8cb7045f31
Glaze <35176230+Glazelf@users.noreply.github.com>
2025-02-20 07:32:58 +0100

    Remove testing code

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -8,11 +8,6 @@ export default async (interaction, messageFlags) => {
     let commandPing = Math.abs(Date.now() - interaction.createdTimestamp);
     let websocketPing = Math.abs(interaction.client.ws.ping);
     let replyString = `Pong!\n- Websocket ping is ${websocketPing}ms.\n- Command ping is ${commandPing}ms.`;
-
-    let problemChannel = await interaction.client.channels.fetch("581776008978497537");
-    console.log(problemChannel)
-    return sendMessage({ interaction: interaction, content: problemChannel, flags: messageFlags.add(MessageFlags.Ephemeral) });
-
     return sendMessage({ interaction: interaction, content: replyString, flags: messageFlags.add(MessageFlags.Ephemeral) });
 };
 
