9cdb0436a21e4527cad2c8c45389c54822e025f3
Glaze <35176230+Glazelf@users.noreply.github.com>
2025-02-20 17:52:01 +0100

    Adjust test log
    Remove fetch and adjust comment

--- commands/api/dictionary.js
+++ commands/api/dictionary.js
@@ -15,10 +15,6 @@ const api = "https://api.dictionaryapi.dev/api/v2/";
 export default async (interaction, messageFlags) => {
     await interaction.deferReply({ flags: messageFlags });
 
-    //// https://github.com/discordjs/discord.js/issues/10563
-    let problemChannel = await interaction.client.rest.get("/channels/581776008978497537");
-    console.log(problemChannel);
-    
     let inputWord = interaction.options.getString("word");
     let inputWordType = interaction.options.getString("wordtype");
     let wordStatus;
