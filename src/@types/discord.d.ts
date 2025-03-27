07d7db5a0ee04c1f505488ebc7ae7da83581895e
Glaze <35176230+Glazelf@users.noreply.github.com>
2025-03-28 00:00:00 +0100

    Add limited settable bitfield class

--- src/@types/discord.d.ts
+++ src/@types/discord.d.ts
@@ -1,7 +1,12 @@
-import { Collection } from "discord.js";
+import {
+    Collection,
+    MessageFlags
+} from "discord.js";
 
 declare module "discord.js" {
     export interface Client {
         commands: Collection<any, any>;
     }
+
+    export type MessageFlagsBitFieldSettable = BitFieldResolvable<"Ephemeral" | "SuppressEmbeds" | "SuppressNotifications", MessageFlags.Ephemeral | MessageFlags.SuppressEmbeds | MessageFlags.SuppressNotifications>
 }
