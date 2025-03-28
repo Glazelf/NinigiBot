591e85b52ffa603d9dccf71099bdfae8f0435997
Glaze <35176230+Glazelf@users.noreply.github.com>
2025-03-28 20:52:09 +0100

    Simplify sendMessage by assuming type consistency

--- src/util/discord/sendMessage.js
+++ src/util/discord/sendMessage.js
@@ -7,31 +7,9 @@ export default async ({ interaction, content = "", embeds = [], files = [], comp
     // 'DEFAULT' = text message, 'APPLICATION_COMMAND' = slash command
     let messageObject = {};
     if (content) messageObject.content = content;
-    if (embeds) {
-        if (Array.isArray(embeds)) {
-            messageObject.embeds = embeds;
-        } else {
-            messageObject.embeds = [embeds];
-        };
-    };
-    if (files) {
-        if (Array.isArray(files)) {
-            messageObject.files = files;
-        } else {
-            messageObject.files = [files];
-        };
-    };
-    // Don't add components to slash commands unless specifically told to do so
-    if (components) {
-        // Components, i.e. buttons
-        if (Array.isArray(components)) {
-            messageObject.components = components;
-        } else {
-            if (components.components.length != 0) {
-                messageObject.components = [components];
-            };
-        };
-    };
+    if (embeds) messageObject.embeds = embeds;
+    if (files) messageObject.files = files;
+    if (components) messageObject.components = components;
     messageObject.flags = flags;
     messageObject.allowedMentions = { parse: ['users', 'roles'], repliedUser: true };
     // let targetUser = interaction.options.getUser("user");
