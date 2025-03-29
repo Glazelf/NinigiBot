f1a79e60b920f559538a5e6bb484613160dd2f3e
Glaze <35176230+Glazelf@users.noreply.github.com>
2025-03-29 12:59:43 +0100

    Add MessageObject type

--- src/@types/discord.d.ts
+++ src/@types/discord.d.ts
@@ -11,4 +11,13 @@ declare module "discord.js" {
 
     // Create an alternative MessageFlagsBitField that includes just the settable bits
     export type MessageFlagsBitFieldSettable = BitFieldResolvable<"Ephemeral" | "SuppressEmbeds" | "SuppressNotifications", MessageFlags.Ephemeral | MessageFlags.SuppressEmbeds | MessageFlags.SuppressNotifications>
+    // Create a message object type
+    export interface MessageObject {
+        content?: String,
+        embeds?: EmbedBuilder[]
+        components?: []
+        files?: []
+        flags: MessageFlagsBitField,
+        allowedMentions: {}
+    }
 }
