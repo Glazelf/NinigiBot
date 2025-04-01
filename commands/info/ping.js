7d24d1cfc5f783d090e9b27e362423defb138ca0
Glaze <35176230+Glazelf@users.noreply.github.com>
2025-04-01 18:24:30 +0200

    Remove test SKU fetch

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -5,8 +5,6 @@ import {
 import sendMessage from "../../util/discord/sendMessage.js";
 
 export default async (interaction, messageFlags) => {
-    let SKUs = await interaction.client.application.fetchSKUs();
-    console.log(SKUs)
     let commandPing = Math.abs(Date.now() - interaction.createdTimestamp);
     let websocketPing = Math.abs(interaction.client.ws.ping);
     let replyString = `Pong!\n- Websocket ping is ${websocketPing}ms.\n- Command ping is ${commandPing}ms.`;
