0c375df3543e788a85cc2afe1e5ede9b16f5f00d
Glaze <35176230+Glazelf@users.noreply.github.com>
2025-11-26 20:02:25 +0100

    Skip over failed member fetches

--- commands/virtual_simulation/shinx.js
+++ commands/virtual_simulation/shinx.js
@@ -164,7 +164,7 @@ export default async (interaction, messageFlags) => {
                     ctx.drawImage(img, 51 * !shinx.user_male, 0, 51, 72, 120, 126, 51, 72);
                     ctx.font = 'normal bold 16px Arial';
                     ctx.fillStyle = '#ffffff';
-                    if (guildDataAvailable) userFinder = await interaction.guild.members.fetch();
+                    if (guildDataAvailable) userFinder = await interaction.guild.members.fetch().catch(e => { return; });
 
                     for (let i = 0; i < guests.length; i++) {
                         const nick = userFinder.get(guests[i].user_id).user.username.split(' ');
@@ -212,7 +212,7 @@ export default async (interaction, messageFlags) => {
             ctx.drawImage(img, 51 * !shinx.user_male, 72 * 0, 51, 72, 60, 223, 51, 72);
             ctx.font = 'normal bolder 18px Arial';
             ctx.fillStyle = 'purple';
-            if (guildDataAvailable) userFinder = await interaction.guild.members.fetch();
+            if (guildDataAvailable) userFinder = await interaction.guild.members.fetch().catch(e => { return; });
 
             for (let i = 0; i < guests.length; i++) {
                 const nick = userFinder.get(guests[i].user_id).user.username.split(' ');
