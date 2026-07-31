af9812c208b714e82f9af1eec8712406fa94c036
Glaze <35176230+Glazelf@users.noreply.github.com>
2026-07-31 22:56:48 +0200

    remove logs

--- util/discord/isGuildDataAvailable.js
+++ util/discord/isGuildDataAvailable.js
@@ -1,10 +1,4 @@
 import { ApplicationIntegrationType } from 'discord.js';
 
 export default (interaction) => {
-    console.log(`inGuild: ${interaction.inGuild()}`)
-    console.log(Object.keys(interaction.authorizingIntegrationOwners).includes(ApplicationIntegrationType.GuildInstall.toString()))
-    console.log(interaction.authorizingIntegrationOwners)
-    console.log(ApplicationIntegrationType.GuildInstall)
-    console.log(ApplicationIntegrationType)
-    return (interaction.inGuild() && Object.keys(interaction.authorizingIntegrationOwners).includes(ApplicationIntegrationType.GuildInstall.toString()));
 };
