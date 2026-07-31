a373f14c5b66fbf88641e4284d30cc63d975db73
Glaze <35176230+Glazelf@users.noreply.github.com>
2026-07-31 22:57:04 +0200

    Fix guild availability check

--- util/discord/isGuildDataAvailable.js
+++ util/discord/isGuildDataAvailable.js
@@ -1,4 +1,6 @@
 import { ApplicationIntegrationType } from 'discord.js';
 
 export default (interaction) => {
+    // A little ugly to hardcode this text but they made the variables less consistent
+    return (interaction.inGuild() && Object.keys(interaction.authorizingIntegrationOwners).includes("guildId"));
 };
