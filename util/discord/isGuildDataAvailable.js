9aa16eb6de4ffad4619e1c6afede6147f9003214
Glaze <35176230+Glazelf@users.noreply.github.com>
2026-07-31 22:52:44 +0200

    MORE test logs

--- util/discord/isGuildDataAvailable.js
+++ util/discord/isGuildDataAvailable.js
@@ -4,5 +4,7 @@ export default (interaction) => {
     console.log(`inGuild: ${interaction.inGuild()}`)
     console.log(Object.keys(interaction.authorizingIntegrationOwners).includes(ApplicationIntegrationType.GuildInstall.toString()))
     console.log(interaction.authorizingIntegrationOwners)
+    console.log(ApplicationIntegrationType.GuildInstall)
+    console.log(ApplicationIntegrationType.GuildInstall.toString())
     return (interaction.inGuild() && Object.keys(interaction.authorizingIntegrationOwners).includes(ApplicationIntegrationType.GuildInstall.toString()));
 };
