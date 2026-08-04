198ba729cab56413ed7cf3aa583d472a22792022
Glaze <35176230+Glazelf@users.noreply.github.com>
2026-08-04 18:33:33 +0200

    Test log

--- util/discord/isGuildDataAvailable.js
+++ util/discord/isGuildDataAvailable.js
@@ -2,5 +2,6 @@ import { ApplicationIntegrationType } from 'discord.js';
 
 export default (interaction) => {
     // A little ugly to hardcode this text but they made the variables less consistent
+    console.log(interaction.authorizingIntegrationOwners)
     return (interaction.inGuild() && Object.keys(interaction.authorizingIntegrationOwners).includes("guildId"));
 };
